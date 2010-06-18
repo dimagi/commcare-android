b6f75707a445dd0c29095f02dd017abfcc7d7698
Clayton Sims <csims@dimagi.com>
2010-06-18 14:26:40 -0400

    sdf

---
+++ src/org/commcare/android/resource/installers/LocaleAndroidInstaller.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package org.commcare.android.resource.installers;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.commcare.android.util.AndroidCommCarePlatform;
+import org.commcare.resources.model.Resource;
+import org.commcare.resources.model.ResourceInitializationException;
+import org.javarosa.core.model.FormDef;
+import org.javarosa.core.reference.Reference;
+import org.javarosa.core.services.locale.Localization;
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.core.util.externalizable.ExtUtil;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
+import org.javarosa.xform.parse.XFormParser;
+
+/**
+ * @author ctsims
+ *
+ */
+public class LocaleAndroidInstaller extends FileSystemInstaller {
+
+	String locale;
+	
+	public LocaleAndroidInstaller() {
+		
+	}
+	
+	public LocaleAndroidInstaller(String destination, String locale) {
+		super(destination);
+		this.locale = locale;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see org.commcare.resources.model.ResourceInstaller#initialize(org.commcare.util.CommCareInstance)
+	 */
+	public boolean initialize(AndroidCommCarePlatform instance) throws ResourceInitializationException {
+		Localization.registerLanguageReference(locale, localLocation);
+		return true;
+	}
+	
+	protected int customInstall(Reference local) throws IOException {
+		return Resource.RESOURCE_STATUS_INSTALLED;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.commcare.resources.model.ResourceInstaller#requiresRuntimeInitialization()
+	 */
+	public boolean requiresRuntimeInitialization() {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.util.externalizable.Externalizable#readExternal(java.io.DataInputStream, org.javarosa.core.util.externalizable.PrototypeFactory)
+	 */
+	public void readExternal(DataInputStream in, PrototypeFactory pf) throws IOException, DeserializationException {
+		super.readExternal(in, pf);
+		this.locale = ExtUtil.nullIfEmpty(ExtUtil.readString(in));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.util.externalizable.Externalizable#writeExternal(java.io.DataOutputStream)
+	 */
+	public void writeExternal(DataOutputStream out) throws IOException {
+		super.writeExternal(out);
+		ExtUtil.writeString(out, ExtUtil.emptyIfNull(locale));
+	}
+
+
+}
