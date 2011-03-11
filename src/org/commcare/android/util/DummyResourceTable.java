f38b370678931214fa0d84f8990ea7ef977cf1d0
Clayton Sims <csims@dimagi.com>
2011-03-11 12:24:15 -0500

    Forgot new dummy resource table class.

---
+++ src/org/commcare/android/util/DummyResourceTable.java
@@ -0,0 +1,210 @@
+/**
+ * 
+ */
+package org.commcare.android.util;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Vector;
+
+import org.commcare.resources.model.InstallerFactory;
+import org.commcare.resources.model.Resource;
+import org.commcare.resources.model.ResourceInitializationException;
+import org.commcare.resources.model.ResourceInstaller;
+import org.commcare.resources.model.ResourceLocation;
+import org.commcare.resources.model.ResourceTable;
+import org.commcare.resources.model.UnresolvedResourceException;
+import org.commcare.util.CommCareInstance;
+import org.commcare.xml.util.UnfullfilledRequirementsException;
+import org.javarosa.core.reference.Reference;
+import org.javarosa.core.services.storage.StorageFullException;
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
+
+/**
+ * @author ctsims
+ *
+ */
+public class DummyResourceTable extends ResourceTable {
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public InstallerFactory getInstallers() {
+		return new InstallerFactory() {
+			public ResourceInstaller getProfileInstaller(boolean forceInstall) {
+				return getDummyInstaller();
+			}
+			
+			public ResourceInstaller getXFormInstaller() {
+				return getDummyInstaller();
+			}
+			
+			public ResourceInstaller getSuiteInstaller() {
+				return getDummyInstaller();
+			}
+			
+			public ResourceInstaller getLocaleFileInstaller(String locale) {
+				return getDummyInstaller();
+			}
+			
+			public ResourceInstaller getLoginImageInstaller() {
+				return getDummyInstaller();
+			}
+			
+			public ResourceInstaller getMediaInstaller(String path) {
+				return getDummyInstaller();
+			}
+			
+			private ResourceInstaller getDummyInstaller() {
+				return new ResourceInstaller() {
+
+					public void readExternal(DataInputStream in,
+							PrototypeFactory pf) throws IOException,
+							DeserializationException {
+						// TODO Auto-generated method stub
+						
+					}
+
+					public void writeExternal(DataOutputStream out)
+							throws IOException {
+						// TODO Auto-generated method stub
+						
+					}
+
+					public boolean requiresRuntimeInitialization() {
+						// TODO Auto-generated method stub
+						return false;
+					}
+
+					public boolean initialize(CommCareInstance instance)
+							throws ResourceInitializationException {
+						// TODO Auto-generated method stub
+						return true;
+					}
+
+					public boolean install(Resource r,
+							ResourceLocation location, Reference ref,
+							ResourceTable table, CommCareInstance instance,
+							boolean upgrade)
+							throws UnresolvedResourceException,
+							UnfullfilledRequirementsException {
+						// TODO Auto-generated method stub
+						return true;
+					}
+
+					public boolean uninstall(Resource r, ResourceTable table,
+							ResourceTable incoming)
+							throws UnresolvedResourceException {
+						// TODO Auto-generated method stub
+						return true;
+					}
+
+					public boolean upgrade(Resource r, ResourceTable table)
+							throws UnresolvedResourceException {
+						// TODO Auto-generated method stub
+						return true;
+					}
+
+					public void cleanup() {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				};
+			}
+
+		};
+	}
+
+	@Override
+	public void removeResource(Resource resource) {
+	}
+
+	@Override
+	public void addResource(Resource resource, ResourceInstaller initializer, String parentId, int status) throws StorageFullException {
+	}
+
+	@Override
+	public void addResource(Resource resource, ResourceInstaller initializer,
+			String parentId) throws StorageFullException {
+	}
+
+	@Override
+	public void addResource(Resource resource, int status)
+			throws StorageFullException {
+	}
+
+	@Override
+	public Vector<Resource> getResourcesForParent(String parent) {
+		return new Vector<Resource>();
+	}
+
+	@Override
+	public Resource getResourceWithId(String id) {
+		return null;
+	}
+
+	@Override
+	public Resource getResourceWithGuid(String guid) {
+		return null;
+	}
+
+	@Override
+	public boolean isReady() {
+		return true;
+	}
+
+	@Override
+	public void commit(Resource r, int status, int version) throws UnresolvedResourceException {
+	}
+
+	@Override
+	public void commit(Resource r, int status)
+			throws UnresolvedResourceException {
+	}
+
+	@Override
+	public void commit(Resource r) throws UnresolvedResourceException {
+	}
+
+	@Override
+	public void prepareResources(ResourceTable master, CommCareInstance instance)
+			throws UnresolvedResourceException,
+			UnfullfilledRequirementsException {
+	}
+
+	@Override
+	public boolean upgradeTable(ResourceTable incoming)
+			throws UnresolvedResourceException {
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Dummy Table";
+	}
+
+	@Override
+	public String getStatus(int status) {
+		return "Dummy Table";
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void clear() {
+	}
+
+	@Override
+	public void initializeResources(CommCareInstance instance)
+			throws ResourceInitializationException {
+	}
+
+}
