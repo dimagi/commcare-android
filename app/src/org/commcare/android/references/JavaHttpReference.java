beb212258a260ebd41ee84107ffa143e590bef73
clayton sims <csims@dimagi.com>
2013-01-07 17:34:47 -0500

    Support HTTP -> HTTPS redirects on install

--- app/src/org/commcare/android/references/JavaHttpReference.java
+++ app/src/org/commcare/android/references/JavaHttpReference.java
@@ -9,8 +9,10 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.commcare.android.javarosa.AndroidLogger;
 import org.commcare.android.logic.GlobalConstants;
 import org.javarosa.core.reference.Reference;
+import org.javarosa.core.services.Logger;
 
 /**
  * @author ctsims
@@ -46,14 +48,53 @@ public class JavaHttpReference implements Reference {
 	public InputStream getStream() throws IOException {
 		URL url = new URL(uri);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		setup(con);
+		// Start the query
+		con.connect();
+		//It's possible we're getting redirected from http to https
+		//if so, we need to handle it explicitly
+		if(con.getResponseCode() == 301) {
+			//only allow one level of redirection here for now.	
+			Logger.log(AndroidLogger.TYPE_WARNING_NETWORK, "Attempting 1 stage redirect from " + uri + " to " + con.getURL().toString());
+			URL newUrl = con.getURL();
+			con.disconnect();
+			con = (HttpURLConnection) newUrl.openConnection();
+			setup(con);
+			con.connect();
+		}
+		
+		//Don't allow redirects _from_ https _to_ https unless they are redirecting to the same server.
+		if(!isValidRedirect(url, con.getURL())) {
+			Logger.log(AndroidLogger.TYPE_WARNING_NETWORK, "Invalid redirect from " + uri + " to " + con.getURL().toString());
+			throw new IOException("Invalid redirect from secure server to insecure server");
+		}
+		
+		return con.getInputStream();
+	}
+	
+	private boolean isValidRedirect(URL url, URL newUrl) {
+		//unless it's https, don't worry about it
+		if(!url.getProtocol().equals("https")) {
+			return true;
+		}
+		
+		//if it is, verify that we're on the same server.
+		if(url.getHost().equals(newUrl.getHost())) {
+			return true;
+		} else {
+			//otherwise we got redirected from a secure link to a different
+			//link, which isn't acceptable for now.
+			return false;
+		}
+	}
+
+	private void setup(HttpURLConnection con) throws IOException {
 		con.setConnectTimeout(GlobalConstants.CONNECTION_TIMEOUT);
 		con.setRequestMethod("GET");
 		con.setDoInput(true);
 		con.setInstanceFollowRedirects(true);
-		// Start the query
-		con.connect();
-		return con.getInputStream();
 	}
+
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.reference.Reference#getURI()
