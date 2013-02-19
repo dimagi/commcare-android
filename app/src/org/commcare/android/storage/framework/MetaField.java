2cf0caef13f08717d6123b52c95866866fbac458
clayton sims <csims@dimagi.com>
2013-02-19 16:58:35 -0500

    Fixed session state meta handling. NOTE this might result in very, very slow object writing for persisted framework objects. Figure that out.
    --HG--
    branch : commcare_2.4

--- app/src/org/commcare/android/storage/framework/MetaField.java
+++ app/src/org/commcare/android/storage/framework/MetaField.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD})
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface MetaField {
 	public String value();
 	public boolean unique() default false;
