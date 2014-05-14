c03ced08fdf0120e13dd94faa2a915408d99a566
Jenny Schweers <jschweers@dimagi.com>
2014-05-14 13:41:38 -0400

    Fix error message for CONNECTION_TIMEOUT

--- app/src/org/commcare/dalvik/activities/FormRecordListActivity.java
+++ app/src/org/commcare/dalvik/activities/FormRecordListActivity.java
@@ -434,7 +434,7 @@ public class FormRecordListActivity extends CommCareActivity<FormRecordListActiv
 							Toast.makeText(receiver, "Bad data from server. Please talk with your supervisor.", Toast.LENGTH_LONG).show();
 							break;							
 						case DataPullTask.CONNECTION_TIMEOUT:
-							Toast.makeText(receiver, "The server took to login to generate a response. Please try again later, and ask your supervisor if the problem persists.", Toast.LENGTH_LONG).show();
+							Toast.makeText(receiver, "The server took too long to generate a response. Please try again later, and ask your supervisor if the problem persists.", Toast.LENGTH_LONG).show();
 							break;
 						case DataPullTask.UNREACHABLE_HOST:
 							Toast.makeText(receiver, "Couldn't contact server, please check your network connection and try again.", Toast.LENGTH_LONG).show();
