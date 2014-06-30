24c56979fbc69f705211ba8349677c534ba9eb25
Jenny Schweers <jschweers@dimagi.com>
2014-06-30 16:35:27 -0400

    Support odk PollSensor extension actually using GPS

--- app/src/org/commcare/android/resource/installers/XFormAndroidInstaller.java
+++ app/src/org/commcare/android/resource/installers/XFormAndroidInstaller.java
@@ -36,7 +36,6 @@ import org.javarosa.form.api.FormEntryCaption;
 import org.javarosa.xform.parse.XFormParseException;
 import org.javarosa.xform.parse.XFormParser;
 import org.odk.collect.android.jr.extensions.IntentExtensionParser;
-import org.odk.collect.android.jr.extensions.PollSensorAction;
 import org.odk.collect.android.jr.extensions.PollSensorExtensionParser;
 
 import android.content.ContentProviderClient;
@@ -78,7 +77,7 @@ public class XFormAndroidInstaller extends FileSystemInstaller {
 	protected int customInstall(Resource r, Reference local, boolean upgrade) throws IOException, UnresolvedResourceException {
 		//Ugh. Really need to sync up the Xform libs between ccodk and odk.
 		XFormParser.registerHandler("intent", new IntentExtensionParser());
-		XFormParser.registerStructuredAction("pollsensor", new PollSensorExtensionParser());
+		XFormParser.registerStructuredAction("pollsensor", new PollSensorExtensionParser(CommCareApplication._()));
 		
 		FormDef formDef;
 		try {
