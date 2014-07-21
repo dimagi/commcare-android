c8c70ce28c99e087ed6cc33aae7b3573f187b3dc
amstone326 <none@none>
2014-07-21 11:32:04 -0400

    added MultipleApps activity and initial xml for view

---
+++ app/src/org/commcare/dalvik/activities/MultipleAppsManagerActivity.java
@@ -0,0 +1,28 @@
+package org.commcare.dalvik.activities;
+
+import org.commcare.dalvik.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MultipleAppsManagerActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.app_manager);
+	}
+	
+	public void installAppClicked(View v) {
+		
+	}
+	
+	public void uninstallAppClicked(View v) {
+		
+	}
+	
+	
+
+}
