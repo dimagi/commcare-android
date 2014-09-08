2752f53b363d7fb04c33da4f62480cfed1a415b6
ALIZA STONE <amstone326@gmail.com>
2014-09-08 00:05:17 -0400

    re-added extra call to setProfile()

--- app/src/org/commcare/android/resource/installers/ProfileAndroidInstaller.java
+++ app/src/org/commcare/android/resource/installers/ProfileAndroidInstaller.java
@@ -96,6 +96,7 @@ public class ProfileAndroidInstaller extends FileSystemInstaller {
                     upgrade ? Resource.RESOURCE_STATUS_UNINITIALIZED : Resource.RESOURCE_STATUS_UNINITIALIZED, false);
             
             Profile p = parser.parse();
+            instance.setProfile(p); //added, may not be ok
             
             if(!upgrade) {
                 initProperties(p);
