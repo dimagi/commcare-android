2f87eaadcb0c165423eec8fb08fd32bea26e44d2
Jenny Schweers <jschweers@dimagi.com>
2014-12-26 09:09:46 -0500

    WIP

--- app/src/org/achartengine/chart/LineChart.java
+++ app/src/org/achartengine/chart/LineChart.java
@@ -99,21 +99,27 @@ public class LineChart extends XYChart {
         }
 
         final float referencePoint;
+        final float boundary;
         switch (fill.getType()) {
         case BOUNDS_ALL:
           referencePoint = yAxisValue;
+          boundary = yAxisValue;
           break;
         case BOUNDS_BELOW:
           referencePoint = yAxisValue;
+          boundary = yAxisValue;
           break;
         case BOUNDS_ABOVE:
           referencePoint = yAxisValue;
+          boundary = yAxisValue;
           break;
         case BELOW:
           referencePoint = canvas.getHeight();
+          boundary = 0;
           break;
         case ABOVE:
           referencePoint = 0;
+          boundary = canvas.getHeight();
           break;
         default:
           throw new RuntimeException(
@@ -171,14 +177,19 @@ public class LineChart extends XYChart {
           fillPoints.set(0, fillPoints.get(0) + 1);
           int i = 0;
           while (i < length) {
-            if (fillPoints.get(i + 1) < 0) {
+            if (yOutOfBounds(fillPoints.get(i + 1), fill.getType(), canvas)) {
+            //if (fillPoints.get(i + 1) < 0) {
+System.out.println("[jls] something is out of bounds");
               int currentIndex = i;
-              // If there's a subsequent point, and its y is on the screen,
+              // If there's a previous point, and its y is on the screen,
               // add an intermediate point at the intersection of the line
               // segment and the y boundary
-              if (i >= 2 && fillPoints.get(i - 1) >= 0) {
+              //if (i >= 2 && fillPoints.get(i - 1) >= 0) {
+              if (i >= 2 && !yOutOfBounds(fillPoints.get(i - 1), fill.getType(), canvas)) {
+System.out.println("[jls] adding previous intermediary");
                 fillPoints.add(i, getXIntermediary(fillPoints.subList(i - 2, i + 2), canvas.getWidth()));
-                fillPoints.add(i + 1, 0f);
+                //fillPoints.add(i + 1, 0f);
+                fillPoints.add(i + 1, boundary);
                 i += 2;
                 length += 2;
                 currentIndex += 2;
@@ -187,13 +198,18 @@ public class LineChart extends XYChart {
               // If there's a subsequent point, and its y is on the screen,
               // add an intermediate point at the intersection of the line
               // segment and the y boundary
-              if (i + 2 < fillPoints.size() && fillPoints.get(i + 3) >= 0) {
+              //if (i + 2 < fillPoints.size() && fillPoints.get(i + 3) >= 0) {
+              if (i + 2 < fillPoints.size() && !yOutOfBounds(fillPoints.get(i + 3), fill.getType(), canvas)) {
+System.out.println("[jls] adding subsequent intermediary");
                 fillPoints.add(i + 2, getXIntermediary(fillPoints.subList(i, i + 4), canvas.getWidth()));
-                fillPoints.add(i + 3, 0f);
+                //fillPoints.add(i + 3, 0f);
+                fillPoints.add(i + 3, boundary);
                 i += 2;
                 length += 2;
               }
-              fillPoints.set(currentIndex + 1, 0f);
+System.out.println("[jls] adjusting current point");
+              //fillPoints.set(currentIndex + 1, 0f);
+              fillPoints.set(currentIndex + 1, boundary);
             }
             i += 2;
           }
@@ -228,6 +244,28 @@ public class LineChart extends XYChart {
     calcX = Math.max(0, calcX);
     calcX = Math.min(calcX, maxX);
     return calcX;
+  }
+  
+  private boolean yOutOfBounds(float y, FillOutsideLine.Type type, Canvas canvas) {
+      if (type == FillOutsideLine.Type.BOUNDS_ALL) {
+        return false;
+        //return ??
+      }
+      if (type == FillOutsideLine.Type.BOUNDS_BELOW) {
+        return false;
+        //return y < yAxisValue;
+      }
+      if (type == FillOutsideLine.Type.BOUNDS_ABOVE) {
+        return false;
+        //return y > yAxisValue;  
+      }
+      if (type == FillOutsideLine.Type.ABOVE) {
+        return y > canvas.getHeight();
+      }
+      if (type == FillOutsideLine.Type.BELOW) {
+        return y < 0;
+      }
+      throw new RuntimeException("Unrecognized fill type in yOutOfBounds");
   }
 
   @Override
