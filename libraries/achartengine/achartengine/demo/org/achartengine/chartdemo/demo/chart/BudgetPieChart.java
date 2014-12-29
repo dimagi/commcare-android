bf0d6e3aba89acf0b8a6c40ad1cf4c9bcdd316e9
Jenny Schweers <jschweers@dimagi.com>
2014-12-29 15:08:47 -0500

    Add AChartEngine, as a separate project

---
+++ libraries/achartengine/achartengine/demo/org/achartengine/chartdemo/demo/chart/BudgetPieChart.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2009 - 2013 SC 4ViewSoft SRL
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.achartengine.chartdemo.demo.chart;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+
+/**
+ * Budget demo pie chart.
+ */
+public class BudgetPieChart extends AbstractDemoChart {
+  /**
+   * Returns the chart name.
+   * 
+   * @return the chart name
+   */
+  public String getName() {
+    return "Budget chart";
+  }
+
+  /**
+   * Returns the chart description.
+   * 
+   * @return the chart description
+   */
+  public String getDesc() {
+    return "The budget per project for this year (pie chart)";
+  }
+
+  /**
+   * Executes the chart demo.
+   * 
+   * @param context the context
+   * @return the built intent
+   */
+  public Intent execute(Context context) {
+    double[] values = new double[] { 12, 14, 11, 10, 19 };
+    int[] colors = new int[] { Color.BLUE, Color.GREEN, Color.MAGENTA, Color.YELLOW, Color.CYAN };
+    DefaultRenderer renderer = buildCategoryRenderer(colors);
+    renderer.setZoomButtonsVisible(true);
+    renderer.setZoomEnabled(true);
+    renderer.setChartTitleTextSize(20);
+    renderer.setDisplayValues(true);
+    renderer.setShowLabels(true);
+    SimpleSeriesRenderer r = renderer.getSeriesRendererAt(0);
+    r.setGradientEnabled(true);
+    r.setGradientStart(0, Color.BLUE);
+    r.setGradientStop(0, Color.GREEN);
+    r.setHighlighted(true);
+    Intent intent = ChartFactory.getPieChartIntent(context,
+        buildCategoryDataset("Project budget", values), renderer, "Budget");
+    return intent;
+  }
+
+}
