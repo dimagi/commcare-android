bc2e101f92fdd40341ef3925230f6895b0854a2c
Daniel Luna <dluna@dimagi.com>
2015-03-30 16:19:32 -0300

    New style for errors in form entry
    Conflicts:
    	app/src/org/odk/collect/android/widgets/QuestionWidget.java

--- app/src/org/odk/collect/android/widgets/QuestionWidget.java
+++ app/src/org/odk/collect/android/widgets/QuestionWidget.java
@@ -205,13 +205,13 @@ public abstract class QuestionWidget extends LinearLayout {
      */
     public void notifyOnScreen(String text, boolean strong, boolean requestFocus){
         if(strong){
-            this.setBackgroundDrawable(this.getContext().getResources().getDrawable(R.drawable.bubble_invalid));
+            this.setBackgroundDrawable(this.getContext().getResources().getDrawable(R.drawable.bubble_invalid_modern));
         } else{
             this.setBackgroundDrawable(this.getContext().getResources().getDrawable(R.drawable.bubble_warn));
         }
 
         if(this.toastView == null) {
-            this.toastView = View.inflate(this.getContext(), R.layout.toast_view, this).findViewById(R.id.toast_view_root);
+            this.toastView = View.inflate(this.getContext(), R.layout.toast_view_modern, this).findViewById(R.id.toast_view_root);
             focusPending = requestFocus;
         } else {
             if(this.toastView.getVisibility() != View.VISIBLE) {
