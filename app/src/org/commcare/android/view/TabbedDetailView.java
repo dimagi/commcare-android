7117a2629ae2028f5b60a9d3fe82e98144036541
Daniel Luna <dluna@dimagi.com>
2015-04-08 19:32:41 -0300

    Changing colors for TabbedDetailView (drawer pulldown)
    Conflicts:
    	app/src/org/commcare/android/view/TabbedDetailView.java

--- app/src/org/commcare/android/view/TabbedDetailView.java
+++ app/src/org/commcare/android/view/TabbedDetailView.java
@@ -1,5 +1,12 @@
 package org.commcare.android.view;
 
+import org.commcare.android.adapters.EntityDetailAdapter;
+import org.commcare.android.adapters.EntityDetailPagerAdapter;
+import org.commcare.dalvik.R;
+import org.commcare.suite.model.Detail;
+import org.commcare.suite.model.DisplayUnit;
+import org.javarosa.core.model.instance.TreeReference;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
@@ -149,7 +156,13 @@ public class TabbedDetailView extends RelativeLayout {
      * Get form list from database and insert into view.
      */
     public void refresh(Detail detail, TreeReference reference, int index, boolean hasDetailCalloutListener) {
-        mEntityDetailPagerAdapter = new EntityDetailPagerAdapter(mContext.getSupportFragmentManager(), detail, index, reference, hasDetailCalloutListener);
+        mEntityDetailPagerAdapter = new EntityDetailPagerAdapter(mContext.getSupportFragmentManager(), detail, index, reference,
+                hasDetailCalloutListener, new EntityDetailAdapter.EntityDetailViewModifier() {
+            @Override
+            public void modifyEntityDetailView(EntityDetailView edv) {
+                edv.setOddEvenRowColors(getResources().getColor(R.color.cc_brand_bg),getResources().getColor(R.color.cc_attention_positive_color));
+            }
+        });
         mViewPager.setAdapter(mEntityDetailPagerAdapter);
         markSelectedTab(0);
     }
