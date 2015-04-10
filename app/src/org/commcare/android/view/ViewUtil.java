09d087ac2a14e6cd5317782eb66e11b683511789
Clayton Sims <csims@dimagi.com>
2015-04-09 20:06:41 -0400

    updated spacing math for txt views and question widgets.
    Two bugs remain: toast view should bleed out to touch the other red border and the whole widget shouldn't be inset
    
    Conflicts:
    	app/res/layout/toast_view_modern.xml
    	app/src/org/commcare/android/view/ViewUtil.java
    	app/src/org/odk/collect/android/widgets/QuestionWidget.java

--- app/src/org/commcare/android/view/ViewUtil.java
+++ app/src/org/commcare/android/view/ViewUtil.java
@@ -3,9 +3,20 @@
  */
 package org.commcare.android.view;
 
+import java.io.File;
+import org.commcare.dalvik.R;
+import org.commcare.suite.model.DisplayUnit;
+import org.javarosa.core.reference.InvalidReferenceException;
+import org.javarosa.core.reference.ReferenceManager;
+import org.javarosa.core.services.locale.Localizer;
+import org.odk.collect.android.utilities.FileUtils;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
@@ -80,5 +91,35 @@ public class ViewUtil {
             }
         }
         return null;
+    }
+
+    public static void hideVirtualKeyboard(Activity activity){
+        InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        View focus = activity.getCurrentFocus();
+        if(focus != null) {
+            inputManager.hideSoftInputFromWindow(focus.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+    
+    /**
+     * Sets the background on a view to the provided drawable while retaining the padding
+     * of the original view (regardless of whether the provided drawable has its own padding)
+     * 
+     * @param v The view whose background will be updated
+     * @param background A background drawable (can be null to clear the background)
+     */
+    @SuppressLint("NewApi")
+    public static void setBackgroundRetainPadding(View v, Drawable background) {
+        //Need to transplant the padding due to background affecting it
+        int[] padding = {v.getPaddingLeft(), v.getPaddingTop(), v.getPaddingRight(),v.getPaddingBottom() };
+        
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+            v.setBackground(background);
+        } else {
+            v.setBackgroundDrawable(background);
+        }
+        v.setPadding(padding[0],padding[1], padding[2], padding[3]);
     }
 }
