812f1b45b594a3337eb1e844acef8a82974bfe9d
Clayton Sims <csims@dimagi.com>
2015-04-21 16:49:13 -0400

    Temporary fix for markdown which retains the video/audio playback, but also doesn't require trailing spaces.

--- app/src/org/commcare/android/util/MarkupUtil.java
+++ app/src/org/commcare/android/util/MarkupUtil.java
@@ -71,9 +71,37 @@ public class MarkupUtil {
 
     public static CharSequence generateMarkdown(Context c, String message){
         Bypass bypass = new Bypass(c);
-        CharSequence mSequence = bypass.markdownToSpannable(convertCharacterEncodings(message));
+        CharSequence mSequence = trimTrailingWhitespace(bypass.markdownToSpannable(convertCharacterEncodings(message)));
         return mSequence;
     }
+    
+    /** Trims trailing whitespace. Removes any of these characters:
+     * 0009, HORIZONTAL TABULATION
+     * 000A, LINE FEED
+     * 000B, VERTICAL TABULATION
+     * 000C, FORM FEED
+     * 000D, CARRIAGE RETURN
+     * 001C, FILE SEPARATOR
+     * 001D, GROUP SEPARATOR
+     * 001E, RECORD SEPARATOR
+     * 001F, UNIT SEPARATOR
+     * @return "" if source is null, otherwise string with all trailing whitespace removed
+     * soruce: http://stackoverflow.com/questions/9589381/remove-extra-line-breaks-after-html-fromhtml
+     */
+    public static CharSequence trimTrailingWhitespace(CharSequence source) {
+
+        if(source == null)
+            return "";
+
+        int i = source.length();
+
+        // loop back to the first non-whitespace character
+        while(--i >= 0 && Character.isWhitespace(source.charAt(i))) {
+        }
+
+        return source.subSequence(0, i+1);
+    }
+
 
     /*
      * Perform conversions from encodings
