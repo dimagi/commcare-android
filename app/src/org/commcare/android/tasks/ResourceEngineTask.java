47089bce6eed1a37916a415f9b2e507466bb03f8
Phillip Mates <pmates@dimagi.com>
2015-05-07 13:29:44 -0400

    reformatting & conditional refactor

--- app/src/org/commcare/android/tasks/ResourceEngineTask.java
+++ app/src/org/commcare/android/tasks/ResourceEngineTask.java
@@ -33,45 +33,69 @@ import android.content.SharedPreferences.Editor;
 import android.os.SystemClock;
 
 /**
- * This task is responsible for
- *
  * @author ctsims
- *
  */
 public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], org.commcare.android.tasks.ResourceEngineTask.ResourceEngineOutcomes, R> implements TableStateListener {
 
     public enum ResourceEngineOutcomes implements MessageTag {
-        /** App installed Succesfully **/
+        /**
+         * App installed Succesfully
+         */
         StatusInstalled("notification.install.installed"),
 
-        /** Missing resources could not be found during install **/
+        /**
+         * Missing resources could not be found during install
+         */
         StatusMissing("notification.install.missing"),
 
-        /** Missing resources could not be found during install **/
+        /**
+         * Missing resources could not be found during install
+         */
         StatusMissingDetails("notification.install.missing.withmessage"),
 
-        /** App is not compatible with current installation **/
+        /**
+         * App is not compatible with current installation
+         */
         StatusBadReqs("notification.install.badreqs"),
 
-        /** Unknown Error **/
+        /**
+         * Unknown Error
+         */
         StatusFailUnknown("notification.install.unknown"),
 
-        /** There's already an app installed **/
+        /**
+         * There's already an app installed
+         */
         StatusFailState("notification.install.badstate"),
 
-        /** There's already an app installed **/
+        /**
+         * There's already an app installed
+         */
         StatusNoLocalStorage("notification.install.nolocal"),
 
-        /** Install is fine **/
+        /**
+         * Install is fine
+         */
         StatusUpToDate("notification.install.uptodate"),
 
-        /** Certificate was bad **/
+        /**
+         * Certificate was bad
+         */
         StatusBadCertificate("notification.install.badcert");
 
-        ResourceEngineOutcomes(String root) {this.root = root;}
+        ResourceEngineOutcomes(String root) {
+            this.root = root;
+        }
+
         private final String root;
-        public String getLocaleKeyBase() { return root;}
-        public String getCategory() { return "install_update"; }
+
+        public String getLocaleKeyBase() {
+            return root;
+        }
+
+        public String getCategory() {
+            return "install_update";
+        }
     }
 
     Context c;
@@ -81,7 +105,10 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
     public static final int PHASE_DOWNLOAD = 1;
     public static final int PHASE_COMMIT = 2;
 
-    public static final long STATUS_UPDATE_WAIT_TIME = 1000; // wait time between dialog updates in milliseconds
+    /**
+     * Wait time between dialog updates in milliseconds
+     */
+    public static final long STATUS_UPDATE_WAIT_TIME = 1000;
 
     protected UnresolvedResourceException missingResourceException = null;
     protected int badReqCode = -1;
@@ -99,8 +126,8 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
     protected boolean majorIsProblem;
 
     public ResourceEngineTask(Context c, boolean upgradeMode, boolean partialMode, CommCareApp app,
-            boolean startOverUpgrade, int taskId, boolean shouldSleep)
-                    throws SessionUnavailableException{
+                              boolean startOverUpgrade, int taskId, boolean shouldSleep)
+            throws SessionUnavailableException {
         this.partialMode = partialMode;
         this.c = c;
         this.upgradeMode = upgradeMode;
@@ -116,20 +143,21 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
     protected ResourceEngineOutcomes doTaskBackground(String... profileRefs) {
         String profileRef = profileRefs[0];
         AndroidCommCarePlatform platform = app.getCommCarePlatform();
-        SharedPreferences prefs =app.getAppPreferences();
+        SharedPreferences prefs = app.getAppPreferences();
 
 
-        //First of all, make sure we record that an attempt was started.
+        // Make sure we record that an attempt was started.
         Editor editor = prefs.edit();
         editor.putLong(CommCarePreferences.LAST_UPDATE_ATTEMPT, new Date().getTime());
         editor.commit();
 
         app.setupSandbox();
 
-        Logger.log(AndroidLogger.TYPE_RESOURCES, "Beginning install attempt for profile " + profileRefs[0]);
+        Logger.log(AndroidLogger.TYPE_RESOURCES,
+                "Beginning install attempt for profile " + profileRefs[0]);
 
         if (shouldSleep) {
-          SystemClock.sleep(2000);
+            SystemClock.sleep(2000);
         }
 
         try {
@@ -139,18 +167,19 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
             // Ok, should figure out what the state of this bad boy is.
             Resource profile = global.getResourceWithId("commcare-application-profile");
 
-            boolean sanityTest1 = (profile != null && profile.getStatus() == Resource.RESOURCE_STATUS_INSTALLED);
+            boolean sanityTest1 = (profile != null &&
+                    profile.getStatus() == Resource.RESOURCE_STATUS_INSTALLED);
 
             if (upgradeMode) {
                 if (!sanityTest1) {
-                  return ResourceEngineOutcomes.StatusFailState;
+                    return ResourceEngineOutcomes.StatusFailState;
                 }
                 global.setStateListener(this);
 
-                /* temporary is the upgrade table, which starts out in the state that it was left
-                 * after the last install- partially populated if it stopped in middle, empty
-                 * if the install was successful
-                 */
+                // temporary is the upgrade table, which starts out in the
+                // state that it was left after the last install- partially
+                // populated if it stopped in middle, empty if the install was
+                // successful
                 ResourceTable temporary = platform.getUpgradeResourceTable();
                 ResourceTable recovery = platform.getRecoveryTable();
                 temporary.setStateListener(this);
@@ -170,7 +199,7 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
                 if (DeveloperPreferences.isNewestAppVersionEnabled() &&
                         (profileUrl != null) &&
                         ("https".equals(profileUrl.getProtocol()) ||
-                         "http".equals(profileUrl.getProtocol()))) {
+                                "http".equals(profileUrl.getProtocol()))) {
                     if (profileUrl.getQuery() != null) {
                         // If the profileRef url already have query strings
                         // just add a new one to the end
@@ -199,14 +228,14 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
                 platform.upgrade(global, temporary, recovery);
 
                 // And see where we ended up to see whether an upgrade actually occurred
-            } else if (partialMode){
+            } else if (partialMode) {
                 global.setStateListener(this);
                 platform.init(profileRef, global, false);
                 app.writeInstalled();
             } else {
                 // this is a standard, clean install
                 if (sanityTest1) {
-                  return ResourceEngineOutcomes.StatusFailState;
+                    return ResourceEngineOutcomes.StatusFailState;
                 }
                 global.setStateListener(this);
 
@@ -218,14 +247,16 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
             // Initialize them now that they're installed
             CommCareApplication._().initializeGlobalResources(app);
 
-            // Alll goood, we need to set our current profile ref to either the one
-            // just used, or the auth ref, if one is available.
 
-            String authRef = platform.getCurrentProfile().getAuthReference() == null ? profileRef : platform.getCurrentProfile().getAuthReference();
-
+            // update the current profile reference
             prefs = app.getAppPreferences();
             Editor edit = prefs.edit();
-            edit.putString("default_app_server", authRef);
+            if (platform.getCurrentProfile().getAuthReference() != null) {
+                edit.putString("default_app_server",
+                        platform.getCurrentProfile().getAuthReference());
+            } else {
+                edit.putString("default_app_server", profileRef);
+            }
             edit.commit();
 
             return ResourceEngineOutcomes.StatusInstalled;
@@ -234,7 +265,8 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
 
             tryToClearApp();
 
-            Logger.log(AndroidLogger.TYPE_ERROR_WORKFLOW, "Couldn't install file to local storage|" + e.getMessage());
+            Logger.log(AndroidLogger.TYPE_ERROR_WORKFLOW,
+                    "Couldn't install file to local storage|" + e.getMessage());
             return ResourceEngineOutcomes.StatusNoLocalStorage;
         } catch (UnfullfilledRequirementsException e) {
             e.printStackTrace();
@@ -246,7 +278,8 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
 
             tryToClearApp();
 
-            Logger.log(AndroidLogger.TYPE_ERROR_WORKFLOW, "App resources are incompatible with this device|" + e.getMessage());
+            Logger.log(AndroidLogger.TYPE_ERROR_WORKFLOW,
+                    "App resources are incompatible with this device|" + e.getMessage());
             return ResourceEngineOutcomes.StatusBadReqs;
         } catch (UnresolvedResourceException e) {
             // couldn't find a resource, which isn't good.
@@ -256,7 +289,7 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
 
             Throwable mExceptionCause = e.getCause();
 
-            if (mExceptionCause instanceof SSLHandshakeException){
+            if (mExceptionCause instanceof SSLHandshakeException) {
                 Throwable mSecondExceptionCause = mExceptionCause.getCause();
                 if (mSecondExceptionCause instanceof CertificateException) {
                     return ResourceEngineOutcomes.StatusBadCertificate;
@@ -264,7 +297,9 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
             }
 
             missingResourceException = e;
-            Logger.log(AndroidLogger.TYPE_WARNING_NETWORK, "A resource couldn't be found, almost certainly due to the network|" + e.getMessage());
+            Logger.log(AndroidLogger.TYPE_WARNING_NETWORK,
+                    "A resource couldn't be found, almost certainly due to the network|" +
+                    e.getMessage());
             if (e.isMessageUseful()) {
                 return ResourceEngineOutcomes.StatusMissingDetails;
             } else {
@@ -275,15 +310,17 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
 
             tryToClearApp();
 
-            Logger.log(AndroidLogger.TYPE_ERROR_WORKFLOW, "Unknown error ocurred during install|" + e.getMessage());
+            Logger.log(AndroidLogger.TYPE_ERROR_WORKFLOW,
+                    "Unknown error ocurred during install|" + e.getMessage());
             return ResourceEngineOutcomes.StatusFailUnknown;
         }
     }
 
     /**
-     *  For now, never clear automatically - just let user choose when to retry vs. resume
+     * For now, never clear automatically - just let user choose when to retry
+     * vs. resume
      */
-    protected void tryToClearApp(){
+    protected void tryToClearApp() {
         //if(partialMode == false && upgradeMode == false){}
     }
 
@@ -310,8 +347,9 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
     long lastTime = 0;
 
     public void resourceStateUpdated(ResourceTable table) {
-        // if last time isn't set or is less than our spacing count, do not perform status update
-        if (System.currentTimeMillis() - lastTime < ResourceEngineTask.STATUS_UPDATE_WAIT_TIME){
+        // if last time isn't set or is less than our spacing count, do not
+        // perform status update
+        if (System.currentTimeMillis() - lastTime < ResourceEngineTask.STATUS_UPDATE_WAIT_TIME) {
             return;
         }
 
@@ -323,20 +361,20 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
 
         for (Resource r : resources) {
             switch (r.getStatus()) {
-            case Resource.RESOURCE_STATUS_UPGRADE:
-                // If we spot an upgrade after we've started the upgrade process,
-                // something now needs to be updated
-                if (phase == PHASE_CHECKING) {
-                    this.phase = PHASE_DOWNLOAD;
-                }
-                score += 1;
-                break;
-            case Resource.RESOURCE_STATUS_INSTALLED:
-                score += 1;
-                break;
-            default:
-                score += 0;
-                break;
+                case Resource.RESOURCE_STATUS_UPGRADE:
+                    // If we spot an upgrade after we've started the upgrade process,
+                    // something now needs to be updated
+                    if (phase == PHASE_CHECKING) {
+                        this.phase = PHASE_DOWNLOAD;
+                    }
+                    score += 1;
+                    break;
+                case Resource.RESOURCE_STATUS_INSTALLED:
+                    score += 1;
+                    break;
+                default:
+                    score += 0;
+                    break;
             }
         }
         lastTime = System.currentTimeMillis();
@@ -344,6 +382,6 @@ public abstract class ResourceEngineTask<R> extends CommCareTask<String, int[], 
     }
 
     public void incrementProgress(int complete, int total) {
-        this.publishProgress(new int[] {complete, total, phase});
+        this.publishProgress(new int[]{complete, total, phase});
     }
 }
