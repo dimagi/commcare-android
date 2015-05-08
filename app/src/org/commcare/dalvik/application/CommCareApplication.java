9a310b07983df1d18e4cf67ba063ffa8a3ee12b9
Phillip Mates <pmates@dimagi.com>
2015-05-07 20:06:55 -0400

    Removing unused class methods

--- app/src/org/commcare/dalvik/application/CommCareApplication.java
+++ app/src/org/commcare/dalvik/application/CommCareApplication.java
@@ -16,7 +16,6 @@ import net.sqlcipher.database.SQLiteException;
 
 import org.commcare.android.database.DbHelper;
 import org.commcare.android.database.SqlStorage;
-import org.commcare.android.database.SqlStorageIterator;
 import org.commcare.android.database.app.models.UserKeyRecord;
 import org.commcare.android.database.global.DatabaseGlobalOpenHelper;
 import org.commcare.android.database.global.models.ApplicationRecord;
@@ -48,7 +47,6 @@ import org.commcare.android.util.SessionUnavailableException;
 import org.commcare.dalvik.R;
 import org.commcare.dalvik.activities.MessageActivity;
 import org.commcare.dalvik.activities.UnrecoverableErrorActivity;
-import org.commcare.dalvik.odk.provider.InstanceProviderAPI.InstanceColumns;
 import org.commcare.dalvik.preferences.CommCarePreferences;
 import org.commcare.dalvik.services.CommCareSessionService;
 import org.commcare.suite.model.Profile;
@@ -79,8 +77,6 @@ import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -278,27 +274,8 @@ public class CommCareApplication extends Application {
         tManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
     }
 
-    private void detachCallListener() {
-        if (listener != null) {
-            TelephonyManager tManager = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
-            tManager.listen(listener, PhoneStateListener.LISTEN_NONE);
-            listener = null;
-        }
-    }
-
     public CallInPhoneListener getCallListener() {
         return listener;
-    }
-
-
-    public int versionCode() {
-        try {
-            PackageManager pm = this.getPackageManager();
-            PackageInfo pi = pm.getPackageInfo(getPackageName(), 0);
-            return pi.versionCode;
-        } catch (NameNotFoundException e) {
-            throw new RuntimeException("Android package name not available.");
-        }
     }
 
     public int[] getCommCareVersion() {
@@ -516,45 +493,6 @@ public class CommCareApplication extends Application {
     }
 
     /**
-     * This method goes through and identifies whether there are elements in the
-     * database which point to/expect files to exist on the file system, and clears
-     * out any records which refer to files that don't exist.
-     */
-    public void cleanUpDatabaseFileLinkages() throws SessionUnavailableException {
-        Vector<Integer> toDelete = new Vector<Integer>();
-
-        SqlStorage<FormRecord> storage = getUserStorage(FormRecord.class);
-
-        //Can't load the records outright, since we'd need to be logged in (The key is encrypted)
-        for (SqlStorageIterator iterator = storage.iterate(); iterator.hasMore(); ) {
-            int id = iterator.nextID();
-            String instanceRecordUri = storage.getMetaDataFieldForRecord(id, FormRecord.META_INSTANCE_URI);
-            if (instanceRecordUri == null) {
-                toDelete.add(id);
-                continue;
-            }
-
-            //otherwise, grab this record and see if the file's around
-
-            Cursor c = this.getContentResolver().query(Uri.parse(instanceRecordUri), new String[]{InstanceColumns.INSTANCE_FILE_PATH}, null, null, null);
-            if (!c.moveToFirst()) {
-                toDelete.add(id);
-            } else {
-                String path = c.getString(c.getColumnIndex(InstanceColumns.INSTANCE_FILE_PATH));
-                if (path == null || !new File(path).exists()) {
-                    toDelete.add(id);
-                }
-            }
-            c.close();
-        }
-
-        for (int recordid : toDelete) {
-            //this should go to the form record wipe cleanup task
-            storage.remove(recordid);
-        }
-    }
-
-    /**
      * This method wipes out all local user data (users, referrals, etc) but leaves
      * application resources in place.
      *
@@ -722,7 +660,7 @@ public class CommCareApplication extends Application {
                         //Register that this user was the last to successfully log in if it's a real user
                         if (!User.TYPE_DEMO.equals(user.getUserType())) {
                             getCurrentApp().getAppPreferences().edit().putString(CommCarePreferences.LAST_LOGGED_IN_USER, record.getUsername()).commit();
-                            performArchivedFormPurge(getCurrentApp(), user);
+                            performArchivedFormPurge(getCurrentApp());
                         }
                     }
                 }
@@ -801,9 +739,8 @@ public class CommCareApplication extends Application {
      * from the device.
      *
      * @param app  The current app
-     * @param user The user who's storage we're reviewing
      */
-    private void performArchivedFormPurge(CommCareApp app, User user) {
+    private void performArchivedFormPurge(CommCareApp app) {
         int daysForReview = -1;
         String daysToPurge = app.getAppPreferences().getString("cc-days-form-retain", "-1");
         try {
@@ -1174,7 +1111,7 @@ public class CommCareApplication extends Application {
         /**
          * Pops up the message to the user by way of toast
          *
-         * @param message Has a 'message' parcel storing pop-up message text
+         * @param m Has a 'message' parcel storing pop-up message text
          */
         @Override
         public void handleMessage(Message m) {
