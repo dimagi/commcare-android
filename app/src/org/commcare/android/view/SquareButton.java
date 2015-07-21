43c12afa4b93a6e61890f5580a64281d3c03aa7e
Will Pride <wpride@dimagi.om>
2015-07-21 13:24:32 -0400

    merge

---
+++ app/src/org/commcare/android/view/SquareButton.java
@@ -0,0 +1,35 @@
+package org.commcare.android.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+
+/**
+ * Created by dancluna on 3/14/15.
+ */
+public class SquareButton extends ImageButton {
+    public SquareButton(Context context) {
+        super(context);
+    }
+
+    public SquareButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        //Get canvas width and height
+        int w = MeasureSpec.getSize(widthMeasureSpec);
+        int h = MeasureSpec.getSize(heightMeasureSpec);
+
+        w = Math.min(w, h);
+        h = w;
+
+        setMeasuredDimension(w, h);
+    }
+}
