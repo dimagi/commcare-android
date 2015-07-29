5ea4069e269a9f26b6d20170c53cbf1cba1a28cd
Phillip Mates <pmates@dimagi.com>
2015-07-29 17:08:54 -0400

    rename ResourceEngineListener.updateProgress

--- app/src/org/commcare/android/tasks/ResourceEngineListener.java
+++ app/src/org/commcare/android/tasks/ResourceEngineListener.java
@@ -8,6 +8,6 @@ public interface ResourceEngineListener {
     public void failMissingResource(UnresolvedResourceException ure, ResourceEngineOutcomes statusmissing);
     public void failBadReqs(int code, String vReq, String vAvail, boolean majorIsProblem);
     public void failUnknown(ResourceEngineOutcomes statusfailunknown);
-    public void updateProgress(int done, int pending, int phase);
+    public void updateResourceProgress(int done, int pending, int phase);
     public void failWithNotification(ResourceEngineOutcomes statusfailstate);
 }
