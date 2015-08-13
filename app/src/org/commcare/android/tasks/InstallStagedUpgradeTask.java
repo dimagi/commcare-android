3a2c6d5a514d40ff4192371fef337209a535594e
Phillip Mates <pmates@dimagi.com>
2015-08-13 16:55:51 -0400

    Task to install a downloaded upgrade

---
+++ app/src/org/commcare/android/tasks/InstallStagedUpgradeTask.java
@@ -0,0 +1,48 @@
+package org.commcare.android.tasks;
+
+import org.commcare.android.tasks.templates.CommCareTask;
+import org.commcare.android.util.AndroidCommCarePlatform;
+import org.commcare.android.util.InstallAndUpdateUtils;
+import org.commcare.dalvik.application.CommCareApp;
+import org.commcare.dalvik.application.CommCareApplication;
+import org.commcare.resources.model.ResourceTable;
+import org.commcare.resources.model.UnresolvedResourceException;
+import org.commcare.util.CommCareResourceManager;
+
+/**
+ * @author Phillip Mates (pmates@dimagi.com)
+ */
+public abstract class InstallStagedUpgradeTask<R>
+        extends CommCareTask<String, int[], ResourceEngineOutcomes, R> {
+
+    public InstallStagedUpgradeTask(int taskId) {
+        this.taskId = taskId;
+        TAG = InstallStagedUpgradeTask.class.getSimpleName();
+    }
+
+    @Override
+    protected ResourceEngineOutcomes doTaskBackground(String... profileRefs) {
+        CommCareApp app = CommCareApplication._().getCurrentApp();
+        app.setupSandbox();
+
+        AndroidCommCarePlatform platform = app.getCommCarePlatform();
+        ResourceTable global = platform.getGlobalResourceTable();
+        ResourceTable temporary = platform.getUpgradeResourceTable();
+        ResourceTable recovery = platform.getRecoveryTable();
+        CommCareResourceManager resourceManager =
+            new CommCareResourceManager(platform, global, temporary, recovery);
+
+        if (!CommCareResourceManager.isUpgradeStaged(temporary)) {
+            return ResourceEngineOutcomes.StatusFailState;
+        }
+
+        try {
+            resourceManager.upgrade();
+        } catch (UnresolvedResourceException e) {
+        }
+        // TODO PLM
+        String profileRef = null;
+        InstallAndUpdateUtils.initAndCommitApp(app, profileRef);
+        return ResourceEngineOutcomes.StatusInstalled;
+    }
+}
