5722d28fda05a23d9db508667a26a55699947c68
Jenny Schweers <jschweers@dimagi.com>
2015-08-25 11:59:42 -0400

    first draft

---
+++ app/src/org/commcare/android/adapters/EntityReferencesAdapter.java
@@ -0,0 +1,100 @@
+package org.commcare.android.adapters;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+
+import org.commcare.android.models.Entity;
+import org.commcare.android.models.NodeEntityFactory;
+import org.commcare.android.view.EntityView;
+import org.commcare.suite.model.Detail;
+import org.javarosa.core.model.instance.TreeReference;
+
+import java.util.Vector;
+
+/**
+ * Created by jschweers on 8/24/2015.
+ */
+public class EntityReferencesAdapter implements ListAdapter {
+
+    private Context context;
+    private Detail detail;
+    private NodeEntityFactory factory;
+    private Vector<TreeReference> references;
+
+    public EntityReferencesAdapter(Context context, Detail detail, NodeEntityFactory factory, Vector<TreeReference> references) {
+        this.context = context;
+        this.detail = detail;
+        this.factory = factory;
+        this.references = references;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    @Override
+    public int getCount() {
+        return references.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return references.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO: display headers
+        EntityView view = (EntityView) convertView;
+        Entity entity = factory.getEntity(references.get(position));
+        if (view == null) {
+            view = new EntityView(context, detail, entity, null, null, position, false);
+        } else {
+            view.refreshViewsForNewEntity(entity, false, position);
+        }
+        return view;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return getCount() > 0;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+    }
+
+}
--- app/src/org/commcare/android/framework/EntityDetailFragment.java
+++ app/src/org/commcare/android/framework/EntityDetailFragment.java
@@ -7,43 +7,56 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import org.commcare.android.adapters.EntityDetailAdapter;
+import org.commcare.android.adapters.EntityListAdapter;
+import org.commcare.android.adapters.EntityReferencesAdapter;
 import org.commcare.android.models.AndroidSessionWrapper;
 import org.commcare.android.models.Entity;
 import org.commcare.android.models.NodeEntityFactory;
+import org.commcare.android.tasks.EntityLoaderListener;
+import org.commcare.android.tasks.EntityLoaderTask;
+import org.commcare.android.util.AndroidUtil;
 import org.commcare.android.util.DetailCalloutListener;
 import org.commcare.android.util.SerializationUtil;
+import org.commcare.android.view.EntityView;
 import org.commcare.dalvik.R;
 import org.commcare.dalvik.application.CommCareApplication;
 import org.commcare.suite.model.Detail;
 import org.javarosa.core.model.instance.TreeReference;
 
+import java.util.List;
+import java.util.Vector;
+
 /**
  * Fragment to display Detail content. Not meant for handling nested Detail objects.
- * @author jschweers
  *
+ * @author jschweers
  */
-public class EntityDetailFragment extends Fragment {
+public class EntityDetailFragment extends Fragment implements EntityLoaderListener {
     public static final String CHILD_DETAIL_INDEX = "edf_child_detail_index";
     public static final String DETAIL_ID = "edf_detail_id";
     public static final String DETAIL_INDEX = "edf_detail_index";
     public static final String CHILD_REFERENCE = "edf_detail_reference";
 
     private AndroidSessionWrapper asw;
-    private EntityDetailAdapter adapter;
+    private ListAdapter adapter;
     private EntityDetailAdapter.EntityDetailViewModifier modifier;
+
+    private EntityLoaderTask loader;
 
     public EntityDetailFragment() {
         super();
         this.asw = CommCareApplication._().getCurrentSessionWrapper();
     }
 
-    public void setEntityDetailModifier(EntityDetailAdapter.EntityDetailViewModifier edvm){
+    public void setEntityDetailModifier(EntityDetailAdapter.EntityDetailViewModifier edvm) {
         this.modifier = edvm;
-        if(adapter != null) {
-            adapter.setModifier(edvm);
+        if (adapter != null) {
+            ((EntityDetailAdapter) adapter).setModifier(edvm);
         }
     }
 
@@ -52,8 +65,8 @@ public class EntityDetailFragment extends Fragment {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if(modifier instanceof Parcelable) {
-            outState.putParcelable(MODIFIER_KEY, (Parcelable)modifier);
+        if (modifier instanceof Parcelable) {
+            outState.putParcelable(MODIFIER_KEY, (Parcelable) modifier);
         } else {
             throw new IllegalArgumentException(modifier + " must implement Parcelable!");
         }
@@ -61,7 +74,7 @@ public class EntityDetailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             this.modifier = savedInstanceState.getParcelable(MODIFIER_KEY);
         }
 
@@ -84,16 +97,68 @@ public class EntityDetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.entity_detail_list, container, false);
         final Activity thisActivity = getActivity();
         final DetailCalloutListener detailCalloutListener =
-                thisActivity instanceof DetailCalloutListener ? ((DetailCalloutListener)thisActivity) : null;
+                thisActivity instanceof DetailCalloutListener ? ((DetailCalloutListener) thisActivity) : null;
 
         final ListView listView = ((ListView) rootView.findViewById(R.id.screen_entity_detail_list));
-        adapter = new EntityDetailAdapter(
-            thisActivity, asw.getSession(), childDetail, entity, 
-            detailCalloutListener, args.getInt(DETAIL_INDEX)
-        );
-        adapter.setModifier(modifier);
+        final LinearLayout headerLayout = ((LinearLayout) rootView.findViewById(R.id.entity_detail_header));
+        if (childDetail.getNodeset() != null && !"".equals(childDetail.getNodeset())) {
+            TreeReference nodeset = childDetail.getNodeset();
+            TreeReference contextualizedNodeset = nodeset.contextualize(SerializationUtil.deserializeFromBundle(args, CHILD_REFERENCE, TreeReference.class));
+            Vector<TreeReference> references = asw.getEvaluationContext().expandReference(contextualizedNodeset);
+            adapter = new EntityReferencesAdapter(thisActivity, childDetail, factory, references);
+
+            // stolen from EntitySelectActivity, make DRYer
+            String[] headers = new String[childDetail.getFields().length];
+            for (int i = 0; i < headers.length; ++i) {
+                headers[i] = childDetail.getFields()[i].getHeader().evaluate();
+            }
+            int[] colors = AndroidUtil.getThemeColorIDs(thisActivity, new int[]{R.attr.entity_view_header_background_color, R.attr.entity_view_header_text_color});
+            EntityView headerView = new EntityView(thisActivity, childDetail, headers, colors[1]);
+            headerView.setBackgroundColor(colors[0]);
+            headerLayout.removeAllViews();
+            headerLayout.addView(headerView);
+            headerLayout.setVisibility(View.VISIBLE);
+        } else {
+            Vector<TreeReference> references = null;
+            adapter = new EntityDetailAdapter(
+                    thisActivity, asw.getSession(), childDetail, entity,
+                    detailCalloutListener, args.getInt(DETAIL_INDEX)
+            );
+            ((EntityDetailAdapter) adapter).setModifier(modifier);
+            headerLayout.setVisibility(View.GONE);
+        }
         listView.setAdapter(adapter);
         return rootView;
     }
 
+    @Override
+    public void attach(EntityLoaderTask task) {
+        this.loader = task;
+    }
+
+    @Override
+    public void deliverResult(List<Entity<TreeReference>> entities, List<TreeReference> references, NodeEntityFactory factory) {
+        Bundle args = getArguments();
+        final Detail detail = asw.getSession().getDetail(args.getString(DETAIL_ID));
+        Detail childDetail = detail;
+        final int thisIndex = args.getInt(CHILD_DETAIL_INDEX, -1);
+        final boolean detailCompound = thisIndex != -1;
+        if (detailCompound) {
+            childDetail = detail.getDetails()[thisIndex];
+        }
+
+        loader = null;
+        int[] order = childDetail.getSortOrder();
+
+        //theloader.execute(childDetail.getNodeset());
+
+        System.out.println("[jls] # of references: " + references.size());
+        System.out.println("[jls] # of entities: " + entities.size());
+        adapter = new EntityListAdapter(getActivity(), childDetail, references, entities, order, null, factory);
+    }
+
+    @Override
+    public void deliverError(Exception e) {
+        ((CommCareActivity) getActivity()).displayException(e);
+    }
 }
