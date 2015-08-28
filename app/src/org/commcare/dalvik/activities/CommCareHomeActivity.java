c3f47e0a054b48c5e94d5cd8feb64ba3f030f58e
ALIZA STONE <amstone326@gmail.com>
2015-08-28 14:51:00 -0400

    clean up

--- app/src/org/commcare/dalvik/activities/CommCareHomeActivity.java
+++ app/src/org/commcare/dalvik/activities/CommCareHomeActivity.java
@@ -9,8 +9,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
