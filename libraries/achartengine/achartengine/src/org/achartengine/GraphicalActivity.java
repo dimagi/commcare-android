464e269a626a7c9d36e4138af8dd1c638c6e3444
Jenny Schweers <jschweers@dimagi.com>
2015-09-01 09:59:16 -0400

    PR feedback

--- libraries/achartengine/achartengine/src/org/achartengine/GraphicalActivity.java
+++ libraries/achartengine/achartengine/src/org/achartengine/GraphicalActivity.java
@@ -1,12 +1,9 @@
 /**
  * Copyright (C) 2009 - 2013 SC 4ViewSoft SRL
- * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -60,13 +57,11 @@ public class GraphicalActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                this.onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            this.onBackPressed();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
 }
