79161957ce080a2aa94a95ba2d322d2f95c77d0b
Phillip Mates <pmates@dimagi.com>
2015-09-09 09:21:16 -0400

    lint and format CommCareExceptionHandler

--- app/src/org/commcare/android/util/CommCareExceptionHandler.java
+++ app/src/org/commcare/android/util/CommCareExceptionHandler.java
@@ -5,22 +5,21 @@ import org.commcare.android.tasks.ExceptionReportTask;
 import java.lang.Thread.UncaughtExceptionHandler;
 
 /**
- * TODO: This class is basically just for testing, it should be rewritten for 
- * completeness and usefulness.
- * 
  * @author ctsims
  */
 public class CommCareExceptionHandler implements UncaughtExceptionHandler {
 
-    UncaughtExceptionHandler parent;
-    
+    private final UncaughtExceptionHandler parent;
+
     public CommCareExceptionHandler(UncaughtExceptionHandler parent) {
         this.parent = parent;
     }
+
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
         ExceptionReportTask task = new ExceptionReportTask();
         task.execute(ex);
+       
         parent.uncaughtException(thread, ex);
     }
 }
