d7728d8ae7a19722e2fc1fbe9bf0635312481936
Will Pride <wpride@dimagi.om>
2015-09-14 17:23:44 -0400

    cherry pick fix

---
+++ build/generated/source/buildConfig/androidTest/debug/org/commcare/dalvik/test/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.commcare.dalvik.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.commcare.dalvik.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+  // Fields from default config.
+  public static final String ACRA_PASSWORD = "";
+  public static final String ACRA_URL = "";
+  public static final String ACRA_USER = "";
+  public static final String BUILD_DATE = "2015-09-14";
+  public static final String BUILD_NUMBER = "-1";
+  public static final String MAPS_API_KEY = "";
+}
