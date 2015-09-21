8c026a20bd7ee0f8a5c12766dd0376b088b32358
Phillip Mates <pmates@dimagi.com>
2015-09-21 12:58:59 -0400

    Remove debug logging, linting, whitespace

--- app/src/org/odk/collect/android/views/HierarchyElementView.java
+++ app/src/org/odk/collect/android/views/HierarchyElementView.java
@@ -1,42 +1,23 @@
-/*
- * Copyright (C) 2009 University of Washington
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package org.odk.collect.android.views;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import org.commcare.dalvik.BuildConfig;
 import org.commcare.dalvik.R;
 import org.odk.collect.android.logic.HierarchyElement;
 
 public class HierarchyElementView extends RelativeLayout {
-    public static final String TAG = HierarchyElementView.class.getSimpleName();
-
-    private TextView mPrimaryTextView;
-    private TextView mSecondaryTextView;
-    private ImageView mIcon;
-
+    private final TextView mPrimaryTextView;
+    private final TextView mSecondaryTextView;
+    private final ImageView mIcon;
 
     public HierarchyElementView(Context context, HierarchyElement it) {
         super(context);
 
-        RelativeLayout layout = (RelativeLayout) inflate(context, R.layout.hierarchy_element_view, null);
+        RelativeLayout layout = (RelativeLayout)inflate(context, R.layout.hierarchy_element_view, null);
 
         layout.setBackgroundColor(it.getColor());
 
@@ -48,11 +29,6 @@ public class HierarchyElementView extends RelativeLayout {
         mIcon.setImageDrawable(it.getIcon());
 
         addView(layout);
-
-        if (BuildConfig.DEBUG) {
-            Log.i(TAG, "Type of HEV (" + hashCode() + ") is " + it.getType());
-            Log.i(TAG, "Icon of HEV (" + hashCode() + ") is " + (it.getIcon() == null ? "null" : it.getIcon().toString()));
-        }
     }
 
 
@@ -60,36 +36,29 @@ public class HierarchyElementView extends RelativeLayout {
         mPrimaryTextView.setText(text);
     }
 
-
     public void setSecondaryText(String text) {
         mSecondaryTextView.setText(text);
     }
-
 
     public void setIcon(Drawable icon) {
         mIcon.setImageDrawable(icon);
     }
 
-
     public void setColor(int color) {
         setBackgroundColor(color);
     }
-
 
     public void showSecondary(boolean bool) {
         if (bool) {
             mSecondaryTextView.setVisibility(VISIBLE);
             setMinimumHeight(dipToPx(64));
-
         } else {
             mSecondaryTextView.setVisibility(GONE);
             setMinimumHeight(dipToPx(32));
-
         }
     }
-    
-    public int dipToPx(int dip) {
-        return (int) (dip * getResources().getDisplayMetrics().density + 0.5f);
-    }
 
+    private int dipToPx(int dip) {
+        return (int)(dip * getResources().getDisplayMetrics().density + 0.5f);
+    }
 }
