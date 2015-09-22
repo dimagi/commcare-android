bc036ad7deccb058f2e6bd23bfaf9580c8713f70
Phillip Mates <pmates@dimagi.com>
2015-09-22 17:14:46 -0400

    App install working, now onto user restore

--- unit-tests/src/org/commcare/android/tests/initialization/AppInitializationTest.java
+++ unit-tests/src/org/commcare/android/tests/initialization/AppInitializationTest.java
@@ -3,22 +3,24 @@ package org.commcare.android.tests.initialization;
 import android.app.Application;
 
 import org.commcare.android.CommCareTestRunner;
+import org.commcare.android.database.DbUtil;
 import org.commcare.android.database.global.models.ApplicationRecord;
 import org.commcare.android.mocks.CommCareTaskConnectorFake;
+import org.commcare.android.tasks.DataPullTask;
 import org.commcare.android.tasks.ResourceEngineTask;
+import org.commcare.android.util.LivePrototypeFactory;
 import org.commcare.dalvik.BuildConfig;
 import org.commcare.dalvik.application.CommCareApp;
+import org.javarosa.core.reference.ReferenceManager;
+import org.javarosa.core.reference.ResourceReferenceFactory;
 import org.javarosa.core.util.PropertyUtils;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowApplication;
-import org.robolectric.shadows.ShadowLooper;
-
-import java.io.File;
 
 /**
  * @author Phillip Mates (pmates@dimagi.com).
@@ -31,16 +33,21 @@ public class AppInitializationTest {
     public void setup() {
         Robolectric.getBackgroundThreadScheduler().pause();
         Robolectric.getForegroundThreadScheduler().pause();
+
+        ReferenceManager._().addReferenceFactory(new ResourceReferenceFactory());
+
+        // Sets DB to use an in-memory store for class serialization tagging.
+        // This avoids the need to use apk reflection to perform read/writes
+        LivePrototypeFactory prototypeFactory = new LivePrototypeFactory();
+        PrototypeFactory.setStaticHasher(prototypeFactory);
+        DbUtil.setDBUtilsPrototypeFactory(prototypeFactory);
+
+        installApp();
+
+        restoreUser();
     }
 
-    @Test
-    public void installApp() {
-        // Robolectric.getBackgroundScheduler().pause();
-        // Robolectric.getUiThreadScheduler().pause();
-
-        String filepath = "resources/commcare-apps/flipper/profile.ccpr";
-        String cczFilePath =
-            ("jr://resource/" + filepath);
+    private void installApp() {
         ApplicationRecord newRecord =
                 new ApplicationRecord(PropertyUtils.genUUID().replace("-", ""),
                         ApplicationRecord.STATUS_UNINITIALIZED);
@@ -68,18 +75,42 @@ public class AppInitializationTest {
                     }
                 };
         task.connect(new CommCareTaskConnectorFake<>());
-        task.execute(cczFilePath);
+
+        String filepath = "/commcare-apps/flipper/profile.ccpr";
+        String resourceFilepath = "jr://resource" + filepath;
+        task.execute(resourceFilepath);
+
         Robolectric.flushBackgroundThreadScheduler();
         Robolectric.flushForegroundThreadScheduler();
-        ShadowApplication.runBackgroundTasks();
-        ShadowLooper.runUiThreadTasks();
-        System.out.print("bar");
+    }
+
+    private void restoreUser() {
+        String username = "";
+        String password = "";
+        String restoreServer = "";
+        String keyProvider = "";
+        Application appContext = RuntimeEnvironment.application;
+        DataPullTask<Object> dataPuller =
+                new DataPullTask<Object>(username, password, restoreServer,
+                        keyProvider, appContext) {
+                    @Override
+                    protected void deliverResult(Object receiver, Integer result) {
+                    }
+
+                    @Override
+                    protected void deliverUpdate(Object receiver, Integer... update) {
+                    }
+
+                    @Override
+                    protected void deliverError(Object receiver, Exception e) {
+                    }
+                };
+
+        dataPuller.connect(new CommCareTaskConnectorFake<>());
+        dataPuller.execute();
     }
 
     @Test
-    public void testAppInit() {
-        Application a = RuntimeEnvironment.application;
-        System.out.print(a.getPackageName());
-        //initFirstUsableAppRecord();
+    public void verifyApp() {
     }
 }
