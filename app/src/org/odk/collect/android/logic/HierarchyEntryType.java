ac7e98b6107d7cc01a1021ef556b9a9a2852eb3d
Phillip Mates <pmates@dimagi.com>
2015-09-24 17:30:56 -0400

    Renaming, comments, and reordering of methods

--- app/src/org/odk/collect/android/logic/HierarchyEntryType.java
+++ app/src/org/odk/collect/android/logic/HierarchyEntryType.java
@@ -1,6 +1,8 @@
 package org.odk.collect.android.logic;
 
 /**
+ * Types of entries for the form hierarchy view
+ *
  * @author Phillip Mates (pmates@dimagi.com).
  */
 public enum HierarchyEntryType {
