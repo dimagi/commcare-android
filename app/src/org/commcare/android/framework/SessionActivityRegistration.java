c0cbad87deb97336d06687872584b393829512b4
Phillip Mates <pmates@dimagi.com>
2015-09-30 11:41:33 -0400

    Don't fail hard when unregistering a receiver that wasn't previously registered

--- app/src/org/commcare/android/framework/SessionActivityRegistration.java
+++ app/src/org/commcare/android/framework/SessionActivityRegistration.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 
 import org.commcare.dalvik.activities.CommCareHomeActivity;
 
@@ -14,6 +15,8 @@ import org.commcare.dalvik.activities.CommCareHomeActivity;
  * @author Phillip Mates (pmates@dimagi.com)
  */
 public class SessionActivityRegistration {
+    private static final String TAG = SessionActivityRegistration.class.getSimpleName();
+
     public static final String USER_SESSION_EXPIRED =
             "org.commcare.dalvik.application.user_session_expired";
 
@@ -52,7 +55,12 @@ public class SessionActivityRegistration {
      * this method in onPause methods of activities that are session sensitive.
      */
     public static void unregisterSessionExpirationReceiver(Activity activity) {
-        activity.unregisterReceiver(userSessionExpiredReceiver);
+        try {
+            activity.unregisterReceiver(userSessionExpiredReceiver);
+        } catch (IllegalArgumentException e) {
+            Log.w(TAG, "Trying to unregister the session expiration receiver " +
+                    "that wasn't previously registerd.");
+        }
     }
 
     /**
