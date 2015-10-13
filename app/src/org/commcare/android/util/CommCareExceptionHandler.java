2f3a36ec9ee3d40207f6bb6aa37624b9ed6e57d8
Phillip Mates <pmates@dimagi.com>
2015-10-12 20:14:17 -0400

    Show user when app crashes due to XPathTypeMismatchException

--- app/src/org/commcare/android/util/CommCareExceptionHandler.java
+++ app/src/org/commcare/android/util/CommCareExceptionHandler.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import org.commcare.android.tasks.ExceptionReportTask;
 import org.commcare.dalvik.activities.CrashWarningActivity;
 import org.javarosa.core.util.NoLocalizedTextException;
+import org.javarosa.xpath.XPathTypeMismatchException;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 
@@ -33,9 +34,8 @@ public class CommCareExceptionHandler implements UncaughtExceptionHandler {
         ExceptionReportTask task = new ExceptionReportTask();
         task.execute(ex);
 
-        if (warnUserAndExit(ex)) {
-            // You must close the crashed thread in order to start a new activity.
-            System.exit(0);
+        if (isUserCreatedCrash(ex)) {
+            warnUserAndExit(ex);
         } else {
             // handle error normally (report to ACRA/play store)
             parent.uncaughtException(thread, ex);
@@ -44,17 +44,20 @@ public class CommCareExceptionHandler implements UncaughtExceptionHandler {
 
     /**
      * Launch activity showing user details of the crash if it is something
-     * they can fix.
+     * they can fix then exit.
      */
-    private boolean warnUserAndExit(Throwable ex) {
-        if (ex instanceof NoLocalizedTextException) {
-            Intent i = new Intent(ctx, CrashWarningActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            i.putExtra(WARNING_MESSAGE_KEY, ex.getMessage());
-            ctx.startActivity(i);
-            return true;
-        }
-        return false;
+    private void warnUserAndExit(Throwable ex) {
+        Intent i = new Intent(ctx, CrashWarningActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.putExtra(WARNING_MESSAGE_KEY, ex.getMessage());
+        ctx.startActivity(i);
+        // You must close the crashed thread in order to start a new activity.
+        System.exit(0);
+    }
+
+    private boolean isUserCreatedCrash(Throwable ex) {
+        return (ex instanceof NoLocalizedTextException ||
+                ex instanceof XPathTypeMismatchException);
     }
 }
