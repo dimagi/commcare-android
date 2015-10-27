d786081dd6f16768655cbe1efa0de35782e6f4d9
Phillip Mates <pmates@dimagi.com>
2015-10-27 18:25:01 -0400

    Prevent infinite loop if form just has 1 empty group

--- app/src/org/odk/collect/android/activities/FormEntryActivity.java
+++ app/src/org/odk/collect/android/activities/FormEntryActivity.java
@@ -966,7 +966,7 @@ public class FormEntryActivity extends CommCareActivity<FormEntryActivity>
                     case FormEntryController.EVENT_END_OF_FORM:
                         Logger.log(AndroidLogger.SOFT_ASSERT,
                                 "Trying to show an end of form event");
-                        showPreviousView(false);
+                        CommCareActivity.createErrorDialog(this, "Unexpectedly reached the end of the form.", EXIT);
                         break group_skip;
                     case FormEntryController.EVENT_PROMPT_NEW_REPEAT:
                         createRepeatDialog();
