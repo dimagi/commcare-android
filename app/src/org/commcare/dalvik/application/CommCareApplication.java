0e4d8c2ab4ba029384a1dded1ec07b2f8cdfd8d0
Phillip Mates <pmates@dimagi.com>
2015-11-13 15:25:23 -0500

    remove unused import

--- app/src/org/commcare/dalvik/application/CommCareApplication.java
+++ app/src/org/commcare/dalvik/application/CommCareApplication.java
@@ -102,7 +102,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.Vector;
 
