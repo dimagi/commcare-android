4799c9ff7046f2513d0d2c7586f6f89ff2bd803c
Phillip Mates <pmates@dimagi.com>
2015-11-29 13:53:26 -0500

    Set card padding

--- app/src/org/commcare/android/adapters/HomeScreenAdapter.java
+++ app/src/org/commcare/android/adapters/HomeScreenAdapter.java
@@ -9,7 +9,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.StateSet;
 import android.view.LayoutInflater;
 import android.view.View;
