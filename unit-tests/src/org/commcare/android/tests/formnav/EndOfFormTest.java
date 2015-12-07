844f89c539d295a4726b2d02a93764dd2db84580
Phillip Mates <pmates@dimagi.com>
2015-12-07 08:06:07 -0500

    Fake form process/send, fix task test launches

--- unit-tests/src/org/commcare/android/tests/formnav/EndOfFormTest.java
+++ unit-tests/src/org/commcare/android/tests/formnav/EndOfFormTest.java
@@ -1,14 +1,14 @@
 package org.commcare.android.tests.formnav;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Environment;
-import android.view.View;
+import android.util.Log;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 import org.commcare.android.CommCareTestRunner;
+import org.commcare.android.database.SqlStorage;
+import org.commcare.android.database.user.models.FormRecord;
+import org.commcare.android.mocks.FormAndDataSyncerFake;
 import org.commcare.android.models.AndroidSessionWrapper;
 import org.commcare.android.util.TestAppInstaller;
 import org.commcare.android.util.TestUtils;
@@ -18,37 +18,32 @@ import org.commcare.dalvik.activities.CommCareHomeActivity;
 import org.commcare.dalvik.application.CommCareApplication;
 import org.commcare.session.CommCareSession;
 import org.commcare.session.SessionNavigator;
-import org.javarosa.core.model.FormIndex;
-import org.javarosa.core.model.data.IAnswerData;
 import org.javarosa.core.reference.ReferenceManager;
 import org.javarosa.core.reference.ResourceReferenceFactory;
-import org.javarosa.form.api.FormEntryController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.odk.collect.android.activities.FormEntryActivity;
 import org.odk.collect.android.views.ODKView;
 import org.odk.collect.android.widgets.IntegerWidget;
-import org.odk.collect.android.widgets.QuestionWidget;
 import org.robolectric.Robolectric;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowEnvironment;
-import org.robolectric.util.ActivityController;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 /**
  * @author Phillip Mates (pmates@dimagi.com).
  */
-@Config(application = org.commcare.dalvik.application.CommCareApplication.class,
+@Config(application = CommCareApplication.class,
         constants = BuildConfig.class)
 @RunWith(CommCareTestRunner.class)
 public class EndOfFormTest {
+
+    private static final String TAG = EndOfFormTest.class.getSimpleName();
 
     @Before
     public void setup() {
@@ -65,38 +60,84 @@ public class EndOfFormTest {
         ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
     }
 
+    /**
+     * Test filling out and saving a form ending in a hidden repeat group. This
+     * type of form exercises uncommon end-of-form code paths
+     */
     @Test
     public void testHiddenRepeatAtEndOfForm() {
-        AndroidSessionWrapper sessionWrapper = CommCareApplication._().getCurrentSessionWrapper();
-        CommCareSession session = sessionWrapper.getSession();
-        session.setCommand("m0-f0");
-        CommCareHomeActivity homeActivity = Robolectric.buildActivity(CommCareHomeActivity.class).create().get();
-        SessionNavigator sessionNavigator = homeActivity.getSessionNavigator();
-        sessionNavigator.startNextSessionStep();
+        CommCareHomeActivity homeActivity = buildHomeActivityForFormEntryLaunch();
 
         ShadowActivity shadowActivity = Shadows.shadowOf(homeActivity);
         Intent formEntryIntent = shadowActivity.getNextStartedActivity();
 
-        // make sure the form entry activity was launched
-        assertTrue(formEntryIntent.getComponent().getClassName().equals(FormEntryActivity.class.getName()));
+        // make sure the form entry activity should be launched
+        String intentActivityName = formEntryIntent.getComponent().getClassName();
+        assertTrue(intentActivityName.equals(FormEntryActivity.class.getName()));
 
-        ActivityController<FormEntryActivity> controller = Robolectric.buildActivity(FormEntryActivity.class).withIntent(formEntryIntent).create().start();
-        FormEntryActivity formEntryActivity = controller.get();
-        controller.resume();
-        Robolectric.flushBackgroundThreadScheduler();
-        Robolectric.flushForegroundThreadScheduler();
+        ShadowActivity shadowFormEntryActivity = navigateFormEntry(formEntryIntent);
+
+        // trigger CommCareHomeActivity.onActivityResult for the completion of
+        // FormEntryActivity
+        shadowActivity.receiveResult(formEntryIntent,
+                shadowFormEntryActivity.getResultCode(),
+                shadowFormEntryActivity.getResultIntent());
+        assertStoredFroms();
+    }
+
+    private CommCareHomeActivity buildHomeActivityForFormEntryLaunch() {
+        AndroidSessionWrapper sessionWrapper =
+                CommCareApplication._().getCurrentSessionWrapper();
+        CommCareSession session = sessionWrapper.getSession();
+        session.setCommand("m0-f0");
+
+        CommCareHomeActivity homeActivity =
+                Robolectric.buildActivity(CommCareHomeActivity.class).create().get();
+        // make sure we don't actually submit forms by using a fake form submitter
+        homeActivity.setFormAndDataSyncer(new FormAndDataSyncerFake(homeActivity));
+        SessionNavigator sessionNavigator = homeActivity.getSessionNavigator();
+        sessionNavigator.startNextSessionStep();
+        return homeActivity;
+    }
+
+    private ShadowActivity navigateFormEntry(Intent formEntryIntent) {
+        // launch form entry
+        FormEntryActivity formEntryActivity =
+                Robolectric.buildActivity(FormEntryActivity.class).withIntent(formEntryIntent)
+                        .create().start().resume().get();
+
         ImageButton nextButton = (ImageButton)formEntryActivity.findViewById(R.id.nav_btn_next);
+        // demonstrate form progress bug where it thinks the form is done until
+        // you enter an answer for the question, which effects the hidden
+        // repeat coming up next
         assertTrue(nextButton.getTag().equals(FormEntryActivity.NAV_STATE_DONE));
+
+        // enter an answer for the question
         ODKView odkView = formEntryActivity.getODKView();
         IntegerWidget favoriteNumber = (IntegerWidget)odkView.getWidgets().get(0);
         favoriteNumber.setAnswer("2");
         assertTrue(nextButton.getTag().equals(FormEntryActivity.NAV_STATE_NEXT));
+        // Finish off the form even by clicking next.
+        // The form progress meter thinks there is more to do, but that is a bug.
         nextButton.performClick();
-        Robolectric.flushBackgroundThreadScheduler();
-        Robolectric.flushForegroundThreadScheduler();
-        int currentEvent = FormEntryActivity.mFormController.getEvent();
-        System.out.print("foo");
-        assertTrue(currentEvent == FormEntryController.EVENT_END_OF_FORM);
-        System.out.print("foo");
+
+        ShadowActivity shadowFormEntryActivity = Shadows.shadowOf(formEntryActivity);
+        while (!shadowFormEntryActivity.isFinishing()) {
+            Log.d(TAG, "Waiting for the form to save and the form entry activity to finish");
+        }
+
+        return shadowFormEntryActivity;
+    }
+
+    private void assertStoredFroms() {
+        SqlStorage<FormRecord> formsStorage =
+                CommCareApplication._().getUserStorage(FormRecord.class);
+
+        int unsentForms = formsStorage.getIDsForValue(FormRecord.META_STATUS,
+                FormRecord.STATUS_UNSENT).size();
+        int incompleteForms = formsStorage.getIDsForValue(FormRecord.META_STATUS,
+                FormRecord.STATUS_INCOMPLETE).size();
+        assertEquals("There should be a single form waiting to be sent", 1, unsentForms);
+        assertEquals("There shouldn't be any forms saved as incomplete", 0, incompleteForms);
     }
 }
