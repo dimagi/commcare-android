71389b17097ae6e43df7970d5080bc6da3980be3
Phillip Mates <pmates@dimagi.com>
2015-12-29 13:28:17 -0500

    Lint markup util and downgrade htmlcleaner to old version

--- app/src/org/commcare/android/util/Stylizer.java
+++ app/src/org/commcare/android/util/Stylizer.java
@@ -6,25 +6,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Stylizer {
-    private HashMap<String, String> globalStyleTable;
     private String globalStyleString;
 
     public Stylizer(Context c) {
-
         globalStyleString = "";
 
-        if (globalStyleTable == null) {
-            globalStyleTable = new HashMap<String, String>();
-        }
-
-        ArrayList<String> mStyles = new ArrayList<String>();
+        ArrayList<String> mStyles = new ArrayList<>();
 
         try {
             BufferedReader bReader = new BufferedReader(new InputStreamReader(c.getAssets().open("app_styles.txt")));
-            ArrayList<String> values = new ArrayList<String>();
+            ArrayList<String> values = new ArrayList<>();
             String line = bReader.readLine();
             while (line != null) {
                 values.add(line);
@@ -42,7 +35,6 @@ public class Stylizer {
             String style = mStyles.get(i);
             String key = style.substring(0, style.indexOf("="));
             String val = style.substring(style.indexOf('=') + 1);
-            globalStyleTable.put(key, val);
             globalStyleString += MarkupUtil.formatKeyVal(key, val);
         }
     }
