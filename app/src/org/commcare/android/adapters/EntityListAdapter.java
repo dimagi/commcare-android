49e4187a2deeab02cd89963837b59138d3d1607c
Phillip Mates <pmates@dimagi.com>
2016-01-28 10:22:19 -0500

    Move entity search nested class to dedicated file

--- app/src/org/commcare/android/adapters/EntityListAdapter.java
+++ app/src/org/commcare/android/adapters/EntityListAdapter.java
@@ -3,12 +3,9 @@ package org.commcare.android.adapters;
 import android.app.Activity;
 import android.database.DataSetObserver;
 import android.speech.tts.TextToSpeech;
-import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
-
-import net.sqlcipher.database.SQLiteDatabase;
 
 import org.commcare.android.models.AsyncNodeEntityFactory;
 import org.commcare.android.models.Entity;
@@ -17,7 +14,6 @@ import org.commcare.android.models.notifications.NotificationMessageFactory;
 import org.commcare.android.models.notifications.NotificationMessageFactory.StockMessages;
 import org.commcare.android.util.AndroidUtil;
 import org.commcare.android.util.CachingAsyncImageLoader;
-import org.commcare.android.util.SessionUnavailableException;
 import org.commcare.android.util.StringUtils;
 import org.commcare.android.view.EntityView;
 import org.commcare.android.view.GridEntityView;
@@ -29,15 +25,12 @@ import org.commcare.suite.model.Detail;
 import org.commcare.suite.model.DetailField;
 import org.javarosa.core.model.Constants;
 import org.javarosa.core.model.instance.TreeReference;
-import org.javarosa.core.services.Logger;
 import org.javarosa.xpath.XPathTypeMismatchException;
 import org.javarosa.xpath.expr.XPathFuncExpr;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * This adapter class handles displaying the cases for a CommCareODK user.
@@ -130,11 +123,15 @@ public class EntityListAdapter implements ListAdapter {
     /**
      * Set the current display set for this adapter
      */
-    private void setCurrent(List<Entity<TreeReference>> arrayList) {
+    void setCurrent(List<Entity<TreeReference>> arrayList) {
         current = arrayList;
         if (actionEnabled) {
             actionPosition = current.size();
         }
+    }
+
+    void setCurrentSearchTerms(String[] searchTerms) {
+        currentSearchTerms = searchTerms;
     }
 
     private void filterValues(String filterRaw) {
@@ -146,150 +143,8 @@ public class EntityListAdapter implements ListAdapter {
             for (int i = 0; i < searchTerms.length; ++i) {
                 searchTerms[i] = StringUtils.normalize(searchTerms[i]);
             }
-            mCurrentSortThread = new EntitySearcher(filterRaw, searchTerms);
+            mCurrentSortThread = new EntitySearcher(this, filterRaw, searchTerms, mAsyncMode, mFuzzySearchEnabled, mNodeFactory, full, context);
             mCurrentSortThread.startThread();
-        }
-    }
-
-    private class EntitySearcher {
-        private final String filterRaw;
-        private final String[] searchTerms;
-        final List<Entity<TreeReference>> matchList;
-        //Ugh, annoying.
-        final ArrayList<Pair<Integer, Integer>> matchScores;
-        private boolean cancelled = false;
-        Thread thread;
-
-        public EntitySearcher(String filterRaw, String[] searchTerms) {
-            this.filterRaw = filterRaw;
-            this.searchTerms = searchTerms;
-            matchList = new ArrayList<>();
-            matchScores = new ArrayList<>();
-        }
-
-        public void startThread() {
-            thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    //Make sure that we have loaded the necessary cached data
-                    //before we attempt to search over it
-                    while (!mNodeFactory.isEntitySetReady()) {
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                    }
-                    search();
-                }
-            });
-            thread.start();
-        }
-
-        public void finish() {
-            this.cancelled = true;
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        private void search() {
-            Locale currentLocale = Locale.getDefault();
-
-            long startTime = System.currentTimeMillis();
-            //It's a bit sketchy here, because this DB lock will prevent
-            //anything else from processing
-            SQLiteDatabase db;
-            try {
-                db = CommCareApplication._().getUserDbHandle();
-            } catch (SessionUnavailableException e) {
-                this.finish();
-                return;
-            }
-            db.beginTransaction();
-            for (int index = 0; index < full.size(); ++index) {
-                //Every once and a while we should make sure we're not blocking anything with the database
-                if (index % 500 == 0) {
-                    db.yieldIfContendedSafely();
-                }
-                Entity<TreeReference> e = full.get(index);
-                if (cancelled) {
-                    break;
-                }
-                if ("".equals(filterRaw)) {
-                    matchList.add(e);
-                    continue;
-                }
-
-                boolean add = false;
-                int score = 0;
-                filter:
-                for (String filter : searchTerms) {
-                    add = false;
-                    for (int i = 0; i < e.getNumFields(); ++i) {
-                        String field = e.getNormalizedField(i);
-                        if (!"".equals(field) && field.toLowerCase(currentLocale).contains(filter)) {
-                            add = true;
-                            continue filter;
-                        } else {
-                            // We possibly now want to test for edit distance for
-                            // fuzzy matching
-                            if (mFuzzySearchEnabled) {
-                                for (String fieldChunk : e.getSortFieldPieces(i)) {
-                                    Pair<Boolean, Integer> match = StringUtils.fuzzyMatch(filter, fieldChunk);
-                                    if (match.first) {
-                                        add = true;
-                                        score += match.second;
-                                        continue filter;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                if (add) {
-                    //matchList.add(e);
-                    matchScores.add(Pair.create(index, score));
-                }
-            }
-            if (mAsyncMode) {
-                Collections.sort(matchScores, new Comparator<Pair<Integer, Integer>>() {
-                    @Override
-                    public int compare(Pair<Integer, Integer> lhs, Pair<Integer, Integer> rhs) {
-                        return lhs.second - rhs.second;
-                    }
-                });
-            }
-
-            for (Pair<Integer, Integer> match : matchScores) {
-                matchList.add(full.get(match.first));
-            }
-
-            db.setTransactionSuccessful();
-            db.endTransaction();
-            if (cancelled) {
-                return;
-            }
-
-            long time = System.currentTimeMillis() - startTime;
-            if (time > 1000) {
-                Logger.log("cache", "Presumably finished caching new entities, time taken: " + time + "ms");
-            }
-
-            context.runOnUiThread(new Runnable() {
-
-                @Override
-                public void run() {
-                    setCurrent(matchList);
-                    currentSearchTerms = searchTerms;
-                    update();
-                }
-
-            });
         }
     }
 
@@ -521,7 +376,7 @@ public class EntityListAdapter implements ListAdapter {
         filterValues(s);
     }
 
-    private void update() {
+    void update() {
         for (DataSetObserver o : observers) {
             o.onChanged();
         }
