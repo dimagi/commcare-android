6e033b0c577d26dccab42d244db7273ac8680792
Phillip Mates <pmates@dimagi.com>
2016-02-03 17:30:55 -0500

    Add back in cond/break

--- app/src/org/commcare/android/adapters/EntitySearcher.java
+++ app/src/org/commcare/android/adapters/EntitySearcher.java
@@ -131,6 +131,9 @@ public class EntitySearcher {
                         }
                     }
                 }
+                if (!add) {
+                    break;
+                }
             }
             if (add) {
                 //matchList.add(e);
