8c365f395f401bad23f77cb0a9633e122a2d0ceb
ALIZA STONE <amstone326@gmail.com>
2016-02-15 15:32:35 -0500

    remove unnecessary fields from ForceCloseLogEntry; implement ForceCloseLogSerializer

--- app/src/org/commcare/android/logging/ForceCloseLogEntry.java
+++ app/src/org/commcare/android/logging/ForceCloseLogEntry.java
@@ -17,10 +17,9 @@ import java.util.Date;
  */
 public class ForceCloseLogEntry extends AndroidLogEntry {
 
+    public static final String STORAGE_KEY = "forcecloses";
+
     private int appBuildNumber;
-    private String username;
-    private String ccVersionString;
-    private String domain;
     private String androidVersion;
     private String deviceModel;
     private String readableSessionString;
@@ -36,13 +35,30 @@ public class ForceCloseLogEntry extends AndroidLogEntry {
     public ForceCloseLogEntry(String stackTrace) {
         super(AndroidLogger.TYPE_FORCECLOSE, stackTrace, new Date());
         appBuildNumber = ReportingUtils.getAppBuildNumber();
-        username = ReportingUtils.getUser();
-        ccVersionString = ReportingUtils.getVersion();
-        domain = ReportingUtils.getDomain();
         androidVersion = Build.VERSION.RELEASE;
         deviceModel = Build.MODEL;
         readableSessionString = ReportingUtils.getCurrentSession();
         serializedSessionString = DevSessionRestorer.getSerializedSessionString();
+    }
+
+    public int getAppBuildNumber() {
+        return appBuildNumber;
+    }
+
+    public String getAndroidVersion() {
+        return androidVersion;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public String getReadableSession() {
+        return readableSessionString;
+    }
+
+    public String getSerializedSessionString() {
+        return serializedSessionString;
     }
 
     @Override
@@ -50,9 +66,6 @@ public class ForceCloseLogEntry extends AndroidLogEntry {
             throws IOException, DeserializationException {
         super.readExternal(in, pf);
         appBuildNumber = ExtUtil.readInt(in);
-        username = ExtUtil.readString(in);
-        ccVersionString = ExtUtil.readString(in);
-        domain = ExtUtil.readString(in);
         androidVersion = ExtUtil.readString(in);
         deviceModel = ExtUtil.readString(in);
         readableSessionString = ExtUtil.readString(in);
@@ -63,9 +76,6 @@ public class ForceCloseLogEntry extends AndroidLogEntry {
     public void writeExternal(DataOutputStream out) throws IOException {
         super.writeExternal(out);
         ExtUtil.writeNumeric(out, appBuildNumber);
-        ExtUtil.writeString(out, username);
-        ExtUtil.writeString(out, ccVersionString);
-        ExtUtil.writeString(out, domain);
         ExtUtil.writeString(out, androidVersion);
         ExtUtil.writeString(out, deviceModel);
         ExtUtil.writeString(out, readableSessionString);
--- app/src/org/commcare/android/logging/ForceCloseLogSerializer.java
+++ app/src/org/commcare/android/logging/ForceCloseLogSerializer.java
@@ -3,30 +3,98 @@ package org.commcare.android.logging;
 import org.commcare.android.database.SqlStorage;
 import org.javarosa.core.log.LogEntry;
 import org.javarosa.core.log.StreamLogSerializer;
+import org.javarosa.core.model.utils.DateUtils;
+import org.javarosa.core.services.storage.EntityFilter;
+import org.javarosa.core.util.SortedIntSet;
 import org.xmlpull.v1.XmlSerializer;
 
 import java.io.IOException;
+import java.util.Hashtable;
 
 /**
- * Created by amstone326 on 2/11/16.
+ * Convert xpath error logs to xml.
+ *
+ * @author Aliza Stone
  */
 public class ForceCloseLogSerializer extends StreamLogSerializer implements DeviceReportElement {
 
-    public ForceCloseLogSerializer(ForceCloseLogEntry entry) {
+    private ForceCloseLogEntry singleEntry;
+    private SqlStorage<ForceCloseLogEntry> logStorage;
+    private XmlSerializer serializer;
 
+    public ForceCloseLogSerializer(ForceCloseLogEntry entry) {
+        this.singleEntry = entry;
     }
 
     public ForceCloseLogSerializer(final SqlStorage<ForceCloseLogEntry> logStorage) {
+        this.logStorage = logStorage;
 
+        this.setPurger(new Purger() {
+            @Override
+            public void purge(final SortedIntSet IDs) {
+                logStorage.removeAll(new EntityFilter<LogEntry>() {
+                    public int preFilter(int id, Hashtable<String, Object> metaData) {
+                        return IDs.contains(id) ? PREFILTER_INCLUDE : PREFILTER_EXCLUDE;
+                    }
+
+                    public boolean matches(LogEntry e) {
+                        throw new RuntimeException("can't happen");
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public void writeToDeviceReport(XmlSerializer serializer) throws IOException {
+        this.serializer = serializer;
 
+        serializer.startTag(DeviceReportWriter.XMLNS, "force_close_subreport");
+        try {
+            if (singleEntry != null) {
+                serializeLog(singleEntry.getID(), singleEntry);
+            } else {
+                for (ForceCloseLogEntry entry : logStorage) {
+                    serializeLog(entry.getID(), entry);
+                }
+            }
+        } finally {
+            serializer.endTag(DeviceReportWriter.XMLNS, "force_close_subreport");
+        }
     }
 
     @Override
     protected void serializeLog(LogEntry entry) throws IOException {
+        final ForceCloseLogEntry forceCloseEntry = (ForceCloseLogEntry)entry;
+        String dateString =
+                DateUtils.formatDateTime(forceCloseEntry.getTime(), DateUtils.FORMAT_ISO8601);
 
+        serializer.startTag(DeviceReportWriter.XMLNS, "force_close");
+        try {
+            serializer.attribute(null, "date", dateString);
+            writeText("type", forceCloseEntry.getType());
+            writeText("msg", forceCloseEntry.getMessage());
+            writeText("build_number", forceCloseEntry.getAppBuildNumber() + "");
+            writeText("android_version", forceCloseEntry.getAndroidVersion());
+            writeText("device_model", forceCloseEntry.getDeviceModel());
+            writeText("session_readable", forceCloseEntry.getReadableSession());
+            writeText("session_serialized", forceCloseEntry.getSerializedSessionString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            serializer.endTag(DeviceReportWriter.XMLNS, "force_close");
+        }
+    }
+
+    private void writeText(String element, String text)
+            throws IllegalArgumentException, IllegalStateException, IOException {
+        serializer.startTag(DeviceReportWriter.XMLNS, element);
+        try {
+            serializer.text(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            serializer.endTag(DeviceReportWriter.XMLNS, element);
+        }
     }
 }
