4b0c84e702858faaf87ea085ac3497cffe8d44f3
Phillip Mates <pmates@dimagi.com>
2016-02-25 16:37:56 -0500

    Remember when task was cancelled and show in dialog text

--- app/src/org/commcare/dalvik/dialogs/CustomProgressDialog.java
+++ app/src/org/commcare/dalvik/dialogs/CustomProgressDialog.java
@@ -1,16 +1,5 @@
 package org.commcare.dalvik.dialogs;
 
-/**
- * @author amstone326
- *
- * Any progress dialog associated with a CommCareTask should use
- * this class to implement the dialog. Any class that launches such a task
- * should implement the generateProgressDialog() method of the DialogController
- * interface, and create the dialog in that method. The rest of the dialog's
- * lifecycle is handled by methods of the DialogController interface that are
- * fully implemented in CommCareActivity
- */
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -27,7 +16,18 @@ import android.widget.TextView;
 
 import org.commcare.android.framework.CommCareActivity;
 import org.commcare.dalvik.R;
+import org.javarosa.core.services.locale.Localization;
 
+/**
+ * Any progress dialog associated with a CommCareTask should use
+ * this class to implement the dialog. Any class that launches such a task
+ * should implement the generateProgressDialog() method of the DialogController
+ * interface, and create the dialog in that method. The rest of the dialog's
+ * lifecycle is handled by methods of the DialogController interface that are
+ * fully implemented in CommCareActivity
+ *
+ * @author amstone326
+ */
 public class CustomProgressDialog extends DialogFragment {
 
     //keys for onSaveInstanceState
@@ -39,9 +39,12 @@ public class CustomProgressDialog extends DialogFragment {
     private final static String KEY_USING_BUTTON = "using_cancel_buton";
     private final static String KEY_TASK_ID = "task_id";
     private final static String KEY_CANCELABLE = "is_cancelable";
+    private final static String KEY_WAS_CANCEL_PRESSED = "was_cancel_pressed";
     private final static String KEY_USING_PROGRESS_BAR = "using_progress_bar";
     private final static String KEY_PROGRESS_BAR_PROGRESS = "progress_bar_progress";
     private final static String KEY_PROGRESS_BAR_MAX = "progress_bar_max";
+
+    private final static String cancellingPrefixText = Localization.get("activity.task.cancelling");
 
     //id of the task that spawned this dialog, -1 if not associated with a CommCareTask
     private int taskId;
@@ -50,6 +53,7 @@ public class CustomProgressDialog extends DialogFragment {
     private String title;
     private String message;
     private boolean isCancelable; //default is false, only set to true if setCancelable() is explicitly called
+    private boolean wasCancelPressed;
 
     //for checkboxes
     private boolean usingCheckbox;
@@ -66,9 +70,9 @@ public class CustomProgressDialog extends DialogFragment {
 
     public static CustomProgressDialog newInstance(String title, String message, int taskId) {
         CustomProgressDialog frag = new CustomProgressDialog();
-        frag.setTitle(title);
-        frag.setMessage(message);
-        frag.setTaskId(taskId);
+        frag.title = title;
+        frag.message = message;
+        frag.taskId = taskId;
         return frag;
     }
 
@@ -92,20 +96,8 @@ public class CustomProgressDialog extends DialogFragment {
         this.progressBarMax = 0;
     }
 
-    private void setTaskId(int id) {
-        this.taskId = id;
-    }
-
     public int getTaskId() {
         return taskId;
-    }
-
-    private void setTitle(String s) {
-        this.title = s;
-    }
-
-    private void setMessage(String s) {
-        this.message = s;
     }
 
     public boolean isChecked() {
@@ -128,6 +120,7 @@ public class CustomProgressDialog extends DialogFragment {
             this.usingCancelButton = savedInstanceState.getBoolean(KEY_USING_BUTTON);
             this.taskId = savedInstanceState.getInt(KEY_TASK_ID);
             this.isCancelable = savedInstanceState.getBoolean(KEY_CANCELABLE);
+            this.wasCancelPressed = savedInstanceState.getBoolean(KEY_WAS_CANCEL_PRESSED);
             this.usingHorizontalProgressBar = savedInstanceState.getBoolean(KEY_USING_PROGRESS_BAR);
             this.progressBarProgress = savedInstanceState.getInt(KEY_PROGRESS_BAR_PROGRESS);
             this.progressBarMax = savedInstanceState.getInt(KEY_PROGRESS_BAR_MAX);
@@ -167,6 +160,9 @@ public class CustomProgressDialog extends DialogFragment {
 
         if (usingCancelButton) {
             setupCancelButton(view);
+            if (wasCancelPressed) {
+                setCancellingText(titleView, messageView);
+            }
         }
 
         builder.setView(view);
@@ -207,10 +203,27 @@ public class CustomProgressDialog extends DialogFragment {
             @Override
             public void onClick(View v) {
                 ((CommCareActivity)getActivity()).cancelCurrentTask();
+                showCancelledState();
             }
 
         });
         b.setVisibility(View.VISIBLE);
+    }
+
+    private void showCancelledState() {
+        wasCancelPressed = true;
+
+        AlertDialog pd = (AlertDialog)getDialog();
+        if (pd != null) {
+            TextView titleView = (TextView)pd.findViewById(R.id.progress_dialog_title).findViewById(R.id.dialog_title_text);
+            TextView messageView = (TextView)pd.findViewById(R.id.progress_dialog_message);
+            setCancellingText(titleView, messageView);
+        }
+    }
+
+    private void setCancellingText(TextView titleTextView, TextView messageTextView) {
+        titleTextView.setText(cancellingPrefixText + " " + title);
+        messageTextView.setText(cancellingPrefixText + "...");
     }
 
     public void updateMessage(String text) {
@@ -233,6 +246,7 @@ public class CustomProgressDialog extends DialogFragment {
         outState.putBoolean(KEY_USING_BUTTON, this.usingCancelButton);
         outState.putInt(KEY_TASK_ID, this.taskId);
         outState.putBoolean(KEY_CANCELABLE, this.isCancelable);
+        outState.putBoolean(KEY_WAS_CANCEL_PRESSED, this.wasCancelPressed);
         outState.putBoolean(KEY_USING_PROGRESS_BAR, this.usingHorizontalProgressBar);
         outState.putInt(KEY_PROGRESS_BAR_PROGRESS, this.progressBarProgress);
         outState.putInt(KEY_PROGRESS_BAR_MAX, this.progressBarMax);
