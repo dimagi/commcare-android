5b712bc629f347aef6c57a6b35879fbf10b1fac8
Jennifer Schweers <orange.jenny@gmail.com>
2016-03-11 01:44:20 -0500

    add test

---
+++ unit-tests/src/org/commcare/android/tests/GraphTest.java
@@ -0,0 +1,33 @@
+package org.commcare.android.tests;
+
+import org.commcare.suite.model.graph.Graph;
+import org.commcare.suite.model.graph.XYSeries;
+import org.javarosa.core.model.instance.TreeReference;
+
+@Config(application = CommCareApplication.class,
+        constants = BuildConfig.class)
+@RunWith(CommCareTestRunner.class)
+public class GraphTest extends XPathEvalTest {
+
+    @Test
+    private void testSeriesNodeSetExpansion() {
+        EvaluationContext ec = getFunctionHandlers();
+        FormInstance instance = createTestInstance();
+
+        addDataRef(instance, "/data/flag", new IntegerData(2));
+        addDataRef(instance, "/data/three[1]", new StringData("alpha"));
+        addDataRef(instance, "/data/three[2]", new StringData("beta"));
+        addDataRef(instance, "/data/three[3]", new StringData("delta"));
+        addDataRef(instance, "/data/two[0]", new StringData("omega"));
+        addDataRef(instance, "/data/two[1]", new StringData("psi"));
+
+        XYSeries pathSeries = new XYSeries("/data/two");
+        Vector<TreeReference> pathNodes = Graph.expandNodeSet(pathSeries, ec);
+        Assert.assertEquals(pathNodes.length, 2);
+
+        XYSeries expressionSeries = new XYSeries("if(/data/flag mod 2 = 1, /data/three, /data/two)");
+        Vector<TreeReference> expressionNodes = Graph.expandNodeSet(expressionSeries, ec);
+        Assert.assertEquals(expressionNodes.length(), 3);
+    }
+
+}
