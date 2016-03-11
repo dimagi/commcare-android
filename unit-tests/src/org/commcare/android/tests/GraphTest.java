f9d215048be2256016c47b950b22ddf81caa928a
Jennifer Schweers <orange.jenny@gmail.com>
2016-03-11 02:09:19 -0500

    fix imports

--- unit-tests/src/org/commcare/android/tests/GraphTest.java
+++ unit-tests/src/org/commcare/android/tests/GraphTest.java
@@ -4,6 +4,20 @@ import org.commcare.suite.model.graph.Graph;
 import org.commcare.suite.model.graph.XYSeries;
 import org.javarosa.core.model.instance.TreeReference;
 
+import org.javarosa.core.model.data.IntegerData;
+import org.javarosa.core.model.data.StringData;
+import org.javarosa.core.model.condition.EvaluationContext;
+import org.javarosa.core.model.instance.FormInstance;
+
+import org.commcare.CommCareApplication;
+import org.commcare.android.CommCareTestRunner;
+import org.commcare.dalvik.BuildConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+
 @Config(application = CommCareApplication.class,
         constants = BuildConfig.class)
 @RunWith(CommCareTestRunner.class)
