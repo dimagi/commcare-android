425426fbf042f77080060db4d57368ace26c2b35
Phillip Mates <pmates@dimagi.com>
2016-03-12 23:30:38 -0500

    Linting IntentWidget

--- app/src/org/commcare/views/widgets/IntentWidget.java
+++ app/src/org/commcare/views/widgets/IntentWidget.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2009 University of Washington
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package org.commcare.views.widgets;
 
 import android.app.Activity;
@@ -95,7 +81,7 @@ public class IntentWidget extends QuestionWidget {
         }
     }
 
-    public Spannable getButtonLabel(){
+    private Spannable getButtonLabel() {
         if (prompt.getAnswerText() == null) {
             if (ic.getButtonLabel() != null) {
                 return new SpannableString(ic.getButtonLabel());
@@ -135,7 +121,7 @@ public class IntentWidget extends QuestionWidget {
         try {
             //Set Data
             String data = mStringAnswer.getText().toString();
-            if (data != null && !"".equals(data)) {
+            if (!"".equals(data)) {
                 intent.putExtra(IntentCallout.INTENT_RESULT_VALUE, data);
             }
             ((Activity)getContext()).startActivityForResult(intent, calloutId);
@@ -150,18 +136,16 @@ public class IntentWidget extends QuestionWidget {
         launchIntentButton.setText(getButtonLabel());
     }
 
-
     @Override
     public void clearAnswer() {
         mStringAnswer.setText(null);
         setButtonLabel();
     }
 
-
     @Override
     public IAnswerData getAnswer() {
         String s = mStringAnswer.getText().toString();
-        if (s == null || s.equals("")) {
+        if (s.equals("")) {
             return null;
         } else {
             return new StringData(s);
