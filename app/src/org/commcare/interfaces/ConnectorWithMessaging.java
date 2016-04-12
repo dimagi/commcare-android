ba3bb4bf98e3e3ab0542884565da146fc8800776
Phillip Mates <pmates@dimagi.com>
2016-04-12 13:55:47 -0400

    comment

--- app/src/org/commcare/interfaces/ConnectorWithMessaging.java
+++ app/src/org/commcare/interfaces/ConnectorWithMessaging.java
@@ -4,10 +4,14 @@ import org.commcare.tasks.templates.CommCareTaskConnector;
 import org.commcare.views.dialogs.DialogController;
 
 /**
+ * Expose message reporting to blocking task receivers
+ *
  * @author Phillip Mates (pmates@dimagi.com)
  */
 public interface ConnectorWithMessaging<R> extends DialogController, CommCareTaskConnector<R> {
     void displayMessage(String message);
+
     void displayBadMessage(String message);
+
     void displayBadMessageWithoutToast(String message);
 }
