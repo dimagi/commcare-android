a61d6428e014bbf2cfca2388b562ff1ac16e4a4e
Phillip Mates <pmates@dimagi.com>
2016-04-12 11:27:50 -0400

    New http generator

---
+++ app/src/org/commcare/network/ModernHttpGenerator.java
@@ -0,0 +1,12 @@
+package org.commcare.network;
+
+/**
+ * @author Phillip Mates (pmates@dimagi.com)
+ */
+public class ModernHttpGenerator {
+    // 200s: success
+    // 401 : auth failed
+    // 500 : server error
+    // 412 : recovery?
+    // 301 : redirect
+}
