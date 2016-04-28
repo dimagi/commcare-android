23dd083bdb8ffa9889e72223269f762b8f42fb7b
Phillip Mates <pmates@dimagi.com>
2016-04-28 17:18:43 -0400

    refactor query test file

--- unit-tests/src/org/commcare/android/tests/activities/QueryRequestActivityTest.java
+++ unit-tests/src/org/commcare/android/tests/activities/QueryRequestActivityTest.java
@@ -2,7 +2,6 @@ package org.commcare.android.tests.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,17 +11,14 @@ import android.widget.TextView;
 
 import org.commcare.CommCareApplication;
 import org.commcare.CommCareTestApplication;
-import org.commcare.activities.PostRequestActivity;
 import org.commcare.activities.QueryRequestActivity;
 import org.commcare.android.CommCareTestRunner;
 import org.commcare.android.mocks.ModernHttpRequesterMock;
-import org.commcare.android.util.ActivityLaunchUtils;
 import org.commcare.android.util.TestAppInstaller;
 import org.commcare.dalvik.BuildConfig;
 import org.commcare.dalvik.R;
 import org.commcare.models.AndroidSessionWrapper;
 import org.commcare.session.CommCareSession;
-import org.commcare.tasks.network.DebugDataPullResponseFactory;
 import org.javarosa.core.services.locale.Localization;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,10 +27,7 @@ import org.robolectric.Robolectric;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.util.ActivityController;
-
-import java.util.Hashtable;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -51,39 +44,47 @@ public class QueryRequestActivityTest {
         TestAppInstaller.installAppAndLogin(
                 "jr://resource/commcare-apps/case_search_and_claim/profile.ccpr",
                 "test", "123");
+
+        setSessionCommand("patient-search");
     }
 
+    /**
+     * Launch the query activity when a query datum isn't at the next needed
+     * datum in the session
+     */
     @Test
     public void launchQueryActivityAtWrongTimeTest() {
         Intent queryActivityIntent =
                 new Intent(RuntimeEnvironment.application, QueryRequestActivity.class);
         QueryRequestActivity queryRequestActivity =
-                Robolectric.buildActivity(QueryRequestActivity.class).withIntent(queryActivityIntent)
-                        .create().start().resume().get();
+                Robolectric.buildActivity(QueryRequestActivity.class)
+                        .withIntent(queryActivityIntent).setup().get();
 
         assertEquals(Activity.RESULT_CANCELED,
                 Shadows.shadowOf(queryRequestActivity).getResultCode());
         assertTrue(queryRequestActivity.isFinishing());
     }
 
+    /**
+     * Launch query activity, fill out query prompts, make request, and store
+     * result in session.  Checks request URL is built correctly.
+     */
     @Test
     public void makeSuccessfulQueryRequestTest() {
         ModernHttpRequesterMock.setResponseCodes(new Integer[]{200});
-        ModernHttpRequesterMock.setExpectedUrls(new String[]{"https://www.fake.com/patient_search/?patient_id=123&name=francisco&device_id=000000000000000"});
-        ModernHttpRequesterMock.setRequestPayloads(new String[]{"jr://resource/commcare-apps/case_search_and_claim/good-query-result.xml"});
-
-        AndroidSessionWrapper sessionWrapper =
-                CommCareApplication._().getCurrentSessionWrapper();
-        CommCareSession session = sessionWrapper.getSession();
-        session.setCommand("patient-search");
+        ModernHttpRequesterMock.setExpectedUrls(
+                new String[]{"https://www.fake.com/patient_search/?patient_id=123&name=francisco&device_id=000000000000000"});
+        ModernHttpRequesterMock.setRequestPayloads(
+                new String[]{"jr://resource/commcare-apps/case_search_and_claim/good-query-result.xml"});
 
         Intent queryActivityIntent =
                 new Intent(RuntimeEnvironment.application, QueryRequestActivity.class);
         QueryRequestActivity queryRequestActivity =
-                Robolectric.buildActivity(QueryRequestActivity.class).withIntent(queryActivityIntent)
-                        .create().start().resume().get();
+                Robolectric.buildActivity(QueryRequestActivity.class)
+                        .withIntent(queryActivityIntent).setup().get();
 
-        LinearLayout promptsLayout = (LinearLayout)queryRequestActivity.findViewById(R.id.query_prompts);
+        LinearLayout promptsLayout =
+                (LinearLayout)queryRequestActivity.findViewById(R.id.query_prompts);
         EditText patientId = (EditText)promptsLayout.getChildAt(1);
         patientId.setText("123");
         EditText patientName = (EditText)promptsLayout.getChildAt(3);
@@ -97,56 +98,59 @@ public class QueryRequestActivityTest {
         assertTrue(queryRequestActivity.isFinishing());
     }
 
+    /**
+     * Make query whose response isn't in the correct format and check the
+     * error message displayed
+     */
     @Test
     public void makeQueryWithBadServerPayloadTest() {
         ModernHttpRequesterMock.setResponseCodes(new Integer[]{200});
-        ModernHttpRequesterMock.setExpectedUrls(new String[]{"https://www.fake.com/patient_search/?patient_id=123&name=francisco&device_id=000000000000000"});
-        ModernHttpRequesterMock.setRequestPayloads(new String[]{"jr://resource/commcare-apps/case_search_and_claim/bad-query-result.xml"});
-
-        AndroidSessionWrapper sessionWrapper =
-                CommCareApplication._().getCurrentSessionWrapper();
-        CommCareSession session = sessionWrapper.getSession();
-        session.setCommand("patient-search");
+        ModernHttpRequesterMock.setExpectedUrls(
+                new String[]{"https://www.fake.com/patient_search/?patient_id=123&name=francisco&device_id=000000000000000"});
+        ModernHttpRequesterMock.setRequestPayloads(
+                new String[]{"jr://resource/commcare-apps/case_search_and_claim/bad-query-result.xml"});
 
         Intent queryActivityIntent =
                 new Intent(RuntimeEnvironment.application, QueryRequestActivity.class);
 
         ActivityController<QueryRequestActivity> controller =
                 Robolectric.buildActivity(QueryRequestActivity.class)
-                        .withIntent(queryActivityIntent).create().start().resume();
+                        .withIntent(queryActivityIntent).setup();
         QueryRequestActivity queryRequestActivity = controller.get();
 
-        LinearLayout promptsLayout = (LinearLayout)queryRequestActivity.findViewById(R.id.query_prompts);
+        LinearLayout promptsLayout =
+                (LinearLayout)queryRequestActivity.findViewById(R.id.query_prompts);
         EditText patientId = (EditText)promptsLayout.getChildAt(1);
         patientId.setText("123");
         EditText patientName = (EditText)promptsLayout.getChildAt(3);
         patientName.setText("francisco");
 
-        Button queryButton = (Button)queryRequestActivity.findViewById(R.id.request_button);
+        Button queryButton =
+                (Button)queryRequestActivity.findViewById(R.id.request_button);
         queryButton.performClick();
 
         TextView errorMessage = (TextView)queryRequestActivity.findViewById(R.id.error_message);
         assertEquals(View.VISIBLE, errorMessage.getVisibility());
-        assertTrue(((String)errorMessage.getText()).contains(Localization.get("query.response.format.error", "")));
+        String expectedErrorPart = Localization.get("query.response.format.error", "");
+        assertTrue(((String)errorMessage.getText()).contains(expectedErrorPart));
 
         Bundle savedInstanceState = new Bundle();
         controller.saveInstanceState(savedInstanceState);
 
         queryRequestActivity = Robolectric.buildActivity(QueryRequestActivity.class)
-                .withIntent(queryActivityIntent).create(savedInstanceState).start().resume().get();
+                .withIntent(queryActivityIntent).setup(savedInstanceState).get();
 
         errorMessage = (TextView)queryRequestActivity.findViewById(R.id.error_message);
         assertEquals(View.VISIBLE, errorMessage.getVisibility());
-        assertTrue(((String)errorMessage.getText()).contains(Localization.get("query.response.format.error", "")));
+        assertTrue(((String)errorMessage.getText()).contains(expectedErrorPart));
     }
 
+    /**
+     * Start filling out query parameters and then 'rotate' the screen to see
+     * if text entry is restored
+     */
     @Test
     public void reloadQueryActivityStateTest() {
-        AndroidSessionWrapper sessionWrapper =
-                CommCareApplication._().getCurrentSessionWrapper();
-        CommCareSession session = sessionWrapper.getSession();
-        session.setCommand("patient-search");
-
         Intent queryActivityIntent =
                 new Intent(RuntimeEnvironment.application, QueryRequestActivity.class);
 
@@ -155,7 +159,8 @@ public class QueryRequestActivityTest {
                         .withIntent(queryActivityIntent).create().start().resume();
         QueryRequestActivity queryRequestActivity = controller.get();
 
-        LinearLayout promptsLayout = (LinearLayout)queryRequestActivity.findViewById(R.id.query_prompts);
+        LinearLayout promptsLayout =
+                (LinearLayout)queryRequestActivity.findViewById(R.id.query_prompts);
         EditText patientId = (EditText)promptsLayout.getChildAt(1);
         patientId.setText("123");
 
@@ -163,12 +168,19 @@ public class QueryRequestActivityTest {
         controller.saveInstanceState(savedInstanceState);
 
         queryRequestActivity = Robolectric.buildActivity(QueryRequestActivity.class)
-                .withIntent(queryActivityIntent).create(savedInstanceState).start().resume().get();
+                .withIntent(queryActivityIntent).setup(savedInstanceState).get();
         promptsLayout = (LinearLayout)queryRequestActivity.findViewById(R.id.query_prompts);
         patientId = (EditText)promptsLayout.getChildAt(1);
         assertEquals("123", patientId.getText().toString());
 
         patientId = (EditText)promptsLayout.getChildAt(3);
         assertEquals("", patientId.getText().toString());
+    }
+
+    private static void setSessionCommand(String command) {
+        AndroidSessionWrapper sessionWrapper =
+                CommCareApplication._().getCurrentSessionWrapper();
+        CommCareSession session = sessionWrapper.getSession();
+        session.setCommand(command);
     }
 }
