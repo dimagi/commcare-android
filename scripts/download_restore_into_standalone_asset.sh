379c388efb811e9f4a5a13dda35b06f3b94b241a
ALIZA STONE <amstone326@gmail.com>
2016-05-18 13:13:36 -0400

    fix bug where domain was hardcoded in restore URL

--- scripts/download_restore_into_standalone_asset.sh
+++ scripts/download_restore_into_standalone_asset.sh
@@ -7,6 +7,6 @@ USERNAME=$2
 PASSWORD=$3
 ASSET_DIR=$4
 
-curl --basic -u $USERNAME@$DOMAIN.commcarehq.org:$PASSWORD "https://www.commcarehq.org/a/aliza-test/phone/restore/?version=2.0" -o "$ASSET_DIR/local_restore_payload.xml"
+curl --basic -u $USERNAME@$DOMAIN.commcarehq.org:$PASSWORD "https://www.commcarehq.org/a/$DOMAIN/phone/restore/?version=2.0" -o "$ASSET_DIR/local_restore_payload.xml"
 
 exit 0
