bcba38927a331d5dfce009859c1d18fa99de7225
Phillip Mates <pmates@dimagi.com>
2016-06-01 13:41:37 -0400

    Migrate DeviceReportRecord to not use reflection

--- app/src/org/commcare/android/javarosa/DeviceReportRecord.java
+++ app/src/org/commcare/android/javarosa/DeviceReportRecord.java
@@ -1,18 +1,23 @@
 package org.commcare.android.javarosa;
 
 import org.commcare.CommCareApplication;
+import org.commcare.android.storage.framework.PersistedPlain;
 import org.commcare.models.encryption.EncryptionIO;
-import org.commcare.android.storage.framework.Persisted;
-import org.commcare.models.framework.Persisting;
 import org.commcare.models.framework.Table;
 import org.commcare.modern.models.EncryptedModel;
 import org.commcare.utils.FileUtil;
 import org.commcare.utils.GlobalConstants;
-import org.commcare.utils.SessionUnavailableException;
 import org.javarosa.core.model.utils.DateUtils;
+import org.javarosa.core.services.storage.IMetaData;
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.core.util.externalizable.ExtUtil;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Date;
 
@@ -21,20 +26,17 @@ import javax.crypto.spec.SecretKeySpec;
 /**
  * A small DB record for keeping track of serialized device reports which we are planning
  * on submitting. Keeps track of the location on disk, and the key we use to encrypt it.
- * <p/>
+ *
  * Fairly similar record to what we're storing for the forms. Should possibly use that
  * one and its process
  *
  * @author ctsims
  */
-
 @Table(DeviceReportRecord.STORAGE_KEY)
-public class DeviceReportRecord extends Persisted implements EncryptedModel {
+public class DeviceReportRecord extends PersistedPlain implements IMetaData, EncryptedModel {
     public static final String STORAGE_KEY = "log_records";
 
-    @Persisting(1)
     private String fileName;
-    @Persisting(2)
     private byte[] aesKey;
 
     public DeviceReportRecord() {
@@ -55,7 +57,6 @@ public class DeviceReportRecord extends Persisted implements EncryptedModel {
         slr.aesKey = CommCareApplication._().createNewSymmetricKey().getEncoded();
         return slr;
     }
-
 
     @Override
     public boolean isEncrypted(String data) {
@@ -78,5 +79,31 @@ public class DeviceReportRecord extends Persisted implements EncryptedModel {
     public final OutputStream openOutputStream() throws FileNotFoundException {
         return EncryptionIO.createFileOutputStream(getFilePath(),
                 new SecretKeySpec(getKey(), "AES"));
+    }
+
+    @Override
+    public void readExternal(DataInputStream in, PrototypeFactory pf) throws IOException, DeserializationException {
+        super.readExternal(in, pf);
+
+        fileName = ExtUtil.readString(in);
+        aesKey = ExtUtil.readBytes(in);
+    }
+
+    @Override
+    public void writeExternal(DataOutputStream out) throws IOException {
+        super.writeExternal(out);
+
+        ExtUtil.writeString(out, fileName);
+        ExtUtil.writeBytes(out, aesKey);
+    }
+
+    @Override
+    public String[] getMetaDataFields() {
+        return new String[]{};
+    }
+
+    @Override
+    public Object getMetaData(String fieldName) {
+        throw new IllegalArgumentException("No metadata field " + fieldName + " in the storage system");
     }
 }
