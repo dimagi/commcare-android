829727ddd3da39ae3d3bc334f6c7c7beeb4f8bb8
Phillip Mates <pmates@dimagi.com>
2016-06-03 11:52:18 -0400

    Avoid asking for sms perms if it is a consumer app

--- app/src/org/commcare/activities/CommCareSetupActivity.java
+++ app/src/org/commcare/activities/CommCareSetupActivity.java
@@ -846,10 +846,6 @@ public class CommCareSetupActivity extends CommCareActivity<CommCareSetupActivit
                     scanSMSLinks(manualSMSInstall);
                 }
             }
-
-            if (isSingleAppBuild()) {
-                SingleAppInstallation.installSingleApp(this, DIALOG_INSTALL_PROGRESS);
-            }
         } else if (requestCode == Permissions.ALL_PERMISSIONS_REQUEST) {
             String[] requiredPerms = Permissions.getRequiredPerms();
 
@@ -870,8 +866,12 @@ public class CommCareSetupActivity extends CommCareActivity<CommCareSetupActivit
                 uiStateScreenTransition();
             }
 
-            // Since SMS asks for more permissions, call was delayed until here
-            performSMSInstall(false);
+            if (isSingleAppBuild()) {
+                SingleAppInstallation.installSingleApp(this, DIALOG_INSTALL_PROGRESS);
+            } else {
+                // Since SMS asks for more permissions, call was delayed until here
+                performSMSInstall(false);
+            }
         }
     }
 
