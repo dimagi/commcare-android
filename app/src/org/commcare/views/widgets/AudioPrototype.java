2ae621e182478be16ccf0610020fc313879f46cb
Saumya Jain <babujain28@gmail.com>
2016-06-07 16:39:29 -0400

    Syncing with previous branch

--- app/src/org/commcare/views/widgets/AudioPrototype.java
+++ app/src/org/commcare/views/widgets/AudioPrototype.java
@@ -1,7 +1,6 @@
 package org.commcare.views.widgets;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.media.MediaRecorder;
@@ -9,13 +8,12 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-
 import org.commcare.dalvik.R;
 import org.commcare.logic.PendingCalloutInterface;
+import org.javarosa.core.services.locale.Localization;
 import org.javarosa.form.api.FormEntryPrompt;
 
 import java.io.IOException;
@@ -31,15 +29,16 @@ public class AudioPrototype extends AudioWidget{
     private Button stop;
     private LinearLayout myLayout;
     private ProgressBar myProgress;
+    private final String FILE_EXT = "/tester.3gpp";
 
     public AudioPrototype(Context context, FormEntryPrompt prompt, PendingCalloutInterface pic){
         super(context, prompt, pic);
 
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/tester.3gpp";
 
         //TODO: Figure out what to do with files. Store them all on file system (What dir?)? Delete them every time? Overwrite every time?
         //TODO: Figure out how to preserve recording view and mRecorder when screen is turned...for now I just disabled rotation while recording.
+        mFileName += FILE_EXT;
     }
 
     @Override
@@ -59,14 +58,18 @@ public class AudioPrototype extends AudioWidget{
                 startRecording();
             }
         });
+
+        start.setText(Localization.get("start.recording"));
+
         stop.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopRecording();
             }
         });
-        stop.setEnabled(false);
 
+        stop.setText(Localization.get("stop.recording"));
+        stop.setEnabled(false);
         addView(myLayout);
         myLayout.setVisibility(GONE);
 
@@ -88,6 +91,8 @@ public class AudioPrototype extends AudioWidget{
         if(mRecorder == null){
            mRecorder = new MediaRecorder();
        }
+
+        mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mRecorder.setOutputFile(mFileName);
@@ -96,13 +101,15 @@ public class AudioPrototype extends AudioWidget{
         try{
             mRecorder.prepare();
         }catch(IOException e){
-            e.printStackTrace();
+            Log.d("Recorder", "Failed to prepare media recorder");
         }
 
         mRecorder.start();
         myProgress.setVisibility(VISIBLE);
         start.setEnabled(false);
         stop.setEnabled(true);
+
+        mRecorder.start();
     }
 
     private void stopRecording(){
@@ -120,6 +127,5 @@ public class AudioPrototype extends AudioWidget{
         myProgress.setVisibility(GONE);
 
         ((Activity) getContext()).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
-
     }
 }
