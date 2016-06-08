82d5a21dbc2209738db2ac5a2a7edfd11472fe89
Phillip Mates <pmates@dimagi.com>
2016-06-08 15:07:55 -0400

    Don't hit storage to check existence in table

--- app/src/org/commcare/engine/resource/AndroidResourceTable.java
+++ app/src/org/commcare/engine/resource/AndroidResourceTable.java
@@ -4,8 +4,10 @@ import org.commcare.models.database.SqlStorage;
 import org.commcare.resources.model.InstallerFactory;
 import org.commcare.resources.model.Resource;
 import org.commcare.resources.model.ResourceTable;
+import org.javarosa.core.services.storage.IStorageIterator;
 import org.javarosa.core.services.storage.IStorageUtilityIndexed;
 
+import java.util.HashSet;
 import java.util.Vector;
 
 /**
@@ -13,6 +15,7 @@ import java.util.Vector;
  */
 public class AndroidResourceTable extends ResourceTable {
     private final SqlStorage<Resource> sqlStorage;
+    private HashSet<String> resourcesInTable;
 
     public AndroidResourceTable(SqlStorage<Resource> storage, InstallerFactory factory) {
         super((IStorageUtilityIndexed) storage, factory);
@@ -24,5 +27,42 @@ public class AndroidResourceTable extends ResourceTable {
         Vector<Resource> v = new Vector<>();
         v.addAll(sqlStorage.getRecordsForValue(Resource.META_INDEX_PARENT_GUID, parent));
         return v;
+    }
+
+    @Override
+    protected boolean resourceDoesntExist(Resource resource) {
+        initResourcesInTable();
+        return !resourcesInTable.contains(resource.getResourceId());
+    }
+
+    private void initResourcesInTable() {
+        if (resourcesInTable == null) {
+            resourcesInTable = new HashSet<>();
+            for (IStorageIterator it = sqlStorage.iterate(); it.hasMore(); ) {
+                Resource r = (Resource)it.nextRecord();
+                resourcesInTable.add(r.getResourceId());
+            }
+        }
+    }
+
+    @Override
+    protected void cleanup() {
+        super.cleanup();
+        initResourcesInTable();
+        resourcesInTable.clear();
+    }
+
+    @Override
+    public void removeResource(Resource resource) {
+        super.removeResource(resource);
+        initResourcesInTable();
+        resourcesInTable.remove(resource.getResourceId());
+    }
+
+    @Override
+    public void commit(Resource r) {
+        super.commit(r);
+        initResourcesInTable();
+        resourcesInTable.add(r.getResourceId());
     }
 }
