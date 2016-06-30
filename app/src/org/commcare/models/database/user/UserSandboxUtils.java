0a15ef40ca9996c3ca517b90bcddd8689535e577
Phillip Mates <pmates@dimagi.com>
2016-06-30 16:30:34 -0400

    Breakup user sandbox migration into helper methods

--- app/src/org/commcare/models/database/user/UserSandboxUtils.java
+++ app/src/org/commcare/models/database/user/UserSandboxUtils.java
@@ -29,10 +29,53 @@ import java.io.IOException;
  */
 public class UserSandboxUtils {
 
-    public static void migrateData(Context c, CommCareApp app, UserKeyRecord incomingSandbox, byte[] unwrappedOldKey, UserKeyRecord newSandbox, byte[] unwrappedNewKey) throws IOException {
-        //Step one: Make a copy of the incoming sandbox's database and re-key it to use the new key.
+    public static void migrateData(Context c, CommCareApp app,
+                                   UserKeyRecord incomingSandbox, byte[] unwrappedOldKey,
+                                   UserKeyRecord newSandbox, byte[] unwrappedNewKey)
+            throws IOException {
         Logger.log(AndroidLogger.TYPE_MAINTENANCE, "Migrating an existing user sandbox for " + newSandbox.getUsername());
+        String newKeyEncoded = rekeyDB(c, incomingSandbox, newSandbox, unwrappedOldKey, unwrappedNewKey);
 
+        Logger.log(AndroidLogger.TYPE_MAINTENANCE, "Database is re-keyed and ready for use. Copying over files now");
+        //OK, so now we have the Db transitioned. What we need to do now is go through and rekey all of our file references.
+        final SQLiteDatabase db = new DatabaseUserOpenHelper(CommCareApplication._(), newSandbox.getUuid()).getWritableDatabase(newKeyEncoded);
+
+        try {
+            //If we were able to iterate over the users, the key was fine, so let's use it to open our db
+            AndroidDbHelper dbh = new AndroidDbHelper(c) {
+                @Override
+                public SQLiteDatabase getHandle() {
+                    return db;
+                }
+            };
+
+            //TODO: At some point we should really just encode the existence/operations on files in the record models themselves
+            //Models with Files: Form Record. Log Record
+            SqlStorage<DeviceReportRecord> reports = new SqlStorage<>(DeviceReportRecord.STORAGE_KEY, DeviceReportRecord.class, dbh);
+            migrateDeviceReports(reports, newSandbox);
+
+            Logger.log(AndroidLogger.TYPE_MAINTENANCE, "Copied over all of the device reports. Moving on to the form records");
+
+            ContentResolver cr = c.getContentResolver();
+
+            SqlStorage<FormRecord> formRecords = new SqlStorage<>(FormRecord.STORAGE_KEY, FormRecord.class, dbh);
+            migrateFormRecords(formRecords, cr, newSandbox);
+        } finally {
+            db.close();
+        }
+
+        Logger.log(AndroidLogger.TYPE_MAINTENANCE, "All form records copied over");
+
+        //OK! So we should be all set, here. Mark the new sandbox as ready and the old sandbox as ready for cleanup.
+        finalizeMigration(app, incomingSandbox, newSandbox);
+    }
+
+    /**
+     * Make a copy of the incoming sandbox's database and re-key it to use the new key.
+     */
+    private static String rekeyDB(Context c, UserKeyRecord incomingSandbox, UserKeyRecord newSandbox,
+                                  byte[] unwrappedOldKey, byte[] unwrappedNewKey)
+            throws IOException {
         File oldDb = c.getDatabasePath(DatabaseUserOpenHelper.getDbName(incomingSandbox.getUuid()));
         File newDb = c.getDatabasePath(DatabaseUserOpenHelper.getDbName(newSandbox.getUuid()));
 
@@ -57,105 +100,83 @@ public class UserSandboxUtils {
         rawDbHandle.execSQL("PRAGMA rekey  = '" + newKeyEncoded + "';");
 
         rawDbHandle.close();
+        return newKeyEncoded;
+    }
 
-        Logger.log(AndroidLogger.TYPE_MAINTENANCE, "Database is re-keyed and ready for use. Copying over files now");
+    private static void migrateDeviceReports(SqlStorage<DeviceReportRecord> reports,
+                                             UserKeyRecord newSandbox) throws IOException {
+        for (DeviceReportRecord r : reports) {
+            File oldPath = new File(r.getFilePath());
+            File newPath = FileUtil.getNewFileLocation(oldPath, newSandbox.getUuid(), true);
 
-        //OK, so now we have the Db transitioned. What we need to do now is go through and rekey all of our file references.
-
-        final SQLiteDatabase db = new DatabaseUserOpenHelper(CommCareApplication._(), newSandbox.getUuid()).getWritableDatabase(newKeyEncoded);
-
-        try {
-
-            //If we were able to iterate over the users, the key was fine, so let's use it to open our db
-            AndroidDbHelper dbh = new AndroidDbHelper(c) {
-                @Override
-                public SQLiteDatabase getHandle() {
-                    return db;
-                }
-            };
-
-            //TODO: At some point we should really just encode the existence/operations on files in the record models themselves
-            //Models with Files: Form Record. Log Record
-            SqlStorage<DeviceReportRecord> reports = new SqlStorage<>(DeviceReportRecord.STORAGE_KEY, DeviceReportRecord.class, dbh);
-
-            //Log records
-            for (DeviceReportRecord r : reports) {
-                File oldPath = new File(r.getFilePath());
-                File newPath = FileUtil.getNewFileLocation(oldPath, newSandbox.getUuid(), true);
-
-                //Copy to a new location while re-encrypting
-                FileUtil.copyFile(oldPath, newPath);
-            }
-
-            Logger.log(AndroidLogger.TYPE_MAINTENANCE, "Copied over all of the device reports. Moving on to the form records");
-
-            ContentResolver cr = c.getContentResolver();
-
-            //Form records are sadly a bit more complex. We need to both move all of the files, 
-            //insert a new record in the content provider, and then update the form record.
-            SqlStorage<FormRecord> formRecords = new SqlStorage<>(FormRecord.STORAGE_KEY, FormRecord.class, dbh);
-            for (FormRecord record : formRecords) {
-                Uri instanceURI = record.getInstanceURI();
-
-                //some records won't have a uri yet.
-                if (instanceURI == null) {
-                    continue;
-                }
-
-                ContentValues values = new ContentValues();
-                File oldForm;
-                {
-
-                    //otherwise read and prepare the record
-                    Cursor oldRecord = cr.query(instanceURI, new String[]{InstanceColumns.INSTANCE_FILE_PATH, InstanceColumns.DISPLAY_NAME, InstanceColumns.SUBMISSION_URI, InstanceColumns.JR_FORM_ID, InstanceColumns.STATUS, InstanceColumns.CAN_EDIT_WHEN_COMPLETE, InstanceColumns.LAST_STATUS_CHANGE_DATE, InstanceColumns.DISPLAY_SUBTEXT}, null, null, null);
-                    if (!oldRecord.moveToFirst()) {
-                        throw new IOException("Non existant form record at URI " + instanceURI.toString());
-                    }
-
-                    values.put(InstanceColumns.DISPLAY_NAME, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.DISPLAY_NAME)));
-                    values.put(InstanceColumns.SUBMISSION_URI, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.SUBMISSION_URI)));
-                    values.put(InstanceColumns.JR_FORM_ID, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.JR_FORM_ID)));
-                    values.put(InstanceColumns.STATUS, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.STATUS)));
-                    values.put(InstanceColumns.CAN_EDIT_WHEN_COMPLETE, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.CAN_EDIT_WHEN_COMPLETE)));
-                    values.put(InstanceColumns.LAST_STATUS_CHANGE_DATE, oldRecord.getLong(oldRecord.getColumnIndex(InstanceColumns.LAST_STATUS_CHANGE_DATE)));
-                    values.put(InstanceColumns.DISPLAY_SUBTEXT, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.DISPLAY_SUBTEXT)));
-
-
-                    //Copy over the other metadata
-
-                    oldForm = new File(oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.INSTANCE_FILE_PATH)));
-
-                    oldRecord.close();
-                }
-
-                File oldFolder = oldForm.getParentFile();
-
-                //find a new spot for it
-                File newFolder = FileUtil.getNewFileLocation(oldFolder, newSandbox.getUuid(), true);
-
-                FileUtil.copyFileDeep(oldFolder, newFolder);
-
-                File newfileToWrite = null;
-                for (File f : newFolder.listFiles()) {
-                    if (f.getName().equals(oldForm.getName())) {
-                        newfileToWrite = f;
-                    }
-                }
-
-                //ok, new directory totally ready. Create a new instanceURI
-                values.put(InstanceColumns.INSTANCE_FILE_PATH, newfileToWrite.getAbsolutePath());
-                Uri newUri = cr.insert(InstanceColumns.CONTENT_URI, values);
-                record = record.updateInstanceAndStatus(newUri.toString(), record.getStatus());
-                formRecords.write(record);
-            }
-
-        } finally {
-            db.close();
+            //Copy to a new location while re-encrypting
+            FileUtil.copyFile(oldPath, newPath);
         }
+    }
+    /**
+     * Form records are sadly a bit more complex. We need to both move all of the files,
+     * insert a new record in the content provider, and then update the form record.
+     */
+    private static void migrateFormRecords(SqlStorage<FormRecord> formRecords,
+                                           ContentResolver cr,
+                                           UserKeyRecord newSandbox) throws IOException {
+        for (FormRecord record : formRecords) {
+            Uri instanceURI = record.getInstanceURI();
 
-        Logger.log(AndroidLogger.TYPE_MAINTENANCE, "All form records copied over");
+            //some records won't have a uri yet.
+            if (instanceURI == null) {
+                continue;
+            }
 
-        //OK! So we should be all set, here. Mark the new sandbox as ready and the old sandbox as ready for cleanup.
+            ContentValues values = new ContentValues();
+            File oldForm;
+            {
+
+                //otherwise read and prepare the record
+                Cursor oldRecord = cr.query(instanceURI, new String[]{InstanceColumns.INSTANCE_FILE_PATH, InstanceColumns.DISPLAY_NAME, InstanceColumns.SUBMISSION_URI, InstanceColumns.JR_FORM_ID, InstanceColumns.STATUS, InstanceColumns.CAN_EDIT_WHEN_COMPLETE, InstanceColumns.LAST_STATUS_CHANGE_DATE, InstanceColumns.DISPLAY_SUBTEXT}, null, null, null);
+                if (!oldRecord.moveToFirst()) {
+                    throw new IOException("Non existant form record at URI " + instanceURI.toString());
+                }
+
+                values.put(InstanceColumns.DISPLAY_NAME, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.DISPLAY_NAME)));
+                values.put(InstanceColumns.SUBMISSION_URI, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.SUBMISSION_URI)));
+                values.put(InstanceColumns.JR_FORM_ID, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.JR_FORM_ID)));
+                values.put(InstanceColumns.STATUS, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.STATUS)));
+                values.put(InstanceColumns.CAN_EDIT_WHEN_COMPLETE, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.CAN_EDIT_WHEN_COMPLETE)));
+                values.put(InstanceColumns.LAST_STATUS_CHANGE_DATE, oldRecord.getLong(oldRecord.getColumnIndex(InstanceColumns.LAST_STATUS_CHANGE_DATE)));
+                values.put(InstanceColumns.DISPLAY_SUBTEXT, oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.DISPLAY_SUBTEXT)));
+
+
+                //Copy over the other metadata
+
+                oldForm = new File(oldRecord.getString(oldRecord.getColumnIndex(InstanceColumns.INSTANCE_FILE_PATH)));
+
+                oldRecord.close();
+            }
+
+            File oldFolder = oldForm.getParentFile();
+
+            //find a new spot for it
+            File newFolder = FileUtil.getNewFileLocation(oldFolder, newSandbox.getUuid(), true);
+
+            FileUtil.copyFileDeep(oldFolder, newFolder);
+
+            File newfileToWrite = null;
+            for (File f : newFolder.listFiles()) {
+                if (f.getName().equals(oldForm.getName())) {
+                    newfileToWrite = f;
+                }
+            }
+
+            //ok, new directory totally ready. Create a new instanceURI
+            values.put(InstanceColumns.INSTANCE_FILE_PATH, newfileToWrite.getAbsolutePath());
+            Uri newUri = cr.insert(InstanceColumns.CONTENT_URI, values);
+            record = record.updateInstanceAndStatus(newUri.toString(), record.getStatus());
+            formRecords.write(record);
+        }
+    }
+
+    private static void finalizeMigration(CommCareApp app, UserKeyRecord incomingSandbox, UserKeyRecord newSandbox) {
         SqlStorage<UserKeyRecord> ukr = app.getStorage(UserKeyRecord.class);
 
         SQLiteDatabase ukrdb = ukr.getAccessLock();
@@ -170,7 +191,6 @@ public class UserSandboxUtils {
             ukrdb.endTransaction();
         }
     }
-
 
     public static String getSqlCipherEncodedKey(byte[] bytes) {
         String hexString = "x\"";
