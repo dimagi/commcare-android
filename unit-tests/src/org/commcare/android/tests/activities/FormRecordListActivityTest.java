fbbe2dc5179e9f8fb136eef3b795e4341c522c31
Phillip Mates <pmates@dimagi.com>
2016-07-05 16:19:31 -0400

    Have saved forms test actually open a saved form

--- unit-tests/src/org/commcare/android/tests/activities/FormRecordListActivityTest.java
+++ unit-tests/src/org/commcare/android/tests/activities/FormRecordListActivityTest.java
@@ -1,11 +1,16 @@
 package org.commcare.android.tests.activities;
 
+import android.content.Intent;
+import android.os.Environment;
 import android.widget.ListView;
 
 import org.commcare.CommCareTestApplication;
+import org.commcare.activities.CommCareHomeActivity;
+import org.commcare.activities.FormEntryActivity;
 import org.commcare.activities.FormRecordListActivity;
 import org.commcare.adapters.IncompleteFormListAdapter;
 import org.commcare.android.CommCareTestRunner;
+import org.commcare.android.database.user.models.FormRecord;
 import org.commcare.android.util.SavedFormLoader;
 import org.commcare.android.util.TestAppInstaller;
 import org.commcare.dalvik.BuildConfig;
@@ -14,9 +19,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowEnvironment;
+import org.robolectric.shadows.ShadowListView;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * @author Phillip Mates (pmates@dimagi.com)
@@ -30,24 +41,69 @@ public class FormRecordListActivityTest {
         TestAppInstaller.installAppAndLogin(
                 "jr://resource/commcare-apps/form_nav_tests/profile.ccpr",
                 "test", "123");
-        SavedFormLoader.loadFormsFromPayload("/commcare-apps/form_nav_tests/form_instances_restore.xml");
+        SavedFormLoader.loadFormsFromPayload("/commcare-apps/form_nav_tests/form_instances_restore.xml",
+                FormRecord.STATUS_SAVED);
+        ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
     }
 
     /**
-     * Opens up the saved form list activity and checks that there are 2 forms listed
+     * Opens up the saved form list activity, checks that 2 forms are listed,
+     * and opens the first one
      */
     @Test
     public void openSavedFormViewTest() {
-        FormRecordListActivity homeActivity =
-                Robolectric.buildActivity(FormRecordListActivity.class).create().start().resume().get();
-        // wait for entities to load
+        Intent savedFormsIntent =
+                new Intent(RuntimeEnvironment.application, FormRecordListActivity.class);
+        ShadowActivity homeActivityShadow = prepSavedFormsActivity(savedFormsIntent);
+
+        FormRecordListActivity savedFormsActivity =
+                Robolectric.buildActivity(FormRecordListActivity.class)
+                        .withIntent(savedFormsIntent).create().start()
+                        .resume().get();
+
+        // wait for saved forms to load
         Robolectric.flushBackgroundThreadScheduler();
         Robolectric.flushForegroundThreadScheduler();
+
+        ShadowListView shadowEntityList = assertSavedFormEntries(savedFormsActivity);
+        shadowEntityList.performItemClick(0);
+
+        launchFormEntryForSavedForm(homeActivityShadow, savedFormsIntent, savedFormsActivity);
+    }
+
+    private static ShadowActivity prepSavedFormsActivity(Intent savedFormsIntent) {
+        CommCareHomeActivity homeActivity =
+                Robolectric.buildActivity(CommCareHomeActivity.class).create().get();
+        ShadowActivity homeShadow = Shadows.shadowOf(homeActivity);
+        homeShadow.startActivityForResult(savedFormsIntent,
+                CommCareHomeActivity.GET_INCOMPLETE_FORM);
+        return homeShadow;
+    }
+
+    private static ShadowListView assertSavedFormEntries(FormRecordListActivity savedFormActivity) {
         ListView entityList =
-                (ListView)homeActivity.findViewById(R.id.screen_entity_select_list);
-        IncompleteFormListAdapter adapter = (IncompleteFormListAdapter)entityList.getAdapter();
+                (ListView)savedFormActivity.findViewById(R.id.screen_entity_select_list);
+        IncompleteFormListAdapter adapter =
+                (IncompleteFormListAdapter)entityList.getAdapter();
         adapter.setFormFilter(FormRecordListActivity.FormRecordFilter.Submitted);
         adapter.resetRecords();
         assertEquals(2, adapter.getCount());
+        return Shadows.shadowOf(entityList);
+    }
+
+    private static void launchFormEntryForSavedForm(ShadowActivity homeActivityShadow,
+                                                    Intent savedFormsIntent,
+                                                    FormRecordListActivity savedFormsActivity) {
+        ShadowActivity formRecordShadow = Shadows.shadowOf(savedFormsActivity);
+        homeActivityShadow.receiveResult(savedFormsIntent,
+                formRecordShadow.getResultCode(),
+                formRecordShadow.getResultIntent());
+        ShadowActivity.IntentForResult formEntryIntent =
+                homeActivityShadow.getNextStartedActivityForResult();
+        FormEntryActivity formEntryActivity =
+                Robolectric.buildActivity(FormEntryActivity.class)
+                        .withIntent(formEntryIntent.intent)
+                        .create().start().resume().get();
+        assertFalse(Shadows.shadowOf(formEntryActivity).isFinishing());
     }
 }
