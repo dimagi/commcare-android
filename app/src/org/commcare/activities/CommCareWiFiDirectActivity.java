7028d73feacfd733530e7d68d79b11af482fa5c2
Will Pride <wpride@dimagi.com>
2016-07-14 17:41:59 -0400

    catch FormRecord pull error and display

--- app/src/org/commcare/activities/CommCareWiFiDirectActivity.java
+++ app/src/org/commcare/activities/CommCareWiFiDirectActivity.java
@@ -58,6 +58,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.Vector;
 
 /**
@@ -654,11 +655,19 @@ public class CommCareWiFiDirectActivity
     }
 
 
-    private void onRecordPullCompleted(Pair<Long, FormRecord[]> result) {
+    private void onRecordPullCompleted(Pair<Long, FormRecord[]> result, CommCareWiFiDirectActivity receiver) {
+
         // for the time being we're going to ignore the result of the record pull and proceed regardless
         myStatusText.setText(localize("wifi.direct.pull.successful"));
         if(result != null){
-            // we didn't pull any form records to file system, this is fine.
+            if(result.first > 0){
+                // if we had files but they failed, we should error and block
+                receiver.myStatusText.setText(localize("wifi.direct.pull.unsuccessful",
+                        "Problem transferring forms to file system"));
+                receiver.transplantStyle(receiver.myStatusText,
+                        R.layout.template_text_notification_problem);
+                return;
+            }
             this.cachedRecords = result.second;
         }
         updateStatusText();
@@ -671,7 +680,7 @@ public class CommCareWiFiDirectActivity
         FormRecordToFileTask formRecordToFileTask = new FormRecordToFileTask(this, toBeTransferredDirectory) {
             @Override
             protected void deliverResult(CommCareWiFiDirectActivity receiver, Pair<Long, FormRecord[]> result) {
-                receiver.onRecordPullCompleted(result);
+                receiver.onRecordPullCompleted(result, receiver);
             }
 
             @Override
