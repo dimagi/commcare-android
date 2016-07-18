f7d4b09cf655d68d57edb8bc4c37c53c816579a8
Phillip Mates <pmates@dimagi.com>
2016-07-18 10:05:11 -0400

    Use dimens to increase padding on larger screens

--- app/src/org/commcare/views/widgets/WidgetUtils.java
+++ app/src/org/commcare/views/widgets/WidgetUtils.java
@@ -5,6 +5,8 @@ import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.TableLayout;
 
+import org.commcare.dalvik.R;
+
 class WidgetUtils {
     private static final TableLayout.LayoutParams params;
 
@@ -16,7 +18,7 @@ class WidgetUtils {
     public static void setupButton(Button btn, Spannable text, int fontSize, boolean enabled) {
         btn.setText(text);
         btn.setTextSize(TypedValue.COMPLEX_UNIT_DIP, fontSize);
-        btn.setPadding(20, 30, 20, 30);
+        btn.setPadding(20, R.dimen.widget_button_padding, 20, R.dimen.widget_button_padding);
         btn.setEnabled(enabled);
         btn.setLayoutParams(WidgetUtils.params);
     }
