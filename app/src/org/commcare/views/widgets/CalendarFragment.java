8b99f3e20b1df82eaf7749c4f0db4673cd28a289
Saumya Jain <babujain28@gmail.com>
2016-07-26 20:01:46 -0400

    Removing unnecessary comments

--- app/src/org/commcare/views/widgets/CalendarFragment.java
+++ app/src/org/commcare/views/widgets/CalendarFragment.java
@@ -7,7 +7,6 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
