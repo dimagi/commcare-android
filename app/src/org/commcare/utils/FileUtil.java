74366ccd472e00dbe8b8a54b7e121f7b82be7eff
Phillip Mates <pmates@dimagi.com>
2016-08-16 16:39:50 -0400

    Remove unused method

--- app/src/org/commcare/utils/FileUtil.java
+++ app/src/org/commcare/utils/FileUtil.java
@@ -536,10 +536,6 @@ public class FileUtil {
         return length > WARNING_SIZE;
     }
 
-    public static boolean isFileTooBigToUpload(File mf) {
-        return mf.length() > FormUploadUtil.MAX_BYTES;
-    }
-
     public static double getFileSize(File mf) {
         return mf.length() / 1024;
     }
