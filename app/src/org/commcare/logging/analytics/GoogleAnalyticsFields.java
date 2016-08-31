4d4c8988950578e3a936d9d40c86970487c8e440
ALIZA STONE <amstone326@gmail.com>
2016-08-31 14:21:46 -0400

    enable offline updates from a .ccz

--- app/src/org/commcare/logging/analytics/GoogleAnalyticsFields.java
+++ app/src/org/commcare/logging/analytics/GoogleAnalyticsFields.java
@@ -140,11 +140,11 @@ public final class GoogleAnalyticsFields {
     public static final String LABEL_MARKDOWN = "Markdown Enabled";
     public static final String LABEL_IMAGE_ABOVE_TEXT = "Image Above Question Text Enabled";
     public static final String LABEL_TRIGGERS_ON_SAVE = "Fire triggers on form save";
-    public static final String LABEL_ANIMATE_FORM_SUBMIT_BUTTON = "Animate form submit button";
     public static final String LABEL_REPORT_BUTTON_ENABLED = "Home Report Button enabled";
     public static final String LABEL_AUTO_PURGE = "Auto Purge on Save Enabled";
     public static final String LABEL_LOAD_FORM_PAYLOAD_AS = "Load form payload as";
     public static final String LABEL_DETAIL_TAB_SWIPE_ACTION = "Detail tab final swipe action enabled";
+    public static final String LABEL_OFFLINE_UPDATE = "Offline Updates enabled";
 
     // Labels for ACTION_OPTIONS_MENU_ITEM in CATEGORY_HOME_SCREEN
     public static final String LABEL_SETTINGS = "Settings";
