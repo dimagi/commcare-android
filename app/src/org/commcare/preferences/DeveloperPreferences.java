f7a36102509e15fd1557d97131595ff7118b08ab
Phillip Mates <pmates@dimagi.com>
2016-10-17 09:18:42 -0400

    Clear hack that was fixed by calling visible on robo activity

--- app/src/org/commcare/preferences/DeveloperPreferences.java
+++ app/src/org/commcare/preferences/DeveloperPreferences.java
@@ -250,14 +250,9 @@ public class DeveloperPreferences extends SessionAwarePreferenceActivity
     }
 
     public static boolean isSessionSavingEnabled() {
-        if (CommCareApplication._().getCurrentApp() == null) {
-            // null check needed for corner case in robolectric tests
-            return false;
-        } else {
-            SharedPreferences properties = CommCareApplication._().getCurrentApp().getAppPreferences();
-            return properties.getString(ENABLE_SAVE_SESSION, CommCarePreferences.NO).
-                    equals(CommCarePreferences.YES);
-        }
+        SharedPreferences properties = CommCareApplication._().getCurrentApp().getAppPreferences();
+        return properties.getString(ENABLE_SAVE_SESSION, CommCarePreferences.NO).
+                equals(CommCarePreferences.YES);
     }
 
     public static void enableSessionSaving() {
