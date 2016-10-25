0e5a4baf0495f39370a45d31f0565fab28629dbf
Phillip Mates <pmates@dimagi.com>
2016-10-25 10:44:13 -0200

    Add an example SMS to test

--- unit-tests/src/org/commcare/utils/SigningUtilTest.java
+++ unit-tests/src/org/commcare/utils/SigningUtilTest.java
@@ -11,15 +11,25 @@ import static org.junit.Assert.assertEquals;
  * @author Phillip Mates (pmates@dimagi.com)
  */
 public class SigningUtilTest {
+
+    @Test
+    public void parseSMSInstallMessage() throws UnsupportedEncodingException, Base64DecoderException {
+        String exampleSMS = "[commcare app - do not delete] aHR0cHM6Ly93d3cuY29tbWNhcmVocS5vcmcvYS9nYy9zbXMvYXBwX2luZm8vN2M3ZDQ5ZmJlZjU5YjcwM2ZiNDY4ZTIwZDUyYTIxZTQv";
+        String encodedURL = SigningUtil.trimMessagePayload(exampleSMS);
+        String decodedURL = SigningUtil.decodeUrl(encodedURL);
+        assertTrue(decodedURL.startsWith("https://www.commcarehq.org"));
+    }
+
     @Test
     public void validateBaseEncodedURLTest() throws UnsupportedEncodingException, Base64DecoderException {
+        // decode a valid URL
         String goodUrlBase = "https://www.commcarehq.org/a/gc/sms/app_info/7c7d49fbef59b703fb468e20d52a21e4/";
-        String badUrlBase = "https://www.corncarehq.org/a/gc/sms/app_info/7c7d49fbef59b703fb468e20d52a21e4/";
         String goodUrl = Base64.encode(goodUrlBase.getBytes("UTF-8"));
-        String badUrl = Base64.encode(badUrlBase.getBytes("UTF-8"));
-
         assertEquals(goodUrlBase, SigningUtil.decodeUrl(goodUrl));
 
+        // try to decode a 'malicious', of non-commcarehq origin, URL
+        String badUrlBase = "https://www.corncarehq.org/a/gc/sms/app_info/7c7d49fbef59b703fb468e20d52a21e4/";
+        String badUrl = Base64.encode(badUrlBase.getBytes("UTF-8"));
         boolean didFail = false;
         try {
             SigningUtil.decodeUrl(badUrl);
