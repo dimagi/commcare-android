af4a063040f00942e92f5aef9f60f203bc07d0b7
Will Pride <wpride@dimagi.com>
2016-11-01 17:50:37 -0700

    remove derp

--- unit-tests/src/org/commcare/CommCareTestApplication.java
+++ unit-tests/src/org/commcare/CommCareTestApplication.java
@@ -58,7 +58,6 @@ public class CommCareTestApplication extends CommCareApplication {
         Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
             public void uncaughtException(Thread thread, Throwable ex) {
-                System.out.println("Derp");
                 Assert.fail(ex.getMessage());
             }
         });
