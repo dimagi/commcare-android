4a5e3b4738f5643d19e027eecfd182d3268d7a45
Phillip Mates <pmates@dimagi.com>
2016-11-18 18:08:48 -0200

    First pass on upgrading mapview

--- app/src/org/commcare/activities/GeoPointMapActivity.java
+++ app/src/org/commcare/activities/GeoPointMapActivity.java
@@ -5,9 +5,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -20,11 +17,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapController;
-import com.google.android.maps.MapView;
-import com.google.android.maps.MyLocationOverlay;
-import com.google.android.maps.Overlay;
 
 import org.commcare.dalvik.R;
 import org.commcare.utils.GeoUtils;
@@ -33,14 +34,16 @@ import org.commcare.views.widgets.GeoPointWidget;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class GeoPointMapActivity extends Activity implements LocationListener {
+/**
+ * Allows location to be chosen using a map instead of current gps coordinates
+ */
+public class GeoPointMapActivity extends Activity implements LocationListener, OnMapReadyCallback {
 
     private MapView mMapView;
+    private GoogleMap map;
     private TextView mLocationStatus;
 
-    private MapController mMapController;
     private LocationManager mLocationManager;
-    private Overlay mLocationOverlay;
 
     private GeoPoint mGeoPoint;
     private Location mLocation;
@@ -62,7 +65,7 @@ public class GeoPointMapActivity extends Activity implements LocationListener {
         loadViewModeState();
         setupButtons();
 
-        loadMapView();
+        loadMapView(savedInstanceState);
 
         mLocationStatus = (TextView)findViewById(R.id.location_status);
         if (inViewMode) {
@@ -75,34 +78,15 @@ public class GeoPointMapActivity extends Activity implements LocationListener {
                     Toast.LENGTH_SHORT).show();
             finish();
         }
-
     }
 
-    private void loadProviders() {
-        List<String> providers = mLocationManager.getProviders(true);
-        for (String provider : providers) {
-            if (provider.equalsIgnoreCase(LocationManager.GPS_PROVIDER)) {
-                mGPSOn = true;
-            }
-            if (provider.equalsIgnoreCase(LocationManager.NETWORK_PROVIDER)) {
-                mNetworkOn = true;
-            }
-        }
-    }
-
-    private void loadMapView() {
+    private void loadMapView(Bundle savedInstanceState) {
+        // Gets the MapView from the XML layout and creates it
         mMapView = (MapView)findViewById(R.id.mapview);
-        mMapController = mMapView.getController();
+        mMapView.onCreate(savedInstanceState);
 
-        mMapView.setBuiltInZoomControls(true);
-        mMapView.setSatellite(false);
-        mMapController.setZoom(16);
-        mLocationOverlay = new MyLocationOverlay(this, mMapView);
-        mMapView.getOverlays().add(mLocationOverlay);
-        if (inViewMode) {
-            Overlay mGeoPointOverlay = new Marker(mGeoPoint);
-            mMapView.getOverlays().add(mGeoPointOverlay);
-        }
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mMapView.getMapAsync(this);
     }
 
     private void setupButtons() {
@@ -130,11 +114,10 @@ public class GeoPointMapActivity extends Activity implements LocationListener {
             mShowLocation.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    mMapController.animateTo(mGeoPoint);
+                    animateToPoint(mGeoPoint.getLatitudeE6(), mGeoPoint.getLongitudeE6());
                 }
             });
         }
-
     }
 
     private void loadViewModeState() {
@@ -143,6 +126,18 @@ public class GeoPointMapActivity extends Activity implements LocationListener {
             double[] location = intent.getDoubleArrayExtra(GeoPointWidget.LOCATION);
             mGeoPoint = new GeoPoint((int)(location[0] * 1E6), (int)(location[1] * 1E6));
             inViewMode = true;
+        }
+    }
+
+    private void loadProviders() {
+        List<String> providers = mLocationManager.getProviders(true);
+        for (String provider : providers) {
+            if (provider.equalsIgnoreCase(LocationManager.GPS_PROVIDER)) {
+                mGPSOn = true;
+            }
+            if (provider.equalsIgnoreCase(LocationManager.NETWORK_PROVIDER)) {
+                mNetworkOn = true;
+            }
         }
     }
 
@@ -166,15 +161,14 @@ public class GeoPointMapActivity extends Activity implements LocationListener {
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mLocationManager.removeUpdates(this);
         }
-        ((MyLocationOverlay)mLocationOverlay).disableMyLocation();
-
     }
 
     @Override
     protected void onResume() {
+        mMapView.onResume();
+
         super.onResume();
 
-        ((MyLocationOverlay)mLocationOverlay).enableMyLocation();
         if (mGPSOn && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
@@ -195,7 +189,7 @@ public class GeoPointMapActivity extends Activity implements LocationListener {
                         new GeoPoint((int)(mLocation.getLatitude() * 1E6),
                                 (int)(mLocation.getLongitude() * 1E6));
 
-                mMapController.animateTo(mGeoPoint);
+                animateToPoint(mGeoPoint.getLatitudeE6(), mGeoPoint.getLongitudeE6());
 
                 if (mLocation.getAccuracy() <= GeoUtils.GOOD_ACCURACY) {
                     returnLocation();
@@ -216,22 +210,37 @@ public class GeoPointMapActivity extends Activity implements LocationListener {
     public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 
-    class Marker extends Overlay {
-        GeoPoint gp = null;
+    @Override
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.setMyLocationEnabled(true);
 
-        Marker(GeoPoint gp) {
-            this.gp = gp;
-        }
+        MapsInitializer.initialize(this);
 
-        @Override
-        public void draw(Canvas canvas, MapView mapView, boolean shadow) {
-            super.draw(canvas, mapView, shadow);
-            Point screenPoint = new Point();
-            mMapView.getProjection().toPixels(gp, screenPoint);
-            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),
-                    R.drawable.ic_maps_indicator_current_position), screenPoint.x, screenPoint.y - 8,
-                    null); // -8 as image is 16px high
+        if (inViewMode) {
+            map.addMarker(new MarkerOptions().position(new LatLng(mGeoPoint.getLatitudeE6(), mGeoPoint.getLongitudeE6())).title("Marker"));
+        } else {
+            map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
         }
+    }
+
+    private void animateToPoint(long lat, long lng) {
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 10);
+        map.animateCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 
 }
