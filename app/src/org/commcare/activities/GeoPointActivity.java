7d5ef0eeb524d12d4d410d7d2398e3752bec66a5
Will Pride <wpride@dimagi.com>
2017-01-25 11:36:19 +0200

    Update args to match translation string

--- app/src/org/commcare/activities/GeoPointActivity.java
+++ app/src/org/commcare/activities/GeoPointActivity.java
@@ -25,6 +25,7 @@ import org.commcare.utils.StringUtils;
 import org.commcare.views.dialogs.GeoProgressDialog;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Set;
 
 /**
@@ -169,9 +170,9 @@ public class GeoPointActivity extends Activity implements LocationListener, Time
     public void onLocationChanged(Location location) {
         this.location = location;
         if (this.location != null) {
-            String[] args = {this.location.getProvider(), truncateDouble(this.location.getAccuracy())};
+            String accuracy = truncateDouble(this.location.getAccuracy());
             locationDialog.setMessage(StringUtils.getStringRobust(this,
-                    R.string.location_provider_accuracy, args));
+                    R.string.location_provider_accuracy, accuracy));
 
             // If location is accurate, we're done
             if (this.location.getAccuracy() <= CommCarePreferences.getGpsWidgetGoodAccuracy()) {
