6244b4db24e67deaa27710850f02197aed0955a5
ALIZA STONE <amstone326@gmail.com>
2017-05-29 14:29:20 -0400

    remove unneeded import

--- app/src/org/commcare/activities/FormEntryActivity.java
+++ app/src/org/commcare/activities/FormEntryActivity.java
@@ -7,7 +7,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.graphics.Rect;
 import android.net.Uri;
