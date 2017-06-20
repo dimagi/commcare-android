408b390495268ef6b583c3e04632147d358ec067
ALIZA STONE <amstone326@gmail.com>
2017-06-20 11:27:44 +0530

    bump openrosa version in request header to indicate compatibility with 422 response

--- app/src/org/commcare/network/HttpRequestGenerator.java
+++ app/src/org/commcare/network/HttpRequestGenerator.java
@@ -233,7 +233,7 @@ public class HttpRequestGenerator implements HttpRequestEndpoints {
 
     private void addHeaders(HttpRequestBase base, String lastToken) {
         //base.addHeader("Accept-Language", lang)
-        base.addHeader("X-OpenRosa-Version", "2.0");
+        base.addHeader("X-OpenRosa-Version", "3.0");
         if (lastToken != null) {
             base.addHeader("X-CommCareHQ-LastSyncToken", lastToken);
         }
