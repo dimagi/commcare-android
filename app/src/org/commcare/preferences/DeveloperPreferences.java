ef0cab9698eb364404b32007fad2dda74a361dd4
ALIZA STONE <amstone326@gmail.com>
2017-08-21 12:46:22 -0400

    add back pref to conditionally hide 'Report Problem'

--- app/src/org/commcare/preferences/DeveloperPreferences.java
+++ app/src/org/commcare/preferences/DeveloperPreferences.java
@@ -58,6 +58,7 @@ public class DeveloperPreferences extends CommCarePreferenceFragment {
     public static final String SHOW_UPDATE_OPTIONS_SETTING = "cc-show-update-target-options";
     public static final String LOCAL_FORM_PAYLOAD_FILE_PATH = "cc-local-form-payload-file-path";
     public final static String REMOTE_FORM_PAYLOAD_URL = "remote-form-payload-url";
+    public final static String HIDE_ISSUE_REPORT = "cc-hide-issue-report";
 
     /**
      * Stores last used password and performs auto-login when that password is
@@ -380,6 +381,10 @@ public class DeveloperPreferences extends CommCarePreferenceFragment {
     public static String getCustomRestoreDocLocation() {
         SharedPreferences properties = CommCareApplication.instance().getCurrentApp().getAppPreferences();
         return properties.getString(PREFS_CUSTOM_RESTORE_DOC_LOCATION, "");
+    }
+
+    public static boolean shouldHideReportIssue() {
+        return doesPropertyMatch(HIDE_ISSUE_REPORT, CommCarePreferences.NO, CommCarePreferences.YES);
     }
 
     private void hideOrShowDangerousSettings() {
