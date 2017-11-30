b5f82bbdfb136a4f7cf8eb74eaa9086816644a76
Will Pride <wpride@dimagi.com>
2017-11-30 11:00:52 -0500

    Add test

---
+++ app/unit-tests/src/org/commcare/android/tests/formentry/IntentCalloutTests.java
@@ -0,0 +1,101 @@
+package org.commcare.android.tests.formentry;
+
+import android.content.Intent;
+import android.os.Environment;
+import android.widget.ImageButton;
+
+import org.commcare.CommCareTestApplication;
+import org.commcare.activities.FormEntryActivity;
+import org.commcare.android.CommCareTestRunner;
+import org.commcare.android.resource.installers.XFormAndroidInstaller;
+import org.commcare.android.util.ActivityLaunchUtils;
+import org.commcare.android.util.TestAppInstaller;
+import org.commcare.dalvik.R;
+import org.commcare.views.widgets.IntegerWidget;
+import org.commcare.views.widgets.IntentWidget;
+import org.commcare.views.widgets.StringNumberWidget;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowEnvironment;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author wpride
+ */
+@Config(application = CommCareTestApplication.class)
+@RunWith(CommCareTestRunner.class)
+public class IntentCalloutTests {
+
+    @Before
+    public void setup() {
+        XFormAndroidInstaller.registerAndroidLevelFormParsers();
+        TestAppInstaller.installAppAndLogin(
+                "jr://resource/commcare-apps/phone_call/profile.ccpr",
+                "test", "123");
+        ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
+    }
+
+    /**
+     * Test different behaviors for possibly grouped intent callout views
+     */
+    @Test
+    public void testIntentCalloutWithData() {
+        ShadowActivity shadowActivity =
+                ActivityLaunchUtils.buildHomeActivityForFormEntryLaunch("m0-f0");
+        Intent formEntryIntent = shadowActivity.getNextStartedActivity();
+
+        FormEntryActivity formEntryActivity = navigateFormStructure(formEntryIntent);
+
+        IntentWidget phoneCallWidget = (IntentWidget) formEntryActivity.getODKView().getWidgets().get(0);
+        Intent intent = phoneCallWidget.getIntentCallout().generate(FormEntryActivity.mFormController.getFormEntryController().getModel().getForm().getEvaluationContext());
+        assert intent.getData().equals("tel:1234567890");
+        assert intent.getAction().equals("android.intent.action.CALL");
+    }
+
+    private FormEntryActivity navigateFormStructure(Intent formEntryIntent) {
+        // launch form entry
+        FormEntryActivity formEntryActivity =
+                Robolectric.buildActivity(FormEntryActivity.class).withIntent(formEntryIntent)
+                        .create().start().resume().get();
+        StringNumberWidget favoriteNumber = (StringNumberWidget)formEntryActivity.getODKView().getWidgets().get(0);
+        favoriteNumber.setAnswer("1234567890");
+        ImageButton nextButton = (ImageButton)formEntryActivity.findViewById(R.id.nav_btn_next);
+        nextButton.performClick();
+        return formEntryActivity;
+    }
+
+    @Test
+    public void testIntentCalloutEmptyData() {
+        ShadowActivity shadowActivity =
+                ActivityLaunchUtils.buildHomeActivityForFormEntryLaunch("m0-f1");
+        Intent formEntryIntent = shadowActivity.getNextStartedActivity();
+        FormEntryActivity formEntryActivity =
+                Robolectric.buildActivity(FormEntryActivity.class).withIntent(formEntryIntent)
+                        .create().start().resume().get();
+        IntentWidget phoneCallWidget = (IntentWidget) formEntryActivity.getODKView().getWidgets().get(0);
+        Intent intent = phoneCallWidget.getIntentCallout().generate(FormEntryActivity.mFormController.getFormEntryController().getModel().getForm().getEvaluationContext());
+        assert intent.getData().equals("");
+        assert intent.getAction().equals("android.intent.action.CALL");
+    }
+
+    @Test
+    public void testIntentCalloutNoData() {
+        ShadowActivity shadowActivity =
+                ActivityLaunchUtils.buildHomeActivityForFormEntryLaunch("m0-f2");
+        Intent formEntryIntent = shadowActivity.getNextStartedActivity();
+        FormEntryActivity formEntryActivity =
+                Robolectric.buildActivity(FormEntryActivity.class).withIntent(formEntryIntent)
+                        .create().start().resume().get();
+        IntentWidget phoneCallWidget = (IntentWidget) formEntryActivity.getODKView().getWidgets().get(0);
+        Intent intent = phoneCallWidget.getIntentCallout().generate(FormEntryActivity.mFormController.getFormEntryController().getModel().getForm().getEvaluationContext());
+        assert intent.getData() == null;
+        assert intent.getAction().equals("android.intent.action.CALL");
+    }
+}
