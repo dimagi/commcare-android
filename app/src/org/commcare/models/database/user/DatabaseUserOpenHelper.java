3cdc6ae8733affebe9c65220c0dc8fbc6c8f86c4
Shubham Goyal <shubham1g5@gmail.com>
2018-03-19 15:00:55 +0530

    corrects comment

--- app/src/org/commcare/models/database/user/DatabaseUserOpenHelper.java
+++ app/src/org/commcare/models/database/user/DatabaseUserOpenHelper.java
@@ -59,7 +59,7 @@ public class DatabaseUserOpenHelper extends SQLiteOpenHelper {
      * V.20 - Migrate index names on indexed fixtures so that multiple fixtures are able to have an index on the same column name
      * V.21 - Reindex all cases to add relationship, and add reasonForQuarantine field to FormRecords
      * V.22 - Add column for appId in entity_cache table
-     * V.23 - Changes FormRecord's 'instanceUri' String field to 'instanceId' Long field
+     * V.23 - Merges InstanceProvider to FormRecord (delete instanceUri, add displayName, filePath and canEditWhenComplete)
      */
 
     private static final int USER_DB_VERSION = 23;
