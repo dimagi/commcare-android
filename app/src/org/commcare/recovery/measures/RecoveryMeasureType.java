a75cc3a7435a8e548da9ce286cb3631d00815a9a
ALIZA STONE <amstone326@gmail.com>
2018-05-08 15:54:01 -0400

    delete unused enum

--- app/src/org/commcare/recovery/measures/RecoveryMeasureType.java
+++
@@ -1,9 +0,0 @@
-package org.commcare.recovery.measures;
-
-/**
- * Created by amstone326 on 4/27/18.
- */
-
-public enum RecoveryMeasureType {
-    REINSTALL_APP, UPDATE_APP, CLEAR_USER_DATA, QUARANTINE_FIRST_FORM
-}
