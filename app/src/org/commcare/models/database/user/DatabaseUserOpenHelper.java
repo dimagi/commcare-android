33fc49ea87eb46ed497a7eb82d61e93527e9e465
Will Pride <wpride@dimagi.com>
2018-06-21 08:44:18 -0400

    increment verison 23 -> 24

--- app/src/org/commcare/models/database/user/DatabaseUserOpenHelper.java
+++ app/src/org/commcare/models/database/user/DatabaseUserOpenHelper.java
@@ -61,9 +61,10 @@ public class DatabaseUserOpenHelper extends SQLiteOpenHelper {
      * V.21 - Reindex all cases to add relationship, and add reasonForQuarantine field to FormRecords
      * V.22 - Add column for appId in entity_cache table
      * V.23 - Merges InstanceProvider to FormRecord (delete instanceUri, add displayName, filePath and canEditWhenComplete)
+     * v.24 - Adds and indexes column for Case external_id
      */
 
-    private static final int USER_DB_VERSION = 23;
+    private static final int USER_DB_VERSION = 24;
 
     private static final String USER_DB_LOCATOR = "database_sandbox_";
 
