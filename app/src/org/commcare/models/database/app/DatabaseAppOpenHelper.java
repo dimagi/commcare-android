25f9dd23e11aedc5dd8038e5c61854f2031b1432
Shubham Goyal <shubham1g5@gmail.com>
2018-07-05 22:48:22 +0530

    Corrects App DB version

--- app/src/org/commcare/models/database/app/DatabaseAppOpenHelper.java
+++ app/src/org/commcare/models/database/app/DatabaseAppOpenHelper.java
@@ -37,7 +37,7 @@ public class DatabaseAppOpenHelper extends SQLiteOpenHelper {
      * V.9 - Adds FormRecord and Instance Record tables, XFormAndroidInstaller: contentUri -> formDefId
      * V.10 - No Change, Added because of incomplete resource table migration for v8 to v9
      */
-    private static final int DB_VERSION_APP = 9;
+    private static final int DB_VERSION_APP = 10;
 
     private static final String DB_LOCATOR_PREF_APP = "database_app_";
 
