528d7f20f86b0de3b4b1670c65ba3f9e8326984c
Radovan Ristovic <radovanr995@gmail.com>
2018-12-14 13:46:28 +0100

    added rtl support viewPager

--- app/src/org/commcare/views/media/AudioPlaybackButtonBase.java
+++ app/src/org/commcare/views/media/AudioPlaybackButtonBase.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
