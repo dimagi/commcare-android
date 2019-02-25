dd6b149657eff41d2eefad4a46ee343aa5294b4d
Shubham Goyal <shubham1g5@gmail.com>
2019-02-25 15:08:49 +0530

    Don't parse the formatted string as double again to retain the formatting

--- app/src/org/commcare/views/widgets/DecimalWidget.java
+++ app/src/org/commcare/views/widgets/DecimalWidget.java
@@ -68,8 +68,8 @@ public class DecimalWidget extends StringWidget {
         if (d != null) {
             Double dAnswer = (Double)getCurrentAnswer().getValue();
             String dString = nf.format(dAnswer);
-            d = Double.parseDouble(dString.replace(',', '.'));
-            mAnswer.setText(d.toString());
+            dString = dString.replace(',', '.');
+            mAnswer.setText(dString);
         }
 
         // disable if read only
