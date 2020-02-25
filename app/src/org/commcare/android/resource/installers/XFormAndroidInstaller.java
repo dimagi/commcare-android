f5d9c001fcc0624778c6e257f95bb18e707c6ddf
Shubham Goyal <shubham1g5@gmail.com>
2020-02-25 22:29:03 +0530

    comment

--- app/src/org/commcare/android/resource/installers/XFormAndroidInstaller.java
+++ app/src/org/commcare/android/resource/installers/XFormAndroidInstaller.java
@@ -78,7 +78,7 @@ public class XFormAndroidInstaller extends FileSystemInstaller {
         if (platform.getFormDefId(namespace) == -1) {
             platform.registerXmlns(namespace, formDefId);
         } else {
-            // Only overwrite a form if the resourceVersion of the new form is higher
+            // Only overwrites the existing form if the resourceVersion of the new form is higher than the existing one
             FormDefRecord existingForm = FormDefRecord.getFormDef(platform.getFormDefStorage(), platform.getFormDefId(namespace));
             FormDefRecord newForm = FormDefRecord.getFormDef(platform.getFormDefStorage(), formDefId);
             if (newForm.getResourceVersion() > existingForm.getResourceVersion()) {
