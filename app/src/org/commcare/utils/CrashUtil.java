cc26aac05853e929b4dae7931f67858d861e71b9
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2020-02-27 12:46:36 +0530

    Add user name as custom key in FirebaseCrashlytics logging

--- app/src/org/commcare/utils/CrashUtil.java
+++ app/src/org/commcare/utils/CrashUtil.java
@@ -16,6 +16,7 @@ public class CrashUtil {
     private static final String APP_NAME = "app_name";
     private static final String DOMAIN = "domain";
     private static final String DEVICE_ID = "device_id";
+    private static final String NAME = "name";
 
     private static boolean crashlyticsEnabled = BuildConfig.USE_CRASHLYTICS;
 
@@ -45,6 +46,7 @@ public class CrashUtil {
     public static void registerUserData() {
         if (crashlyticsEnabled) {
             FirebaseCrashlytics.getInstance().setUserId(CommCareApplication.instance().getCurrentUserId());
+            FirebaseCrashlytics.getInstance().setCustomKey(NAME, ReportingUtils.getUser());
         }
     }
 
