93e70a0a7c43d3a036fbb80f2a0a75125aa30bee
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2020-04-17 16:18:29 +0530

    Restrict repeated checking of in-app update in a day

--- app/src/org/commcare/appupdate/AppUpdateControllerFactory.java
+++ app/src/org/commcare/appupdate/AppUpdateControllerFactory.java
@@ -5,6 +5,10 @@ import android.os.Build;
 
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
 
+import org.commcare.preferences.HiddenPreferences;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * A factory that creates {@link AppUpdateController} instance.
  * @author $|-|!˅@M
@@ -17,7 +21,9 @@ public class AppUpdateControllerFactory {
      * @return A new {@link FlexibleAppUpdateController} to use for in-app updates
      */
     public static FlexibleAppUpdateController create(Runnable callback, Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        long lastCheckTime = HiddenPreferences.getLastInAppUpdateCheckTime();
+        boolean isLastUpdateOverOneDay = (System.currentTimeMillis() - lastCheckTime) > TimeUnit.DAYS.toMillis(1);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && isLastUpdateOverOneDay) {
             // In-app updates works only with devices running Android 5.0 (API level 21) or higher
             return new CommcareFlexibleAppUpdateManager(callback, AppUpdateManagerFactory.create(context));
         } else {
