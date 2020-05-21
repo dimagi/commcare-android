0fafd9b97955bc329e4fd62db25d7a5d0b2faac4
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2020-05-21 15:34:16 +0530

    Add repeated rules and permissions to base class

--- app/instrumentation-tests/src/org/commcare/tests/FormAttachmentUploadTest.java
+++ app/instrumentation-tests/src/org/commcare/tests/FormAttachmentUploadTest.java
@@ -1,25 +1,18 @@
 package org.commcare.tests;
 
-import android.Manifest;
 import androidx.test.espresso.action.GeneralLocation;
 import androidx.test.espresso.action.GeneralSwipeAction;
 import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Swipe;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.rule.GrantPermissionRule;
 import org.commcare.CommCareApplication;
-import org.commcare.activities.DispatchActivity;
 import org.commcare.activities.DrawActivity;
-import org.commcare.activities.FormEntryActivity;
 import org.commcare.dalvik.R;
 import org.commcare.modern.util.Pair;
 import org.commcare.utils.HQApi;
 import org.commcare.utils.Utility;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import java.util.concurrent.TimeUnit;
@@ -42,25 +35,7 @@ import static junit.framework.Assert.assertTrue;
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class FormAttachmentUploadTest {
-
-    @Rule
-    public ActivityTestRule<DispatchActivity> activityTestRule = new ActivityTestRule<>(DispatchActivity.class);
-
-    @Rule
-    public IntentsTestRule<FormEntryActivity> intentsRule = new IntentsTestRule<>(FormEntryActivity.class);
-
-    @Rule
-    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(
-            Manifest.permission.READ_PHONE_STATE,
-            Manifest.permission.CALL_PHONE,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.CAMERA
-    );
+public class FormAttachmentUploadTest extends BaseTest {
 
     @Before
     public void setup() {
