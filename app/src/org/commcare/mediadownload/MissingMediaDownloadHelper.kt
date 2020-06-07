a5fb86158cb77cb120d00ae8a1e34026b9987108
Shubham Goyal <shubham1g5@gmail.com>
2020-06-07 17:01:35 +0530

    Error Handling

--- app/src/org/commcare/mediadownload/MissingMediaDownloadHelper.kt
+++ app/src/org/commcare/mediadownload/MissingMediaDownloadHelper.kt
@@ -1,34 +1,34 @@
 package org.commcare.mediadownload
 
-import android.util.Log
 import androidx.work.*
 import kotlinx.coroutines.*
 import org.commcare.CommCareApplication
 import org.commcare.android.resource.installers.MediaFileAndroidInstaller
 import org.commcare.dalvik.R
 import org.commcare.engine.resource.AndroidResourceUtils
+import org.commcare.engine.resource.AppInstallStatus
 import org.commcare.engine.resource.ResourceInstallUtils
+import org.commcare.engine.resource.installers.LocalStorageUnavailableException
 import org.commcare.preferences.HiddenPreferences
-import org.commcare.resources.model.InstallCancelled
-import org.commcare.resources.model.Resource
-import org.commcare.resources.model.ResourceTable
-import org.commcare.resources.model.TableStateListener
+import org.commcare.resources.model.*
+import org.commcare.tasks.ResultAndError
+import org.commcare.update.UpdateHelper
+import org.commcare.util.LogTypes
 import org.commcare.utils.AndroidCommCarePlatform
 import org.commcare.utils.FileUtil
 import org.commcare.views.dialogs.PinnedNotificationWithProgress
 import org.javarosa.core.services.Logger
+import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 // Contains helper functions to download lazy or missing media resources
 object MissingMediaDownloadHelper : TableStateListener, InstallCancelled {
 
-    var resourceInProgress: Resource? = null
-        private set
-
+    private var resourceInProgress: Resource? = null
     private val jobs = ArrayList<Job>()
     private lateinit var mPinnedNotificationProgress: PinnedNotificationWithProgress
 
-
+    // constants
     private const val BACK_OFF_DELAY = 5 * 60 * 1000L // 5 mins
     private const val REQUEST_NAME = "missing_media_download_request"
 
@@ -71,29 +71,41 @@ object MissingMediaDownloadHelper : TableStateListener, InstallCancelled {
      *
      * Downloads any missing lazy resources, make sure to call this on background thread
      */
-    fun downloadAllLazyMedia(): Boolean {
+    fun downloadAllLazyMedia(): AppInstallStatus {
         val platform = CommCareApplication.instance().commCarePlatform
         val global = platform.globalResourceTable
 
         global.setInstallCancellationChecker(this)
-
         startPinnedNotification()
 
         val lazyResourceIds = global.lazyResourceIds
-
         lazyResourceIds.asSequence()
-                .withIndex()
-                .onEach { incrementProgress(it.index + 1, lazyResourceIds.size) }
-                .takeWhile { !wasInstallCancelled() }
-                .map { global.getResource(it.value) }
-                .filter { isResourceMissing(it) }
-                .takeWhile { !wasInstallCancelled() }
-                .onEach { recoverResource(platform, it) }
-                .toList()
+                .runCatching {
+                    withIndex()
+                            .onEach { incrementProgress(it.index + 1, lazyResourceIds.size) }
+                            .takeWhile { !wasInstallCancelled() }
+                            .map { global.getResource(it.value) }
+                            .filter { isResourceMissing(it) }
+                            .takeWhile { !wasInstallCancelled() }
+                            .onEach { recoverResource(platform, it) }.toList()
+                }.onFailure {
+                    Logger.log(LogTypes.TYPE_MAINTENANCE, "An error occured while lazy downloading a media resource : " + it.message)
+                    return handleRecoverResourceFailure(it).data
+                }
 
         cancelNotification()
         global.setInstallCancellationChecker(null)
-        return true
+        return AppInstallStatus.Installed
+    }
+
+    private fun handleRecoverResourceFailure(it: Throwable): ResultAndError<AppInstallStatus> {
+        return when (it) {
+            is InvalidResourceException -> ResultAndError(AppInstallStatus.InvalidResource, it.message)
+            is LocalStorageUnavailableException -> ResultAndError(AppInstallStatus.NoLocalStorage, it.message)
+            is UnresolvedResourceException -> ResultAndError(ResourceInstallUtils.processUnresolvedResource(it), it.message)
+            is InstallCancelledException -> ResultAndError(AppInstallStatus.Cancelled)
+            else -> ResultAndError(AppInstallStatus.UnknownFailure, it.message)
+        }
     }
 
     private fun isResourceMissing(it: Resource): Boolean {
@@ -111,13 +123,11 @@ object MissingMediaDownloadHelper : TableStateListener, InstallCancelled {
         jobs.add(
                 CoroutineScope(Dispatchers.Default).launch {
                     var result: MissingMediaDownloadResult = MissingMediaDownloadResult.Error("Unknown Error")
-                    try {
+                    kotlin.runCatching {
                         result = downloadMissingMediaResource(mediaUri)
-                    } catch (e: Exception) {
-                        Logger.exception(" An error occured while recovering a missing resource", e);
-                        withContext(Dispatchers.Main) {
-                            missingMediaDownloadListener.onComplete(MissingMediaDownloadResult.Exception(e))
-                        }
+                    }.onFailure {
+                        Logger.exception(" An error occured while recovering a missing resource", it as Exception?)
+                        result = MissingMediaDownloadResult.Error(handleRecoverResourceFailure(it).errorMessage)
                     }
 
                     withContext(Dispatchers.Main) {
@@ -153,16 +163,13 @@ object MissingMediaDownloadHelper : TableStateListener, InstallCancelled {
         resourceInProgress = it
         platform.globalResourceTable.recoverResource(it, platform, ResourceInstallUtils.getProfileReference())
         resourceInProgress = null
-
     }
-
 
     // Cancels all missing media work
     @JvmStatic
     fun cancelAllDownloads() {
         jobs.map { job -> job.cancel() }
     }
-
 
     override fun incrementProgress(complete: Int, total: Int) {
         updateNotification(complete, total)
@@ -180,12 +187,10 @@ object MissingMediaDownloadHelper : TableStateListener, InstallCancelled {
         return installCancelled != null && installCancelled!!.wasInstallCancelled()
     }
 
-
     private fun startPinnedNotification() {
         mPinnedNotificationProgress = PinnedNotificationWithProgress(CommCareApplication.instance(), "media.pinned.download",
                 "media.pinned.progress", R.drawable.update_download_icon)
     }
-
 
     private fun updateNotification(complete: Int, total: Int) {
         mPinnedNotificationProgress.handleTaskUpdate(complete, total)
--- app/src/org/commcare/mediadownload/MissingMediaDownloadWorker.kt
+++ app/src/org/commcare/mediadownload/MissingMediaDownloadWorker.kt
@@ -3,6 +3,7 @@ package org.commcare.mediadownload
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import org.commcare.engine.resource.AppInstallStatus
 import org.commcare.resources.model.InstallCancelled
 
 class MissingMediaDownloadWorker(appContext: Context, workerParams: WorkerParameters)
@@ -10,14 +11,8 @@ class MissingMediaDownloadWorker(appContext: Context, workerParams: WorkerParame
 
     override suspend fun doWork(): Result {
         MissingMediaDownloadHelper.installCancelled = this
-        kotlin.runCatching {
-            MissingMediaDownloadHelper.downloadAllLazyMedia()
-        }.onSuccess {
-            return Result.success()
-        }.onFailure {
-            return Result.failure()
-        }
-        return Result.failure()
+        val result = MissingMediaDownloadHelper.downloadAllLazyMedia()
+        return if (result == AppInstallStatus.Installed) Result.success() else Result.retry()
     }
 
     override fun wasInstallCancelled(): Boolean {
