611b04e3c6467b2411efb1e9a7c8ea50f83bc0ac
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2020-09-07 12:07:13 +0530

    Setup Screenshot tests

---
+++ app/instrumentation-tests/src/org/commcare/screenshotTests/LoginActivityScreenshot.kt
@@ -0,0 +1,23 @@
+package org.commcare.screenshotTests
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.facebook.testing.screenshot.Screenshot
+import org.commcare.CommCareInstrumentationTestApplication
+
+import org.commcare.androidTests.BaseTest
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class LoginActivityScreenshot: BaseTest() {
+
+    @Test
+    fun testLoginActivityUI() {
+        val application = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+                as CommCareInstrumentationTestApplication
+        Screenshot.snapActivity(application.currentActivity).record()
+    }
+}
