5c120705224ab1cb5cd4cb5b984dbfb46de30ae0
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2020-10-20 13:01:16 +0530

    Add option to re-run failing espresso tests

--- scripts/browserstack.py
+++ scripts/browserstack.py
@@ -14,7 +14,7 @@ def appendData(command, dataUrl):
     var = "file=@{}".format(dataUrl)
     return command + " " + json.dumps(var)
 
-def buildTestCommand(appToken, testToken):
+def buildTestCommand(appToken, testToken, classes=None):
     test = {}
     if "BROWSERSTACK_DEVICES" in os.environ:
         list = os.environ["BROWSERSTACK_DEVICES"]
@@ -27,7 +27,67 @@ def buildTestCommand(appToken, testToken):
     test["deviceLogs"] = True
     test["testSuite"] = testToken
     test["networkLogs"] = True
+
+    if classes:
+        test["class"] = classes
+
     return json.dumps(json.dumps(test))
+
+def isSuccessfulBuild(buildId):
+    resultCommand = 'curl -u "{}:{}" -X GET "https://api-cloud.browserstack.com/app-automate/espresso/v2/builds/{}"'.format(userName, password, buildId)
+    result = subprocess.Popen(shlex.split(resultCommand), stdout=PIPE, stderr=None, shell=False)
+    status = json.loads(result.communicate()[0])["status"]
+
+    while status == "running":
+        print("Waiting for tests on browserstack to complete")
+        time.sleep(120)
+        result = subprocess.Popen(shlex.split(resultCommand), stdout=PIPE, stderr=None, shell=False)
+        status = json.loads(result.communicate()[0])["status"]
+
+    return status
+
+
+def testResult(buildId):
+    status = isSuccessfulBuild(buildId)
+
+    # if test succeeded then we can simply return from here.
+    if (status == "passed"):
+        return
+
+    # Otherwise run the failing test one more time.
+
+    # Get the sessionID from test result
+    resultCommand = 'curl -u "{}:{}" -X GET "https://api-cloud.browserstack.com/app-automate/espresso/v2/builds/{}"'.format(userName, password, buildId)
+    result = subprocess.Popen(shlex.split(resultCommand), stdout=PIPE, stderr=None, shell=False)
+    sessionId = json.loads(result.communicate()[0])["devices"][0]["sessions"][0]["id"]
+
+    # Gather the sessionDetails
+    testDetailsCommand = 'curl -u "{}:{}" -X GET "https://api-cloud.browserstack.com/app-automate/espresso/v2/builds/{}/sessions/{}"'.format(userName, password, buildId, sessionId)
+    testDetailsResult = subprocess.Popen(shlex.split(testDetailsCommand), stdout=PIPE, stderr=None, shell=False)
+    testcases = json.loads(testDetailsResult.communicate()[0])["testcases"]["data"]
+
+    # Create an array of failing classes.
+    classes = []
+    for testcase in testcases:
+        methods = testcase["testcases"]
+        for method in methods:
+            if (method["status"] != "passed"):
+                classes.append("org.commcare.androidTests." + testcase["class"])
+                break
+
+    # Now that we know all the test-classes that are failing, we can re-run those.
+
+    runConfig = buildTestCommand(appToken, testToken, classes)
+    runCmd = 'curl -X POST "{}" -d \ {} -H "Content-Type: application/json" -u "{}:{}"'.format(espressoUrl, runConfig, userName, password)
+    output = subprocess.Popen(shlex.split(runCmd), stdout=PIPE, stderr=None, shell=False).communicate()
+    buildId = json.loads(output[0])["build_id"]
+
+    status = isSuccessfulBuild(buildId)
+
+    if (status != "passed"):
+        print("Instrumentation Tests Failed. Visit browserstack dashboard for more details.")
+        sys.exit(-1)
+
 
 if __name__ == "__main__":
 
@@ -65,18 +125,4 @@ if __name__ == "__main__":
     buildId = json.loads(output[0])["build_id"]
 
     # Get the result of the test build
-
-    resultCommand = 'curl -u "{}:{}" -X GET "https://api-cloud.browserstack.com/app-automate/espresso/builds/{}"'.format(userName, password, buildId)
-
-    result = subprocess.Popen(shlex.split(resultCommand), stdout=PIPE, stderr=None, shell=False)
-    status = json.loads(result.communicate()[0])["status"]
-
-    while status == "running":
-        print("Waiting for tests on browserstack to complete")
-        time.sleep(60)
-        result = subprocess.Popen(shlex.split(resultCommand), stdout=PIPE, stderr=None, shell=False)
-        status = json.loads(result.communicate()[0])["status"]
-
-    if (status == "failed" or status == "error"):
-        print("Instrumentation Tests Failed. Visit browserstack dashboard for more details.")
-        sys.exit(-1)
+    testResult(buildId)
