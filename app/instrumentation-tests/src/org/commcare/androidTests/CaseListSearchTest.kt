800a73667f5c5f5491c6030a332bf90aeca5519f
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2020-10-21 12:17:37 +0530

    Fix import issues

--- app/instrumentation-tests/src/org/commcare/androidTests/CaseListSearchTest.kt
+++ app/instrumentation-tests/src/org/commcare/androidTests/CaseListSearchTest.kt
@@ -1,15 +1,18 @@
 package org.commcare.androidTests
 
+import android.content.res.Resources
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.commcare.dalvik.R
+import org.commcare.utils.CustomMatchers
 import org.commcare.utils.InstrumentationUtility
 import org.hamcrest.Matchers.*
 import org.junit.Before
