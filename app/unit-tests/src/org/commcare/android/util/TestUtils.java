7911d847eedcbc47b56711548248efdbca87a0ff
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2021-08-31 12:52:22 +0530

    Replace System.class with getClass() or ClassName.class

--- app/unit-tests/src/org/commcare/android/util/TestUtils.java
+++ app/unit-tests/src/org/commcare/android/util/TestUtils.java
@@ -156,7 +156,7 @@ public class TestUtils {
         DataModelPullParser parser;
 
         try {
-            InputStream is = System.class.getResourceAsStream(resourcePath);
+            InputStream is = TestUtils.class.getResourceAsStream(resourcePath);
 
             parser = new DataModelPullParser(is, getFactory(db, bulkProcessingEnabled), true, true);
             parser.parse();
@@ -186,7 +186,7 @@ public class TestUtils {
         }
 
         try {
-            InputStream is = System.class.getResourceAsStream(resourcePath);
+            InputStream is = TestUtils.class.getResourceAsStream(resourcePath);
 
             parser = new DataModelPullParser(is, androidTransactionFactory, true, true);
             parser.parse();
