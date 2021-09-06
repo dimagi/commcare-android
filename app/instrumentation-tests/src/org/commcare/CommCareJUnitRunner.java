b7a6b686ac5837e7e04c48590e99a53bf2b0b078
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2021-09-06 19:17:04 +0530

    Fixes androidTest apk crash
     java.lang.NoClassDefFoundError: Failed resolution of: Lkotlin/reflect/jvm/internal/KPropertyImpl;
     	at java.lang.Class.classForName(Native Method)
     	at java.lang.Class.forName(Class.java:454)
     	at androidx.test.internal.runner.TestLoader.doCreateRunner(TestLoader.java:72)
     	at androidx.test.internal.runner.TestLoader.getRunnersFor(TestLoader.java:104)
     	at androidx.test.internal.runner.TestRequestBuilder.build(TestRequestBuilder.java:793)
     	at androidx.test.runner.AndroidJUnitRunner.buildRequest(AndroidJUnitRunner.java:547)
     	at androidx.test.runner.AndroidJUnitRunner.onStart(AndroidJUnitRunner.java:390)
     	at android.app.Instrumentation$InstrumentationThread.run(Instrumentation.java:2213)
     Caused by: java.lang.ClassNotFoundException: kotlin.reflect.jvm.internal.KPropertyImpl

--- app/instrumentation-tests/src/org/commcare/CommCareJUnitRunner.java
+++ app/instrumentation-tests/src/org/commcare/CommCareJUnitRunner.java
@@ -2,10 +2,18 @@ package org.commcare;
 
 import android.app.Application;
 import android.content.Context;
-
+import android.os.Bundle;
 import androidx.test.runner.AndroidJUnitRunner;
 
 public class CommCareJUnitRunner extends AndroidJUnitRunner {
+
+    @Override
+    public void onCreate(Bundle arguments) {
+        // A fix for java.lang.NoClassDefFoundError: Failed resolution of: Lkotlin/reflect/jvm/internal/KPropertyImpl;
+        arguments.putString("package", "org.commcare.androidTests");
+        super.onCreate(arguments);
+    }
+
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         return super.newApplication(cl, CommCareInstrumentationTestApplication.class.getName(), context);
