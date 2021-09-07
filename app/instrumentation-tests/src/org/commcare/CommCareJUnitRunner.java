e7072564a4592d33fbb4803a34b96bf8b05fbf65
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2021-09-07 17:49:00 +0530

    Remove mockk-android
    Also reverts https://github.com/dimagi/commcare-android/pull/2524/commits/b7a6b686ac5837e7e04c48590e99a53bf2b0b078 \n Mockk android causes a lot of issues like OOM in jenkins, noclassdeffound errors, java.lang.verifyErrors, and more importantly it doesn't suppport mocking objects below android pie

--- app/instrumentation-tests/src/org/commcare/CommCareJUnitRunner.java
+++ app/instrumentation-tests/src/org/commcare/CommCareJUnitRunner.java
@@ -2,17 +2,9 @@ package org.commcare;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
 import androidx.test.runner.AndroidJUnitRunner;
 
 public class CommCareJUnitRunner extends AndroidJUnitRunner {
-
-    @Override
-    public void onCreate(Bundle arguments) {
-        // A fix for java.lang.NoClassDefFoundError: Failed resolution of: Lkotlin/reflect/jvm/internal/KPropertyImpl;
-        arguments.putString("package", "org.commcare.androidTests");
-        super.onCreate(arguments);
-    }
 
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
