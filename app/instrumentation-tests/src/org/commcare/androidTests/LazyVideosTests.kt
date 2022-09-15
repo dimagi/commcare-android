7d1d220a08941afd1cdc707cbb6bb1c1aa2f66cc
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2022-09-15 15:51:49 +0530

    updated lazy_videos_tests.ccz,  added new functions in LazyVideosTests.kt, updated test steps in testVideosWithValidReference()

--- app/instrumentation-tests/src/org/commcare/androidTests/LazyVideosTests.kt
+++ app/instrumentation-tests/src/org/commcare/androidTests/LazyVideosTests.kt
@@ -3,10 +3,13 @@ package org.commcare.androidTests
 
 import android.content.Intent
 import android.os.Build
+import android.util.Log
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -42,7 +45,11 @@ class LazyVideosTests : BaseTest() {
 
     @Before
     fun setup() {
-        installApp(APP_NAME, CCZ_NAME)
+        InstrumentationUtility.uninstallCurrentApp()
+        InstrumentationUtility.installApp(CCZ_NAME)
+        Espresso.pressBack()
+        updateApp()
+
     }
 
     @After
@@ -52,34 +59,61 @@ class LazyVideosTests : BaseTest() {
 
     @Test
     fun testVideosWithReferences() {
-//        testVideosWithValidReference()
+        testVideosWithValidReference()
         testVideosWithNoReferences()
     }
 
+    private fun updateApp(){
+        InstrumentationUtility.openOptionsMenu()
+        onView(withText("Go To App Manager"))
+            .perform(click())
+        onView(withText(APP_NAME)).perform(click())
+        onView(withText("Update"))
+            .perform(click())
+        InstrumentationUtility.waitForView(withSubstring("Update to version"))
+        gotoLogin()
+    }
+
+    private fun gotoLogin() {
+        for (i in 0..2) { // Try atmost 3 times.
+            if (onView(withId(R.id.edit_username)).isPresent()) {
+                return
+            } else {
+                Espresso.pressBack()
+            }
+        }
+    }
     private fun testVideosWithValidReference() {
         InstrumentationUtility.login("test1", "123")
         InstrumentationUtility.openForm(1, 0)
         InstrumentationUtility.waitForView(withId(R.id.video_button))
-        onView(withTagValue(CoreMatchers.`is`(R.drawable.update_download_icon))).check(matches(isDisplayed()))
-        if (onView(withText(R.string.video_download_prompt)).isPresent()) {
+        if ((onView(withTagValue(CoreMatchers.`is`(R.drawable.update_download_icon))).isPresent())
+            or (onView(withText(R.string.video_download_prompt)).isPresent())) {
+            InstrumentationUtility.stubIntentWithAction(Intent.ACTION_SEND)
+            onView(withTagValue(CoreMatchers.`is`(R.drawable.update_download_icon))).perform(click())
             onView(withId(R.id.video_button)).perform(click())
-            onView(withSubstring("Download started")).check(matches(isDisplayed()))
-            InstrumentationUtility.waitForView(withText("Download complete"))
-            onView(withTagValue(CoreMatchers.`is`(android.R.drawable.ic_media_play))).check(matches(isDisplayed()))
-        } else {
-            InstrumentationUtility.stubIntentWithAction(Intent.ACTION_VIEW)
-            onView(withId(R.id.video_button)).perform(click())
-            InstrumentationUtility.nextPage()
+            InstrumentationUtility.waitForView(withSubstring("Download in Progress"))
+            InstrumentationUtility.waitForView(withSubstring("Download complete"))
+            InstrumentationUtility.waitForView(withTagValue(CoreMatchers.`is`(android.R.drawable.ic_media_play)))
         }
+        InstrumentationUtility.stubIntentWithAction(Intent.ACTION_VIEW)
+        onView(withId(R.id.video_button)).perform(click())
+        InstrumentationUtility.nextPage()
+        Thread.sleep(5000)
         assertTrue(onView(allOf(withId(R.id.inline_video_view))).isPresent())
         InstrumentationUtility.nextPage()
-        onView(withTagValue(CoreMatchers.`is`(android.R.drawable.ic_media_play))).check(matches(isDisplayed()))
+        InstrumentationUtility.waitForView(CustomMatchers.withDrawable(
+            CommCareApplication.instance(),
+            R.drawable.icon_info_outline_lightcool
+        ))
+
         onView(
             CustomMatchers.withDrawable(
                 CommCareApplication.instance(),
                 R.drawable.icon_info_outline_lightcool
             )
         ).perform(click())
+        onView(withTagValue(CoreMatchers.`is`(android.R.drawable.ic_media_play))).check(matches(isDisplayed()))
         InstrumentationUtility.stubIntentWithAction(Intent.ACTION_VIEW)
         onView(withId(R.id.video_button)).perform(click())
         onView(withText("OK")).perform(click())
@@ -91,6 +125,7 @@ class LazyVideosTests : BaseTest() {
                 R.drawable.icon_info_outline_lightcool
             )
         ).perform(click())
+        onView(withTagValue(CoreMatchers.`is`(android.R.drawable.ic_media_play))).check(matches(isDisplayed()))
         InstrumentationUtility.stubIntentWithAction(Intent.ACTION_VIEW)
         onView(withId(R.id.video_button)).perform(click())
         onView(withText("OK")).perform(click())
