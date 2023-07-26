68209a8dbc87fa398adc2ab9726480249b94b7fc
Dave Viggiano <dviggiano@dimagi.com>
2023-07-25 23:18:54 -0400

    BROKEN: Attempting to use getRecordsForValues to query app_data records. Added Registering state to ConnectIDManager for when user is mid-registration. Fixed some issues raised in PR.

--- app/src/org/commcare/android/database/global/models/ConnectKeyRecord.java
+++ app/src/org/commcare/android/database/global/models/ConnectKeyRecord.java
@@ -7,18 +7,14 @@ import org.commcare.modern.database.Table;
 @Table(ConnectKeyRecord.STORAGE_KEY)
 public class ConnectKeyRecord extends Persisted {
     public static final String STORAGE_KEY = "connect_key";
-
-    @Persisting(1)
-    String userId;
     @Persisting(2)
     String encryptedPassphrase;
 
     public ConnectKeyRecord() { }
-    public ConnectKeyRecord(String userId, String encryptedPassphrase) {
-        this.userId = userId;
+
+    public ConnectKeyRecord(String encryptedPassphrase) {
         this.encryptedPassphrase = encryptedPassphrase;
     }
 
-    public String getUserID() { return userId; }
     public String getEncryptedPassphrase() { return encryptedPassphrase; }
 }
