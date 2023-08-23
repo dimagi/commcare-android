105f82e717d7656cfc33929089c9397fcab76cec
Dave Viggiano <dviggiano@dimagi.com>
2023-08-23 15:55:36 -0400

    Addressing more lint warnings, mostly code formatting and adding Javadoc comments

--- app/src/org/commcare/android/database/global/models/ConnectKeyRecord.java
+++ app/src/org/commcare/android/database/global/models/ConnectKeyRecord.java
@@ -4,17 +4,23 @@ import org.commcare.android.storage.framework.Persisted;
 import org.commcare.models.framework.Persisting;
 import org.commcare.modern.database.Table;
 
+/**
+ * @author dviggiano
+ */
 @Table(ConnectKeyRecord.STORAGE_KEY)
 public class ConnectKeyRecord extends Persisted {
     public static final String STORAGE_KEY = "connect_key";
     @Persisting(1)
     String encryptedPassphrase;
 
-    public ConnectKeyRecord() { }
+    public ConnectKeyRecord() {
+    }
 
     public ConnectKeyRecord(String encryptedPassphrase) {
         this.encryptedPassphrase = encryptedPassphrase;
     }
 
-    public String getEncryptedPassphrase() { return encryptedPassphrase; }
+    public String getEncryptedPassphrase() {
+        return encryptedPassphrase;
+    }
 }
