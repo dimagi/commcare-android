beba3c228641eac35f2afa51cb42cc802e179c32
Ahmad Vazirna <avazirna@gmail.com>
2023-08-25 15:11:46 +0200

    Pass on logged in user ID to data update broadcast

--- app/src/org/commcare/sync/ExternalDataUpdateHelper.java
+++ app/src/org/commcare/sync/ExternalDataUpdateHelper.java
@@ -4,6 +4,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
+import org.commcare.CommCareApplication;
+
 import java.util.ArrayList;
 
 import javax.annotation.Nullable;
@@ -19,6 +21,11 @@ public class ExternalDataUpdateHelper {
         Intent i = new Intent("org.commcare.dalvik.api.action.data.update");
         if (updatedCases != null) {
             i.putStringArrayListExtra("cases", updatedCases);
+        }
+        // This is to be used to skip any cases that are not owned by the user but that might be
+        // in the database at the time of the data refresh
+        if (CommCareApplication.instance().getSession().isActive()) {
+            i.putExtra("logged-in-user-id", CommCareApplication.instance().getCurrentUserId());
         }
         c.sendBroadcast(i, "org.commcare.dalvik.provider.cases.read");
 
