8562c78d6b44963b35b9f40e103e49b4a6e0860f
Ahmad Vazirna <avazirna@gmail.com>
2023-08-30 11:17:47 +0200

    Add method to convert Base64 string to map

--- commcare-support-library/src/main/java/org/commcare/commcaresupportlibrary/BiometricUtils.java
+++ commcare-support-library/src/main/java/org/commcare/commcaresupportlibrary/BiometricUtils.java
@@ -3,6 +3,8 @@ package org.commcare.commcaresupportlibrary;
 import android.util.Base64;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,5 +33,35 @@ public class BiometricUtils {
         }
         String templatesInJson = new Gson().toJson(templatesBase64Encoded);
         return Base64.encodeToString(templatesInJson.getBytes(), Base64.DEFAULT);
+    }
+
+    /**
+     * This method converts the Base64 encoded biometric templates to its original form
+     * @param base64EncodedTemplates String containing Base64 encoded biometric templates
+     * @return Map containing biometric templates
+     */
+    public static Map<BiometricUtils.BiometricIdentifier, byte[]> convertBase64StringTemplatesToMap(String base64EncodedTemplates){
+        if (base64EncodedTemplates == null || base64EncodedTemplates.isEmpty()) {
+            return null;
+        }
+
+        Map<Integer, String> templatesBase64Encoded = null;
+        try {
+            String templatesInJson = new String(Base64.decode(base64EncodedTemplates.getBytes(),
+                    Base64.DEFAULT));
+            templatesBase64Encoded = new Gson().fromJson(templatesInJson,
+                    (new TypeToken<HashMap<Integer, String>>() {}.getType()));
+        }
+        catch(IllegalArgumentException | UnsupportedOperationException | JsonSyntaxException e){
+            return null;
+        }
+
+        Map<BiometricUtils.BiometricIdentifier, byte[]> templates
+                = new HashMap<>(templatesBase64Encoded.size());
+        for (Map.Entry<Integer, String> template : templatesBase64Encoded.entrySet()) {
+            templates.put(BiometricIdentifier.values()[template.getKey()],
+                    Base64.decode(template.getValue(), Base64.DEFAULT));
+        }
+        return templates;
     }
 }
