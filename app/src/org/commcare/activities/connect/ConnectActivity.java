f2eee8096914f4f68682de00956398976b0e0d7c
Shubham Goyal <shubham1g5@gmail.com>
2023-09-18 12:39:46 -0500

    Set up Download fragment to download a commcare app and update progress accordingly

--- app/src/org/commcare/activities/connect/ConnectActivity.java
+++ app/src/org/commcare/activities/connect/ConnectActivity.java
@@ -2,18 +2,22 @@ package org.commcare.activities.connect;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
 import org.commcare.activities.CommCareActivity;
 import org.commcare.dalvik.R;
-import org.commcare.views.dialogs.CustomProgressDialog;
+import org.commcare.fragments.connect.ConnectDownloadingFragment;
+import org.commcare.tasks.ResourceEngineListener;
 
 import javax.annotation.Nullable;
 
-public class ConnectActivity extends CommCareActivity<ConnectActivity> {
+public class ConnectActivity extends CommCareActivity<ResourceEngineListener> {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_connect);
-
         setTitle(getString(R.string.connect_title));
     }
 
@@ -22,8 +26,24 @@ public class ConnectActivity extends CommCareActivity<ConnectActivity> {
         return false;
     }
 
-//    @Override
-//    public CustomProgressDialog generateProgressDialog(int taskId) {
-//        return CustomProgressDialog.newInstance(null, getString(R.string.please_wait), taskId);
-//    }
+    @Override
+    public ResourceEngineListener getReceiver() {
+        ConnectDownloadingFragment downloadFragment = getConnectDownloadFragment();
+        if (downloadFragment != null) {
+            return downloadFragment;
+        }
+        return null;
+    }
+
+    @Nullable
+    private ConnectDownloadingFragment getConnectDownloadFragment() {
+        NavHostFragment navHostFragment =
+                (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_connect);
+        Fragment currentFragment =
+                navHostFragment.getChildFragmentManager().getPrimaryNavigationFragment();
+        if (currentFragment instanceof ConnectDownloadingFragment) {
+            return (ConnectDownloadingFragment)currentFragment;
+        }
+        return null;
+    }
 }
