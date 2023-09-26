9371986a1273fc05c9b695ef45da41a13ce86fbb
Ahmad Vazirna <avazirna@gmail.com>
2023-09-26 15:32:59 +0200

    Add callout response key to biometric identifiers

--- commcare-support-library/src/main/java/org/commcare/commcaresupportlibrary/identity/BiometricIdentifier.java
+++ commcare-support-library/src/main/java/org/commcare/commcaresupportlibrary/identity/BiometricIdentifier.java
@@ -1,7 +1,25 @@
 package org.commcare.commcaresupportlibrary.identity;
 
 public enum BiometricIdentifier {
-    RIGHT_THUMB, RIGHT_INDEX_FINGER, RIGHT_MIDDLE_FINGER, RIGHT_RING_FINGER, RIGHT_PINKY_FINGER,
-    LEFT_THUMB, LEFT_INDEX_FINGER, LEFT_MIDDLE_FINGER, LEFT_RING_FINGER, LEFT_PINKY_FINGER,
-    FACE
+    RIGHT_THUMB("right_thumb_template"),
+    RIGHT_INDEX_FINGER("right_index_finger_template"),
+    RIGHT_MIDDLE_FINGER("right_middle_finger_template"),
+    RIGHT_RING_FINGER("right_ring_finger_template"),
+    RIGHT_PINKY_FINGER("right_pinky_finger_template"),
+    LEFT_THUMB("left_thumb_template"),
+    LEFT_INDEX_FINGER("left_index_finger_template"),
+    LEFT_MIDDLE_FINGER("left_middle_finger_template"),
+    LEFT_RING_FINGER("left_ring_finger_template"),
+    LEFT_PINKY_FINGER("left_pinky_finger_template"),
+    FACE("face_template");
+
+    private final String calloutResponseKey;
+
+    BiometricIdentifier(String calloutResponseKey) {
+        this.calloutResponseKey = calloutResponseKey;
+    }
+
+    public String getCalloutResponseKey() {
+        return calloutResponseKey;
+    }
 }
