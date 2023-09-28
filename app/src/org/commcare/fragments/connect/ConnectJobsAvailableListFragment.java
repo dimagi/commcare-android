fa78e9a838417cfc23d9d98f2fd194617ffcca86
Dave Viggiano <dviggiano@dimagi.com>
2023-09-28 10:42:56 -0400

    Added last updated indicator and button to try refresh at top of jobs list fragment.

--- app/src/org/commcare/fragments/connect/ConnectJobsAvailableListFragment.java
+++ app/src/org/commcare/fragments/connect/ConnectJobsAvailableListFragment.java
@@ -5,23 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.commcare.activities.connect.ConnectIdDatabaseHelper;
-import org.commcare.activities.connect.ConnectIdNetworkHelper;
 import org.commcare.adapters.ConnectJobAdapter;
-import org.commcare.android.database.connect.models.ConnectJobRecord;
 import org.commcare.dalvik.R;
-import org.javarosa.core.io.StreamsUtil;
-import org.javarosa.core.services.Logger;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -59,40 +44,7 @@ public class ConnectJobsAvailableListFragment extends Fragment {
 
         recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), linearLayoutManager.getOrientation()));
 
-        ConnectIdNetworkHelper.getConnectOpportunities(getContext(), new ConnectIdNetworkHelper.INetworkResultHandler() {
-            @Override
-            public void processSuccess(int responseCode, InputStream responseData) {
-                try {
-                    String responseAsString = new String(StreamsUtil.inputStreamToByteArray(responseData));
-                    if (responseAsString.length() > 0) {
-                        //Parse the JSON
-                        JSONArray json = new JSONArray(responseAsString);
-                        List<ConnectJobRecord> jobs = new ArrayList<>(json.length());
-                        for(int i=0; i<json.length(); i++) {
-                            JSONObject obj = (JSONObject)json.get(i);
-                            jobs.add(ConnectJobRecord.fromJson(obj));
-                        }
-
-                        //Store retrieved jobs
-                        ConnectIdDatabaseHelper.storeJobs(getContext(), jobs);
-
-                        recyclerView.setAdapter(new ConnectJobAdapter(true));
-                    }
-                } catch (IOException | JSONException | ParseException e) {
-                    Logger.exception("Parsing return from Opportunities request", e);
-                }
-            }
-
-            @Override
-            public void processFailure(int responseCode, IOException e) {
-                Logger.log("ERROR", String.format(Locale.getDefault(), "Failed: %d", responseCode));
-            }
-
-            @Override
-            public void processNetworkFailure() {
-                Logger.log("ERROR", "Failed (network)");
-            }
-        });
+        recyclerView.setAdapter(new ConnectJobAdapter(true));
 
         return view;
     }
--- app/src/org/commcare/fragments/connect/ConnectJobsListsFragment.java
+++ app/src/org/commcare/fragments/connect/ConnectJobsListsFragment.java
@@ -12,10 +12,31 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
+import org.commcare.activities.connect.ConnectIdDatabaseHelper;
+import org.commcare.activities.connect.ConnectIdNetworkHelper;
+import org.commcare.android.database.connect.models.ConnectJobRecord;
 import org.commcare.dalvik.R;
+import org.javarosa.core.io.StreamsUtil;
+import org.javarosa.core.services.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Fragment for showing the two job lists (available and mine)
@@ -23,6 +44,8 @@ import org.commcare.dalvik.R;
  * @author dviggiano
  */
 public class ConnectJobsListsFragment extends Fragment {
+    private ViewStateAdapter viewStateAdapter;
+    private TextView updateText;
 
     public ConnectJobsListsFragment() {
         // Required empty public constructor
@@ -44,8 +67,17 @@ public class ConnectJobsListsFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_connect_jobs_list, container, false);
 
+        updateText = view.findViewById(R.id.connect_jobs_last_update);
+        updateUpdatedDate();
+
+        ImageView refreshButton = view.findViewById(R.id.connect_jobs_refresh);
+        refreshButton.setOnClickListener(v -> {
+            refreshData();
+        });
+
         final ViewPager2 pager = view.findViewById(R.id.jobs_view_pager);
-        pager.setAdapter(new ViewStateAdapter(getChildFragmentManager(), getLifecycle()));
+        viewStateAdapter = new ViewStateAdapter(getChildFragmentManager(), getLifecycle());
+        pager.setAdapter(viewStateAdapter);
 
         final TabLayout tabLayout = view.findViewById(R.id.connect_jobs_tabs);
         tabLayout.addTab(tabLayout.newTab().setText(R.string.connect_jobs_all));
@@ -75,7 +107,53 @@ public class ConnectJobsListsFragment extends Fragment {
             }
         });
 
+        refreshData();
+
         return view;
+    }
+
+    public void refreshData() {
+        ConnectIdNetworkHelper.getConnectOpportunities(getContext(), new ConnectIdNetworkHelper.INetworkResultHandler() {
+            @Override
+            public void processSuccess(int responseCode, InputStream responseData) {
+                try {
+                    String responseAsString = new String(StreamsUtil.inputStreamToByteArray(responseData));
+                    if (responseAsString.length() > 0) {
+                        //Parse the JSON
+                        JSONArray json = new JSONArray(responseAsString);
+                        List<ConnectJobRecord> jobs = new ArrayList<>(json.length());
+                        for(int i=0; i<json.length(); i++) {
+                            JSONObject obj = (JSONObject)json.get(i);
+                            jobs.add(ConnectJobRecord.fromJson(obj));
+                        }
+
+                        //Store retrieved jobs
+                        ConnectIdDatabaseHelper.storeJobs(getContext(), jobs);
+
+                        updateUpdatedDate();
+                        viewStateAdapter.notifyDataSetChanged();
+                    }
+                } catch (IOException | JSONException | ParseException e) {
+                    Logger.exception("Parsing return from Opportunities request", e);
+                }
+            }
+
+            @Override
+            public void processFailure(int responseCode, IOException e) {
+                Logger.log("ERROR", String.format(Locale.getDefault(), "Opportunities call failed: %d", responseCode));
+            }
+
+            @Override
+            public void processNetworkFailure() {
+                Logger.log("ERROR", "Failed (network)");
+            }
+        });
+    }
+
+    private void updateUpdatedDate() {
+        Date lastUpdate = new Date();
+        DateFormat df = SimpleDateFormat.getDateTimeInstance();
+        updateText.setText(getString(R.string.connect_last_update, df.format(lastUpdate)));
     }
 
     private static class ViewStateAdapter extends FragmentStateAdapter {
