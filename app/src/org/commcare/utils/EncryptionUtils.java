8fdcbf600ed36daa3808106b4064215e9bf6cc4e
Ahmad Vazirna <avazirna@gmail.com>
2023-10-10 15:10:38 +0200

    Check is Android KeyStore is supported

--- app/src/org/commcare/utils/EncryptionUtils.java
+++ app/src/org/commcare/utils/EncryptionUtils.java
@@ -20,6 +20,7 @@ import javax.crypto.KeyGenerator;
 import javax.security.auth.x500.X500Principal;
 
 import static org.commcare.util.EncryptionUtils.ANDROID_KEYSTORE_PROVIDER_NAME;
+import static org.commcare.util.EncryptionUtils.isAndroidKeyStoreSupported;
 
 /**
  * Utility class for encrypting submissions during the SaveToDiskTask.
@@ -41,45 +42,46 @@ public class EncryptionUtils {
 
     // Generates a cryptrographic key and adds it to the Android KeyStore
     public static void generateCryptographicKeyForKeyStore(String keyAlias) {
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                KeyGenerator keyGenerator = KeyGenerator
-                        .getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEYSTORE_PROVIDER_NAME);
-                KeyGenParameterSpec keyGenParameterSpec = new KeyGenParameterSpec.Builder(keyAlias,
-                        KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                        .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                        .build();
-                keyGenerator.init(keyGenParameterSpec);
-                keyGenerator.generateKey();
+        if (isAndroidKeyStoreSupported()) {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    KeyGenerator keyGenerator = KeyGenerator
+                            .getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEYSTORE_PROVIDER_NAME);
+                    KeyGenParameterSpec keyGenParameterSpec = new KeyGenParameterSpec.Builder(keyAlias,
+                            KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                            .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                            .build();
+                    keyGenerator.init(keyGenParameterSpec);
+                    keyGenerator.generateKey();
+                } else {
+                    KeyPairGenerator keyGenerator = KeyPairGenerator
+                            .getInstance(KeyProperties.KEY_ALGORITHM_RSA, ANDROID_KEYSTORE_PROVIDER_NAME);
+                    GregorianCalendar start = new GregorianCalendar();
+                    GregorianCalendar end = new GregorianCalendar();
+                    end.add(GregorianCalendar.YEAR, 1);
+
+                    KeyPairGeneratorSpec keySpec = new KeyPairGeneratorSpec.Builder(CommCareApplication.instance())
+                            // Key alias to be used to retrieve it from the KeyStore
+                            .setAlias(keyAlias)
+                            // The subject used for the self-signed certificate of the generated pair
+                            .setSubject(new X500Principal(String.format("CN=%s", keyAlias)))
+                            // The serial number used for the self-signed certificate of the
+                            // generated pair
+                            .setSerialNumber(BigInteger.valueOf(1337))
+                            // Date range of validity for the generated pair
+                            .setStartDate(start.getTime())
+                            .setEndDate(end.getTime())
+                            .build();
+
+                    keyGenerator.initialize(keySpec);
+                    keyGenerator.generateKeyPair();
+                }
+
+            } catch (NoSuchAlgorithmException | NoSuchProviderException |
+                     InvalidAlgorithmParameterException e) {
+                throw new RuntimeException(e);
             }
-            else {
-                KeyPairGenerator keyGenerator = KeyPairGenerator
-                        .getInstance(KeyProperties.KEY_ALGORITHM_RSA, ANDROID_KEYSTORE_PROVIDER_NAME);
-                GregorianCalendar start = new GregorianCalendar();
-                GregorianCalendar end = new GregorianCalendar();
-                end.add(GregorianCalendar.YEAR, 1);
-
-                KeyPairGeneratorSpec keySpec = new KeyPairGeneratorSpec.Builder(CommCareApplication.instance())
-                        // Key alias to be used to retrieve it from the KeyStore
-                        .setAlias(keyAlias)
-                        // The subject used for the self-signed certificate of the generated pair
-                        .setSubject(new X500Principal(String.format("CN=%s", keyAlias)))
-                        // The serial number used for the self-signed certificate of the
-                        // generated pair
-                        .setSerialNumber(BigInteger.valueOf(1337))
-                        // Date range of validity for the generated pair
-                        .setStartDate(start.getTime())
-                        .setEndDate(end.getTime())
-                        .build();
-
-                keyGenerator.initialize(keySpec);
-                keyGenerator.generateKeyPair();
-            }
-
-        } catch (NoSuchAlgorithmException | NoSuchProviderException |
-                 InvalidAlgorithmParameterException e) {
-            throw new RuntimeException(e);
         }
     }
 }
