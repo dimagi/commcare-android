7597a0a71d17847780a962c230d854f3c1cea13f
Jay Panchal <jay@logicrays.com>
2024-06-05 16:59:41 +0530

    Add french language translation

---
+++ app/src/org/commcare/utils/LocaleHelper.java
@@ -0,0 +1,44 @@
+package org.commcare.utils;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
+
+import java.util.Locale;
+
+public class LocaleHelper {
+
+    public static void setLocale(Context context, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Configuration config = context.getResources().getConfiguration();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            setSystemLocale(config, locale);
+        } else {
+            setSystemLocaleLegacy(config, locale);
+        }
+        context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private static void setSystemLocale(Configuration config, Locale locale) {
+        config.setLocale(locale);
+    }
+
+    @SuppressWarnings("deprecation")
+    private static void setSystemLocaleLegacy(Configuration config, Locale locale) {
+        config.locale = locale;
+    }
+
+    public static String getLocale(Context context) {
+        Locale locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            locale = context.getResources().getConfiguration().getLocales().get(0);
+        } else {
+            locale = context.getResources().getConfiguration().locale;
+        }
+        return locale.getLanguage();
+    }
+}
