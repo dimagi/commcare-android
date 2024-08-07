62bb3114d6150c3b96406809a781dced9dae9525
Ahmad Vazirna <avazirna@gmail.com>
2024-08-07 13:44:49 +0200

    Return serialized form index

--- app/src/org/commcare/logic/AndroidFormController.java
+++ app/src/org/commcare/logic/AndroidFormController.java
@@ -1,17 +1,15 @@
 package org.commcare.logic;
 
+import android.util.Base64;
 import androidx.annotation.NonNull;
 
 import org.commcare.google.services.analytics.FormAnalyticsHelper;
-import org.commcare.utils.FileUtil;
+import org.commcare.utils.SerializationUtil;
 import org.commcare.views.widgets.WidgetFactory;
 import org.javarosa.core.model.FormDef;
 import org.javarosa.core.model.FormIndex;
 import org.javarosa.form.api.FormController;
 import org.javarosa.form.api.FormEntryController;
-
-import java.io.File;
-import java.util.Date;
 
 /**
  * Wrapper around FormController to handle Android-specific form entry actions
@@ -87,5 +85,15 @@ public class AndroidFormController extends FormController implements PendingCall
 
     public FormDef getFormDef() {
         return mFormEntryController.getModel().getForm();
+    }
+
+    // TODO: we should cache this
+    public String getSerializedFormIndex() {
+        try{
+            byte[] serializedFormIndex = SerializationUtil.serialize(getFormIndex());
+            return Base64.encodeToString(serializedFormIndex, Base64.DEFAULT);
+        } catch (Exception e){
+            return null;
+        }
     }
 }
