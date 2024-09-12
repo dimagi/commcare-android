1351a0a6bc68e445065542b89606dbf2af857973
Dave Viggiano <dviggiano@dimagi.com>
2024-09-12 13:12:55 -0400

    Added unit tests for encryption, and mock encryption provider to support test framework

---
+++ app/unit-tests/src/org/commcare/utils/EncryptionUtilsTest.java
@@ -0,0 +1,34 @@
+package org.commcare.utils;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Unit test for the encryption and decryption of a string
+ *
+ * @author dviggiano
+ */
+public class EncryptionUtilsTest {
+    @Test
+    public void testEncryption() {
+        try {
+            String testData = "This is a test string";
+            byte[] testBytes = testData.getBytes(StandardCharsets.UTF_8);
+
+            EncryptionKeyProvider provider = new MockEncryptionKeyProvider();
+
+            byte[] encrypted = EncryptionUtils.encrypt(testBytes, provider.getKey(null, true));
+            String encryptedString = new String(encrypted);
+            Assert.assertFalse(testData.equals(encryptedString));
+
+            byte[] decrypted = EncryptionUtils.decrypt(encrypted, provider.getKey(null, false));
+            String decryptedString = new String(decrypted);
+            Assert.assertEquals(testData, decryptedString);
+        } catch (Exception e) {
+            Assert.fail("Exception: " + e);
+        }
+    }
+}
