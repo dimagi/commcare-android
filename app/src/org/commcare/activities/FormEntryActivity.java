1c02caad82dee33ec8e2bc2cda94c5a0d074a4ad
Ahmad Vazirna <avazirna@gmail.com>
2024-09-16 23:54:35 +0200

    Replace exit flag with autoSaving flag

--- app/src/org/commcare/activities/FormEntryActivity.java
+++ app/src/org/commcare/activities/FormEntryActivity.java
@@ -252,17 +252,17 @@ public class FormEntryActivity extends SaveSessionCommCareActivity<FormEntryActi
     public void formSaveCallback(Runnable listener) {
         // note that we have started saving the form
         customFormSaveCallback = listener;
-        interruptAndSaveForm(exit);
+        interruptAndSaveForm(false);
     }
 
-    private void interruptAndSaveForm(boolean exit) {
+    private void interruptAndSaveForm(boolean autoSaving) {
         if (mFormController != null) {
             // Set flag that will allow us to restore this form when we log back in
             CommCareApplication.instance().getCurrentSessionWrapper().setCurrentStateAsInterrupted(
                     mFormController.getFormIndex());
 
             // Start saving form; will trigger expireUserSession() on completion
-            saveIncompleteFormToDisk(exit);
+            saveIncompleteFormToDisk(autoSaving);
         }
     }
 
@@ -776,8 +776,9 @@ public class FormEntryActivity extends SaveSessionCommCareActivity<FormEntryActi
         saveDataToDisk(FormEntryConstants.EXIT, true, updatedSaveName, false);
     }
 
-    private void saveIncompleteFormToDisk(boolean exit) {
-        saveDataToDisk(exit, false, null, true);
+    private void saveIncompleteFormToDisk(boolean autoSaving) {
+        boolean shouldExit = autoSaving ? FormEntryConstants.DO_NOT_EXIT : FormEntryConstants.EXIT;
+        saveDataToDisk(shouldExit, false, null, true);
     }
 
     /**
@@ -935,7 +936,7 @@ public class FormEntryActivity extends SaveSessionCommCareActivity<FormEntryActi
     protected void onStop() {
         super.onStop();
         if (shouldSaveFormOnStop()) {
-            interruptAndSaveForm(false);
+            interruptAndSaveForm(true);
         }
     }
 
