2eeaa24a096e70d76ddb68c3bcd148b09d2b4f47
Dave Viggiano <dviggiano@dimagi.com>
2024-09-18 12:16:22 -0400

    Restored code path for secondary phone verification (when clicking either button on first page).

--- app/src/org/commcare/fragments/connectId/ConnectIdMessageFragment.java
+++ app/src/org/commcare/fragments/connectId/ConnectIdMessageFragment.java
@@ -177,6 +177,14 @@ public class ConnectIdMessageFragment extends Fragment {
                 }
                 break;
             case ConnectConstants.CONNECT_VERIFY_ALT_PHONE_MESSAGE:
+                if (success) {
+                    if (secondButton) {
+                        directions = ConnectIdMessageFragmentDirections.actionConnectidMessageToConnectidPhone(ConnectConstants.CONNECT_VERIFY_ALT_PHONE_CHANGE, ConnectConstants.METHOD_CHANGE_ALTERNATE, null);
+                    } else {
+                        directions = ConnectIdMessageFragmentDirections.actionConnectidMessageToConnectidPhoneVerify(ConnectConstants.CONNECT_VERIFY_ALT_PHONE, String.format(Locale.getDefault(), "%d",
+                                ConnectIdPhoneVerificationFragmnet.MethodVerifyAlternate), null, user.getUserId(), user.getPassword(), null).setAllowChange(false);
+                    }
+                }
                 break;
         }
         if (success) {
