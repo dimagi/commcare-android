f1ee84dcbbaa12ee420698685ec7f657a9e95aab
Devansh <devanshstan17@gmail.com>
2024-10-17 11:26:58 +0530

    viewpager2 Poc with issues

--- app/src/org/commcare/adapters/EntityDetailPagerAdapter.java
+++ app/src/org/commcare/adapters/EntityDetailPagerAdapter.java
@@ -1,24 +1,29 @@
 package org.commcare.adapters;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import org.commcare.CommCareApplication;
 import org.commcare.cases.entity.EntityUtil;
+import org.commcare.dalvik.R;
 import org.commcare.fragments.EntityDetailFragment;
 import org.commcare.fragments.EntitySubnodeDetailFragment;
 import org.commcare.suite.model.Detail;
 import org.commcare.utils.SerializationUtil;
 import org.javarosa.core.model.instance.TreeReference;
 
+import java.util.Objects;
+
 /**
  * Subclass of FragmentStatePagerAdapter for populating a ViewPager (swipe-based paging widget) with entity detail fields.
  *
  * @author jschweers
  */
-public class EntityDetailPagerAdapter extends FragmentStatePagerAdapter {
+public class EntityDetailPagerAdapter extends FragmentStateAdapter {
 
     private ListItemViewModifier modifier;
     private final Detail detail;
@@ -28,7 +33,7 @@ public class EntityDetailPagerAdapter extends FragmentStatePagerAdapter {
 
     public EntityDetailPagerAdapter(FragmentManager fm, Detail detail, int detailIndex,
                                     TreeReference reference, ListItemViewModifier modifier) {
-        super(fm);
+        super(Objects.requireNonNull(fm.findFragmentById(R.id.entity_detail_tabs)));
         this.detail = detail;
         this.detailIndex = detailIndex;
         this.mEntityReference = reference;
@@ -38,10 +43,11 @@ public class EntityDetailPagerAdapter extends FragmentStatePagerAdapter {
                         CommCareApplication.instance().getCurrentSessionWrapper().getEvaluationContext()));
     }
 
+    @NonNull
     @Override
-    public Fragment getItem(int i) {
+    public Fragment createFragment(int position) {
         EntityDetailFragment fragment;
-        if (detail.getNodeset() != null || (detail.isCompound() && displayableChildDetails[i].getNodeset() != null)) {
+        if (detail.getNodeset() != null || (detail.isCompound() && displayableChildDetails[position].getNodeset() != null)) {
             fragment = new EntitySubnodeDetailFragment();
         } else {
             fragment = new EntityDetailFragment();
@@ -50,7 +56,7 @@ public class EntityDetailPagerAdapter extends FragmentStatePagerAdapter {
         Bundle args = new Bundle();
         args.putString(EntityDetailFragment.DETAIL_ID, detail.getId());
         if (detail.isCompound()) {
-            args.putInt(EntityDetailFragment.CHILD_DETAIL_INDEX, i);
+            args.putInt(EntityDetailFragment.CHILD_DETAIL_INDEX, position);
         }
         args.putInt(EntityDetailFragment.DETAIL_INDEX, detailIndex);
         SerializationUtil.serializeToBundle(args, EntityDetailFragment.CHILD_REFERENCE, mEntityReference);
@@ -59,14 +65,7 @@ public class EntityDetailPagerAdapter extends FragmentStatePagerAdapter {
     }
 
     @Override
-    public CharSequence getPageTitle(int position) {
-        Detail detailShowing = detail.isCompound() ? displayableChildDetails[position] : detail;
-        return detailShowing.getTitle().getText().evaluate();
-    }
-
-    @Override
-    public int getCount() {
+    public int getItemCount() {
         return detail.isCompound() ? displayableChildDetails.length : 1;
     }
-
 }
