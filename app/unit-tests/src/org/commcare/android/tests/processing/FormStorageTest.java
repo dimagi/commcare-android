20c3cf80ace74f64dc8e8140d2e5e9aa80f9c952
Dave Viggiano <dviggiano@dimagi.com>
2024-11-06 13:31:54 -0500

    Repaired broken unit test.

--- app/unit-tests/src/org/commcare/android/tests/processing/FormStorageTest.java
+++ app/unit-tests/src/org/commcare/android/tests/processing/FormStorageTest.java
@@ -360,7 +360,6 @@ public class FormStorageTest {
             // Added in 2.55
             , "org.javarosa.core.model.FormIndex"
             , "org.commcare.models.database.InterruptedFormState"
-
             , "org.commcare.android.database.connect.models.ConnectLinkedAppRecordV3"
             , "org.commcare.android.database.connect.models.ConnectLinkedAppRecordV8"
             , "org.commcare.android.database.connect.models.ConnectLinkedAppRecordV9"
