a3af6b6f1027a3fd80d53030de5f5f62f7b62e86
parthmittal <pmittal@dimagi-associate.com>
2024-11-22 08:51:12 +0530

    -added launch icon and title bar icon in commcare

--- app/src/org/commcare/activities/CommCareActivity.java
+++ app/src/org/commcare/activities/CommCareActivity.java
@@ -28,6 +28,7 @@ import android.widget.Toast;
 
 import org.commcare.CommCareApplication;
 import org.commcare.android.database.user.models.ACase;
+import org.commcare.dalvik.R;
 import org.commcare.fragments.BreadcrumbBarFragment;
 import org.commcare.fragments.ContainerFragment;
 import org.commcare.fragments.TaskConnectorFragment;
@@ -162,7 +163,7 @@ public abstract class CommCareActivity<R> extends AppCompatActivity
             persistManagedUiState(fm);
 
             if (getSupportActionBar() != null) {
-                getSupportActionBar().setLogo(org.commcare.dalvik.R.mipmap.commcare_launcher);
+                getSupportActionBar().setLogo(org.commcare.dalvik.R.drawable.logo_spacing);
             }
 
             if (shouldShowBreadcrumbBar()) {
