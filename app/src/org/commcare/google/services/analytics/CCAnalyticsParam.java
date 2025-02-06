14f5c984192dd41dc742b95d54096f3c0111fa69
Shubham Goyal <shubham1g5@gmail.com>
2025-02-06 21:56:06 +0530

    Adds form entry event and form schema to existing form events

--- app/src/org/commcare/google/services/analytics/CCAnalyticsParam.java
+++ app/src/org/commcare/google/services/analytics/CCAnalyticsParam.java
@@ -20,6 +20,7 @@ public class CCAnalyticsParam {
     static final String REASON = "reason";
     static final String UI_STATE = "uite_state";
     static final String USERNAME = "username";
+    static final String FORM_ID = "form_id";
 
     static final String USER_RETURNED = "user_returned";
 
