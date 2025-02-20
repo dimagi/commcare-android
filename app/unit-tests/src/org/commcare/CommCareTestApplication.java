6b5765581815fcdbc212abb7aac30643f09a5854
Shubham Goyal <shubham1g5@gmail.com>
2025-02-20 23:16:57 +0530

    fix for workmanager initialization issues in test

--- app/unit-tests/src/org/commcare/CommCareTestApplication.java
+++ app/unit-tests/src/org/commcare/CommCareTestApplication.java
@@ -66,7 +66,6 @@ public class CommCareTestApplication extends CommCareApplication implements Test
     private static final String TAG = CommCareTestApplication.class.getSimpleName();
     private static PrototypeFactory testPrototypeFactory;
     private static final ArrayList<String> factoryClassNames = new ArrayList<>();
-
     private String cachedUserPassword;
 
     private final ArrayList<Throwable> asyncExceptions = new ArrayList<>();
@@ -75,6 +74,7 @@ public class CommCareTestApplication extends CommCareApplication implements Test
     public void onCreate() {
         // set if before calling super to initialte the dataChangeLogger correctly
         setExternalStorageState(Environment.MEDIA_MOUNTED);
+        initWorkManager();
 
         super.onCreate();
 
@@ -104,10 +104,15 @@ public class CommCareTestApplication extends CommCareApplication implements Test
 
     public static void initWorkManager() {
         Context context = ApplicationProvider.getApplicationContext();
-        Configuration config = new Configuration.Builder()
-                .setMinimumLoggingLevel(Log.DEBUG)
-                .build();
-        WorkManager.initialize(context, config);
+        try {
+            // first try to get instance to see if it's already initialised
+            WorkManager.getInstance(context);
+        } catch (IllegalStateException e) {
+            Configuration config = new Configuration.Builder()
+                    .setMinimumLoggingLevel(Log.DEBUG)
+                    .build();
+            WorkManager.initialize(context, config);
+        }
     }
 
     @Override
