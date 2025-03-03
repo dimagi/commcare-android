466a632fa0f5fa52d0c0963225eaad802c28791a
Dave Viggiano <dviggiano@dimagi.com>
2025-03-03 15:49:48 -0500

    Attempting to retrieve encryption key for consented channel when a message is received for that channel and the key hasn't been retrieved yet.

--- app/src/org/commcare/connect/network/ApiConnectId.java
+++ app/src/org/commcare/connect/network/ApiConnectId.java
@@ -2,6 +2,7 @@ package org.commcare.connect.network;
 
 import android.content.Context;
 import android.os.Handler;
+import android.util.Log;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
@@ -9,6 +10,7 @@ import com.google.common.collect.Multimap;
 import org.commcare.CommCareApplication;
 import org.commcare.activities.CommCareActivity;
 import org.commcare.android.database.connect.models.ConnectLinkedAppRecord;
+import org.commcare.android.database.connect.models.ConnectMessagingChannelRecord;
 import org.commcare.android.database.connect.models.ConnectMessagingMessageRecord;
 import org.commcare.connect.ConnectConstants;
 import org.commcare.connect.ConnectDatabaseHelper;
@@ -509,6 +511,21 @@ public class ApiConnectId {
                 API_VERSION_CONNECT_ID, authInfo, params, false, false, callback);
     }
 
+    public static void retrieveChannelEncryptionKeySync(Context context, ConnectMessagingChannelRecord channel) {
+        AuthInfo.TokenAuth auth = ApiConnectId.retrieveConnectIdTokenSync(context);
+        if(auth != null) {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("channel_id", channel.getChannelId());
+
+            ConnectNetworkHelper.PostResult result = ConnectNetworkHelper.postSync(context,
+                    channel.getKeyUrl(), null, auth, params, true, true);
+
+            if(result.responseCode >= 200 && result.responseCode < 300) {
+                handleReceivedEncryptionKey(context, result.responseStream, channel);
+            }
+        }
+    }
+
     public static void retrieveChannelEncryptionKey(Context context, String channelId, String channelUrl, IApiCallback callback) {
         ConnectSsoHelper.retrieveConnectTokenAsync(context, token -> {
             HashMap<String, Object> params = new HashMap<>();
@@ -518,6 +535,21 @@ public class ApiConnectId {
                     channelUrl,
                     null, token, params, true, true, callback);
         });
+    }
+
+    public static void handleReceivedEncryptionKey(Context context, InputStream stream, ConnectMessagingChannelRecord channel) {
+        try {
+            String responseAsString = new String(
+                    StreamsUtil.inputStreamToByteArray(stream));
+
+            if(responseAsString.length() > 0) {
+                JSONObject json = new JSONObject(responseAsString);
+                channel.setKey(json.getString("key"));
+                ConnectDatabaseHelper.storeMessagingChannel(context, channel);
+            }
+        } catch(Exception e) {
+            Logger.exception("Messaging channel encryption key", e);
+        }
     }
 
     public static void confirmReceivedMessages(Context context, String username, String password,
