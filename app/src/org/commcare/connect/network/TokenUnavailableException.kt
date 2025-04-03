6329a157e0a9c99445aa3c5041cbd5057c298695
David Viggiano <dave.viggiano@gmail.com>
2025-04-03 15:50:49 -0400

    Added javadocs for token exceptions explaining their usage. Removed extra data in TokenUnavailableException (it was never used since logging happens earlier in the code).

--- app/src/org/commcare/connect/network/TokenUnavailableException.kt
+++ app/src/org/commcare/connect/network/TokenUnavailableException.kt
@@ -2,21 +2,14 @@ package org.commcare.connect.network
 
 import java.io.IOException
 
-class TokenUnavailableException: IOException {
-    var innerException: Exception? = null
-    var responseCode: Int? = null
-
+/**
+ * Exception thrown when an SSO token is unavailable.
+ * This can occur when the API call to retrieve an SSO token fails, i.e. due to bad network connectivity
+ * It can also happen if one SSO token (ConnectID) is being used as auth to acquire another SSO token (HQ),
+ *      and the first token is rejected by the second OAuth server (i.e. due to expired token)
+ * The best course of action is usually to try again.
+ */
+class TokenUnavailableException() : IOException() {
     override val message: String
         get() = "Token is unavailable"
-
-    constructor() : super() {
-    }
-
-    constructor(innerException: Exception) : super(innerException) {
-        this.innerException = innerException
-    }
-
-    constructor(responseCode: Int) : super() {
-        this.responseCode = responseCode
-    }
 }
