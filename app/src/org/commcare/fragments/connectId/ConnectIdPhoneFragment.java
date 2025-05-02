a77fc976e1c674a5fce032c0803952be2a841cf0
parthmittal <pmittal@dimagi-associate.com>
2025-05-02 10:37:46 +0530

    -removed skip phone number check , introduced phone is valid or not , formatting

--- app/src/org/commcare/fragments/connectId/ConnectIdPhoneFragment.java
+++ app/src/org/commcare/fragments/connectId/ConnectIdPhoneFragment.java
@@ -24,8 +24,6 @@ import org.commcare.utils.PhoneNumberHelper;
 import java.io.InputStream;
 import java.util.Locale;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
@@ -39,7 +37,6 @@ public class ConnectIdPhoneFragment extends Fragment {
 
     private String existingPhone;
     private ScreenConnectPrimaryPhoneBinding binding;
-    protected boolean skipPhoneNumberCheck = false;
 
     TextWatcher watcher = new TextWatcher() {
         @Override
@@ -48,7 +45,7 @@ public class ConnectIdPhoneFragment extends Fragment {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            checkPhoneNumber();
+            updateButtonEnabled();
         }
 
         @Override
@@ -83,7 +80,7 @@ public class ConnectIdPhoneFragment extends Fragment {
             existingPhone = ConnectIdPhoneFragmentArgs.fromBundle(getArguments()).getPhone();
         }
         binding.connectPrimaryPhoneInput.addTextChangedListener(watcher);
-        binding.connectPrimaryPhoneButton.setOnClickListener(v -> handleButtonPress());
+        binding.connectPrimaryPhoneButton.setOnClickListener(v -> checkPhoneNumber());
         //Special case for initial reg. screen. Remembering phone number before account has been created
         ConnectUserRecord user = ConnectManager.getUser(getActivity());
         String title = getString(R.string.connect_phone_title_primary);
@@ -107,11 +104,19 @@ public class ConnectIdPhoneFragment extends Fragment {
         ConnectDatabaseHelper.setRegistrationPhase(getActivity(),
                 ConnectConstants.CONNECT_REGISTRATION_CONFIGURE_BIOMETRICS);
         NavDirections directions =
-                ConnectIdPhoneFragmentDirections.actionConnectidPhoneNoToConnectidPhoneVerify(ConnectConstants.CONNECT_REGISTRATION_VERIFY_PRIMARY_PHONE, String.valueOf(
-                                ConnectIdPhoneVerificationFragment.MethodRegistrationPrimary), phone, user.getUserId(),
-                        user.getPassword(), null, false);
-
+                ConnectIdPhoneFragmentDirections.actionConnectidPhoneNoToConnectidPhoneVerify(
+                        ConnectConstants.CONNECT_REGISTRATION_VERIFY_PRIMARY_PHONE,
+                        String.valueOf(ConnectIdPhoneVerificationFragment.MethodRegistrationPrimary), phone,
+                        user.getUserId(), user.getPassword(), null, false);
         Navigation.findNavController(binding.connectPrimaryPhoneButton).navigate(directions);
+    }
+
+    private void updateButtonEnabled() {
+        String phone = PhoneNumberHelper.buildPhoneNumber(binding.countryCode.getText().toString(),
+                binding.connectPrimaryPhoneInput.getText().toString());
+
+        boolean valid = PhoneNumberHelper.getInstance(requireActivity()).isValidPhoneNumber(phone);
+        binding.connectPrimaryPhoneButton.setEnabled(valid);
     }
 
     void displayNumber(String fullNumber) {
@@ -131,14 +136,11 @@ public class ConnectIdPhoneFragment extends Fragment {
         if (fullNumber != null && fullNumber.startsWith(codeText)) {
             fullNumber = fullNumber.substring(codeText.length());
         }
-        skipPhoneNumberCheck = false;
         binding.connectPrimaryPhoneInput.setText(fullNumber);
-        skipPhoneNumberCheck = true;
         binding.countryCode.setText(codeText);
-        skipPhoneNumberCheck = false;
     }
 
-    public void handleButtonPress() {
+    private void updatePhoneNumberCall() {
         String phone = PhoneNumberHelper.buildPhoneNumber(binding.countryCode.getText().toString(),
                 binding.connectPrimaryPhoneInput.getText().toString());
         ConnectUserRecord user = ConnectManager.getUser(getContext());
@@ -147,38 +149,32 @@ public class ConnectIdPhoneFragment extends Fragment {
             IApiCallback callback = new IApiCallback() {
                 @Override
                 public void processSuccess(int responseCode, InputStream responseData) {
-                    skipPhoneNumberCheck = false;
                     finish(phone);
                 }
 
                 @Override
                 public void processFailure(int responseCode) {
-                    skipPhoneNumberCheck = false;
                     Toast.makeText(getContext(), getString(R.string.connect_phone_change_error),
                             Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void processNetworkFailure() {
-                    skipPhoneNumberCheck = false;
                     ConnectNetworkHelper.showNetworkError(requireContext());
                 }
 
                 @Override
                 public void processTokenUnavailableError() {
-                    skipPhoneNumberCheck = false;
                     ConnectNetworkHelper.handleTokenUnavailableException(requireContext());
                 }
 
                 @Override
                 public void processTokenRequestDeniedError() {
-                    skipPhoneNumberCheck = false;
                     ConnectNetworkHelper.handleTokenRequestDeniedException(requireContext());
                 }
 
                 @Override
                 public void processOldApiError() {
-                    skipPhoneNumberCheck = false;
                     ConnectNetworkHelper.showOutdatedApiError(requireContext());
                 }
             };
@@ -192,79 +188,72 @@ public class ConnectIdPhoneFragment extends Fragment {
     }
 
     private void resetPhoneNumber() {
-        skipPhoneNumberCheck = false;
         binding.connectPrimaryPhoneAvailability.setText("");
     }
 
-    public void checkPhoneNumber() {
-        if (!skipPhoneNumberCheck) {
-            String phone =
-                    binding.countryCode.getText().toString() + binding.connectPrimaryPhoneInput.getText().toString();
+    private void displayMessage(String displayText, boolean isButtonEnabled) {
+        binding.errorTextView.setText(displayText);
+        binding.connectPrimaryPhoneButton.setEnabled(isButtonEnabled);
+    }
 
-            boolean valid = PhoneNumberHelper.getInstance(getContext()).isValidPhoneNumber(phone);
-            ConnectUserRecord user = ConnectManager.getUser(getContext());
+    private void checkPhoneNumber() {
+        String phone =
+                binding.countryCode.getText().toString()
+                        + binding.connectPrimaryPhoneInput.getText().toString();
 
-            if (valid) {
-                String existingPrimary = user != null ? user.getPrimaryPhone() : existingPhone;
-                if (existingPrimary != null && existingPrimary.equals(phone)) {
-                    binding.errorTextView.setText("");
-                    binding.connectPrimaryPhoneButton.setEnabled(true);
-                } else {
-                    binding.connectPrimaryPhoneAvailability.setText(getString(R.string.connect_phone_checking));
-                    binding.connectPrimaryPhoneButton.setEnabled(false);
-                    ApiConnectId.checkPhoneAvailable(getContext(), phone,
-                            new IApiCallback() {
-                                @Override
-                                public void processSuccess(int responseCode, InputStream responseData) {
-                                    resetPhoneNumber();
-                                    binding.errorTextView.setText(getString(R.string.connect_phone_available));
-                                    binding.connectPrimaryPhoneButton.setEnabled(true);
+        boolean valid = PhoneNumberHelper.getInstance(getContext()).isValidPhoneNumber(phone);
+        ConnectUserRecord user = ConnectManager.getUser(getContext());
 
-                                }
+        if (valid) {
+            String existingPrimary = user != null ? user.getPrimaryPhone() : existingPhone;
+            if (existingPrimary != null && existingPrimary.equals(phone)) {
+                displayMessage(getString(R.string.connect_phone_invalid), false);
+            } else {
+                displayMessage(getString(R.string.connect_phone_checking), false);
+                ApiConnectId.checkPhoneAvailable(getContext(), phone,
+                        new IApiCallback() {
+                            @Override
+                            public void processSuccess(int responseCode, InputStream responseData) {
+                                resetPhoneNumber();
+                                displayMessage(getString(R.string.connect_phone_available), true);
+                                updatePhoneNumberCall();
+                            }
 
-                                @Override
-                                public void processFailure(int responseCode) {
-                                    resetPhoneNumber();
-                                    binding.errorTextView.setText(getString(R.string.connect_phone_unavailable));
-                                    binding.connectPrimaryPhoneButton.setEnabled(false);
-                                }
+                            @Override
+                            public void processFailure(int responseCode) {
+                                resetPhoneNumber();
+                                displayMessage(getString(R.string.connect_phone_unavailable), false);
+                            }
 
-                                @Override
-                                public void processNetworkFailure() {
-                                    resetPhoneNumber();
-                                    binding.errorTextView.setText(getString(R.string.recovery_network_unavailable));
-                                }
+                            @Override
+                            public void processNetworkFailure() {
+                                resetPhoneNumber();
+                                displayMessage(getString(R.string.recovery_network_unavailable), false);
+                            }
 
-                                @Override
-                                public void processTokenUnavailableError() {
-                                    resetPhoneNumber();
-                                    binding.errorTextView.setText(getString(R.string.recovery_network_token_unavailable));
-                                }
+                            @Override
+                            public void processTokenUnavailableError() {
+                                resetPhoneNumber();
+                                displayMessage(getString(R.string.recovery_network_token_unavailable), false);
+                            }
 
-                                @Override
-                                public void processTokenRequestDeniedError() {
-                                    resetPhoneNumber();
-                                    binding.errorTextView.setText(getString(R.string.recovery_network_token_request_rejected));
-                                }
+                            @Override
+                            public void processTokenRequestDeniedError() {
+                                resetPhoneNumber();
+                                displayMessage(getString(R.string.recovery_network_token_request_rejected),
+                                        false);
+                            }
 
-                                @Override
-                                public void processOldApiError() {
-                                    resetPhoneNumber();
-                                    binding.errorTextView.setText(getString(R.string.recovery_network_outdated));
-                                }
-                            });
-                }
+                            @Override
+                            public void processOldApiError() {
+                                resetPhoneNumber();
+                                displayMessage(getString(R.string.recovery_network_outdated), false);
+                            }
+                        });
             }
 
         } else {
-            binding.errorTextView.setText(getString(R.string.connect_phone_invalid));
-            binding.connectPrimaryPhoneButton.setEnabled(false);
+            displayMessage(getString(R.string.connect_phone_invalid), false);
         }
-    }
-
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
     }
 }
