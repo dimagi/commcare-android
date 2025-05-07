2ca0c5a8041d0b7f3b93bd67b103ed994d5bf5e1
parthmittal <pmittal@dimagi-associate.com>
2025-05-07 20:15:09 +0530

    -refactored the extension function

--- app/src/org/commcare/utils/JsonExtensions.kt
+++ app/src/org/commcare/utils/JsonExtensions.kt
@@ -4,7 +4,8 @@ package org.commcare.utils
 
 import org.json.JSONObject
 
-fun JSONObject.optStringSafe(key: String): String? {
-    val value = this.optString(key, null)
-    return if (this.isNull(key) || value == "null") null else value
+fun JSONObject.optStringSafe(key: String, fallback: String? = null): String? {
+    if (this.isNull(key)) return fallback
+    val value = fallback?.let { this.optString(key, it) }
+    return value ?: fallback
 }
