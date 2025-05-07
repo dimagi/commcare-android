85167f91dd22f99da0f18a11a67cccddc2d02968
parthmittal <pmittal@dimagi-associate.com>
2025-05-07 20:15:09 +0530

    -refactored the extension function

--- app/src/org/commcare/android/database/connect/models/ConnectJobRecord.java
+++ app/src/org/commcare/android/database/connect/models/ConnectJobRecord.java
@@ -223,10 +223,9 @@ public class ConnectJobRecord extends Persisted implements Serializable {
         String flagsKey = "verification_flags";
         JSONObject flags = json.has(flagsKey) && !json.isNull(flagsKey) ? json.getJSONObject(flagsKey) : null;
         if (flags != null) {
-            job.dailyStartTime = JsonExtensions.optStringSafe(flags, META_DAILY_START_TIME) != null
-                    ? JsonExtensions.optStringSafe(flags, META_DAILY_START_TIME) : "";
-            job.dailyFinishTime = JsonExtensions.optStringSafe(flags, META_DAILY_FINISH_TIME) != null
-                    ? JsonExtensions.optStringSafe(flags, META_DAILY_FINISH_TIME) : "";
+            job.dailyStartTime =
+                    JsonExtensions.optStringSafe(flags, META_DAILY_START_TIME, "");
+            job.dailyFinishTime = JsonExtensions.optStringSafe(flags, META_DAILY_FINISH_TIME, "");
         }
 
         JSONArray unitsJson = json.getJSONArray(META_PAYMENT_UNITS);
