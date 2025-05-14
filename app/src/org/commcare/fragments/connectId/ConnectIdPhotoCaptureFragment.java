2dc5c5955dfa887430451f27b75cc398ea6c30a6
Shubham Goyal <shubham1g5@gmail.com>
2025-05-14 19:59:28 +0530

    Display and upload Photo on server

--- app/src/org/commcare/fragments/connectId/ConnectIdPhotoCaptureFragment.java
+++ app/src/org/commcare/fragments/connectId/ConnectIdPhotoCaptureFragment.java
@@ -2,6 +2,7 @@ package org.commcare.fragments.connectId;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +14,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.commcare.android.database.connect.models.ConnectUserRecord;
+import org.commcare.connect.ConnectIDManager;
+import org.commcare.connect.network.ApiConnectId;
+import org.commcare.connect.network.ConnectNetworkHelper;
+import org.commcare.connect.network.IApiCallback;
 import org.commcare.dalvik.databinding.ScreenConnectidPhotoCaptureBinding;
 import org.commcare.fragments.MicroImageActivity;
+import org.commcare.utils.MediaUtil;
+
+import java.io.InputStream;
 
 /**
  * Screen to capture user's photo as part of Connect ID Account management process
@@ -22,15 +31,18 @@ import org.commcare.fragments.MicroImageActivity;
 public class ConnectIdPhotoCaptureFragment extends Fragment {
 
     private ActivityResultLauncher<Intent> takePhotoLauncher;
+    private @NonNull ScreenConnectidPhotoCaptureBinding viewBinding;
+    private ConnectUserRecord connectUserRecord;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        ScreenConnectidPhotoCaptureBinding binding = ScreenConnectidPhotoCaptureBinding.inflate(inflater, container, false);
+        viewBinding = ScreenConnectidPhotoCaptureBinding.inflate(inflater, container, false);
+        connectUserRecord = ConnectIDManager.getInstance().getUser(getContext());
         initTakePhotoLauncher();
-        setUpUi(binding);
-        return binding.getRoot();
+        setUpUi();
+        return viewBinding.getRoot();
     }
 
     private void initTakePhotoLauncher() {
@@ -38,22 +50,99 @@ public class ConnectIdPhotoCaptureFragment extends Fragment {
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
                     if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                        /**
-                         * 1. extract photo
-                         * 2. show it on screen
-                         * 3. On save, make an API call to save it on server
-                         */
+                        String photoAsBase64 = result.getData().getStringExtra(MicroImageActivity.MICRO_IMAGE_BASE_64_RESULT_KEY);
+                        displayImage(photoAsBase64);
+                        uploadImage(photoAsBase64);
                     }
                 }
         );
     }
 
-    private void setUpUi(ScreenConnectidPhotoCaptureBinding binding) {
-        binding.takePhotoButton.setOnClickListener(v -> executeTakePhoto());
+    private void setUpUi() {
+        viewBinding.takePhotoButton.setOnClickListener(v -> executeTakePhoto());
+    }
+
+    private void uploadImage(String photoAsBase64) {
+        IApiCallback networkResponseCallback = new IApiCallback() {
+            @Override
+            public void processSuccess(int responseCode, InputStream responseData) {
+                onPhotoUploadSuccess(photoAsBase64);
+            }
+
+            @Override
+            public void processFailure(int responseCode) {
+                onPhotoUploadFailure();
+            }
+
+            @Override
+            public void processNetworkFailure() {
+                onPhotoUploadFailure();
+                ConnectNetworkHelper.showNetworkError(getContext());
+            }
+
+            @Override
+            public void processTokenUnavailableError() {
+                onPhotoUploadFailure();
+                ConnectNetworkHelper.handleTokenUnavailableException(requireActivity());
+            }
+
+            @Override
+            public void processTokenRequestDeniedError() {
+                ConnectNetworkHelper.handleTokenDeniedException(requireActivity());
+            }
+
+            @Override
+            public void processOldApiError() {
+                ConnectNetworkHelper.showOutdatedApiError(getContext());
+            }
+        };
+        ApiConnectId.updatePhoto(requireContext(), connectUserRecord.getUserId(), connectUserRecord.getPassword(),
+                photoAsBase64, networkResponseCallback);
+    }
+
+    private void onPhotoUploadFailure() {
+        showError();
+        enableTakePhotoButton();
+        enableSaveButton();
+    }
+
+    private void enableSaveButton() {
+    }
+
+    private void onPhotoUploadSuccess(String photoAsBase64) {
+        clearError();
+        enableTakePhotoButton();
+        savePhotoToDatabase(photoAsBase64);
+    }
+
+    private void savePhotoToDatabase(String photoAsBase64) {
+
+    }
+
+    private void clearError() {
+
+    }
+
+    private void showError() {
+
+    }
+
+    private void displayImage(String photoAsBase64) {
+        Bitmap bitmap = MediaUtil.decodeBase64EncodedBitmap(photoAsBase64);
+        viewBinding.photoImageView.setImageBitmap(bitmap);
     }
 
     private void executeTakePhoto() {
+        disableTakePhotoButton();
         Intent intent = new Intent(requireContext(), MicroImageActivity.class);
         takePhotoLauncher.launch(intent);
+    }
+
+    private void disableTakePhotoButton() {
+        viewBinding.takePhotoButton.setEnabled(false);
+    }
+
+    private void enableTakePhotoButton() {
+        viewBinding.takePhotoButton.setEnabled(true);
     }
 }
