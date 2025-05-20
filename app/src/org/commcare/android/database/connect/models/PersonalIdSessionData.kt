6a21c30d5f61882d8d8d37b2aa1be865afa923f0
parthmittal <pmittal@dimagi-associate.com>
2025-05-20 17:15:40 +0530

    -make normal model class removed singleton

--- app/src/org/commcare/android/database/connect/models/PersonalIdSessionData.kt
+++ app/src/org/commcare/android/database/connect/models/PersonalIdSessionData.kt
@@ -2,24 +2,17 @@ package org.commcare.android.database.connect.models
 
 import androidx.annotation.StringDef
 
-class PersonalIdSessionData  // Private constructor
-private constructor() {
-    @StringDef(*[DEVICE_TYPE, BIOMETRIC_TYPE])
-    @Retention(AnnotationRetention.SOURCE)
-    annotation class acceptedPinType
+class PersonalIdSessionData {
 
-    // Getters and setters
-    // Fields from various payloads
-    @JvmField
-    @acceptedPinType
+    @StringDef(DEVICE_TYPE, BIOMETRIC_TYPE)
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class AcceptedPinType
+
+    @AcceptedPinType
     var requiredLock: String? = null
-    @JvmField
     var demoUser: Boolean? = null
-    @JvmField
     var token: String? = null
-    @JvmField
     var sessionFailureCode: String? = null
-    @JvmField
     var sessionFailureSubcode: String? = null
     var accountExists: Boolean? = null
     var photoBase64: String? = null
@@ -28,25 +21,22 @@ private constructor() {
     var oauthPassword: String? = null
     var accountOrphaned: Boolean? = null
 
-    // Optional: Clear all data
     fun clear() {
-        instance = null
+        requiredLock = null
+        demoUser = null
+        token = null
+        sessionFailureCode = null
+        sessionFailureSubcode = null
+        accountExists = null
+        photoBase64 = null
+        username = null
+        dbKey = null
+        oauthPassword = null
+        accountOrphaned = null
     }
 
     companion object {
-        // Singleton instance
-        private var instance: PersonalIdSessionData? = null
         private const val DEVICE_TYPE = "device"
         private const val BIOMETRIC_TYPE = "biometric"
-
-        // Singleton accessor
-        @JvmStatic
-        @Synchronized
-        fun getInstance(): PersonalIdSessionData? {
-            if (instance == null) {
-                instance = PersonalIdSessionData()
-            }
-            return instance
-        }
     }
 }
