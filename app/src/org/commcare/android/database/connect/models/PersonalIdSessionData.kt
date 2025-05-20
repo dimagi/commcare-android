736721490a6817e0a24d8a22d40c3b933d79ff5e
parthmittal <pmittal@dimagi-associate.com>
2025-05-21 01:44:52 +0530

    - changed PersonalIdSessionData class to data class

--- app/src/org/commcare/android/database/connect/models/PersonalIdSessionData.kt
+++ app/src/org/commcare/android/database/connect/models/PersonalIdSessionData.kt
@@ -2,41 +2,34 @@ package org.commcare.android.database.connect.models
 
 import androidx.annotation.StringDef
 
-class PersonalIdSessionData {
+/**
+ * Data holder for personal identification session state during Connect ID flows.
+ * This includes authentication configuration and account-related metadata.
+ */
+data class PersonalIdSessionData(
+        @DeviceAuthType var requiredLock: String? = null, //Tells which device auth is required for the given user
+        var demoUser: Boolean? = null, //states weather it is a demo user or normal user
+        var token: String? = null, // session token
+        var sessionFailureCode: String? = null, // Reason code to tell why user is not allowed to move forward with the flow
+        var sessionFailureSubcode: String? = null, // Sub Reason code to tell why user is not allowed to move forward with the flow
+        var accountExists: Boolean? = null, // Tells weather its new user or old
+        var photoBase64: String? = null, // photo of the user
+        var username: String? = null, // name os the user
+        var dbKey: String? = null, // DB Key
+        var oauthPassword: String? = null, // password to verify usser
+        var accountOrphaned: Boolean? = null // Nobody owns this account
+) {
 
+    /**
+     * Annotation to restrict accepted authentication types used by the device.
+     * Only DEVICE_TYPE or BIOMETRIC_TYPE are allowed.
+     */
     @StringDef(DEVICE_TYPE, BIOMETRIC_TYPE)
     @Retention(AnnotationRetention.SOURCE)
-    annotation class AcceptedPinType
-
-    @AcceptedPinType
-    var requiredLock: String? = null
-    var demoUser: Boolean? = null
-    var token: String? = null
-    var sessionFailureCode: String? = null
-    var sessionFailureSubcode: String? = null
-    var accountExists: Boolean? = null
-    var photoBase64: String? = null
-    var username: String? = null
-    var dbKey: String? = null
-    var oauthPassword: String? = null
-    var accountOrphaned: Boolean? = null
-
-    fun clear() {
-        requiredLock = null
-        demoUser = null
-        token = null
-        sessionFailureCode = null
-        sessionFailureSubcode = null
-        accountExists = null
-        photoBase64 = null
-        username = null
-        dbKey = null
-        oauthPassword = null
-        accountOrphaned = null
-    }
+    annotation class DeviceAuthType
 
     companion object {
-        private const val DEVICE_TYPE = "device"
-        private const val BIOMETRIC_TYPE = "biometric"
+        const val DEVICE_TYPE = "device"
+        const val BIOMETRIC_TYPE = "biometric"
     }
 }
