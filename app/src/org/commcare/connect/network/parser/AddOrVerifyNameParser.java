697fca9714cfc45e9dc7d6196ce006687fa7ce72
parthmittal <pmittal@dimagi-associate.com>
2025-05-22 18:00:39 +0530

    -added name parser

---
+++ app/src/org/commcare/connect/network/parser/AddOrVerifyNameParser.java
@@ -0,0 +1,30 @@
+package org.commcare.connect.network.parser;
+
+import org.commcare.android.database.connect.models.PersonalIdSessionData;
+import org.commcare.utils.JsonExtensions;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Parses a JSON response from the check_name API call
+ * and populates a PersonalIdSessionData instance.
+ */
+public class AddOrVerifyNameParser {
+    private final JSONObject json;
+
+    public AddOrVerifyNameParser(JSONObject json) {
+        this.json = json;
+    }
+
+    /**
+     * Parses and sets values on the given PersonalIdSessionData instance.
+     *
+     * @param sessionData the instance to populate
+     * @throws JSONException if a parsing error occurs
+     */
+
+    public void parse(PersonalIdSessionData sessionData) throws JSONException {
+        sessionData.setAccountExists(json.optBoolean("account_exists", false));
+        sessionData.setToken(JsonExtensions.optStringSafe(json, "photo", null));
+    }
+}
