c0c0353884bc659e9cf5135487c59930a57cbb70
David Viggiano <dave.viggiano@gmail.com>
2025-05-28 10:54:35 -0400

    Improved generic PersonalID de-configure error message, and renamed it to be more clear.

--- app/src/org/commcare/utils/GlobalErrors.java
+++ app/src/org/commcare/utils/GlobalErrors.java
@@ -3,7 +3,7 @@ package org.commcare.utils;
 import org.commcare.dalvik.R;
 
 public enum GlobalErrors {
-    PERSONALID_GENERIC_ERROR(R.string.connect_db_corrupt),
+    PERSONALID_GENERIC_ERROR(R.string.personalid_generic_error),
     PERSONALID_LOST_CONFIGURATION_ERROR(R.string.recovery_network_token_request_rejected);
 
     final int messageId;
