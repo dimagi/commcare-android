d79eef89e950936cae5e6dffe8b8d07877de3059
parthmittal <pmittal@dimagi-associate.com>
2025-05-29 16:46:55 +0530

    -complete profile parser

---
+++ app/src/org/commcare/connect/network/parser/CompleteProfileResponseParser.java
@@ -0,0 +1,25 @@
+package org.commcare.connect.network.parser;
+
+import org.commcare.android.database.connect.models.PersonalIdSessionData;
+import org.commcare.utils.JsonExtensions;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Parses a JSON response from the complete profile API call
+ * and populates a PersonalIdSessionData instance.
+ */
+public class CompleteProfileResponseParser implements PersonalIdApiResponseParser{
+    /**
+     * Parses and sets values on the given PersonalIdSessionData instance.
+     *
+     * @param sessionData the instance to populate
+     * @throws JSONException if a parsing error occurs
+     */
+    @Override
+    public void parse(JSONObject json, PersonalIdSessionData sessionData) throws JSONException {
+        sessionData.setPersonalId(JsonExtensions.optStringSafe(json, "username", null));
+        sessionData.setDbKey(JsonExtensions.optStringSafe(json, "db_key", null));
+        sessionData.setOauthPassword(JsonExtensions.optStringSafe(json, "password", null));
+    }
+}
