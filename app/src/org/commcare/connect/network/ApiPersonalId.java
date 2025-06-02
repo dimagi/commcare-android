a99e588acad0540188fdb4cf536b3314917a8859
Jay Panchal <jpanchal@dimagi-associate.com>
2025-06-02 11:14:23 +0530

    - Resolved comments

--- app/src/org/commcare/connect/network/ApiPersonalId.java
+++ app/src/org/commcare/connect/network/ApiPersonalId.java
@@ -229,11 +229,13 @@ public class ApiPersonalId {
                     } catch (IOException e) {
                         // Handle error when reading the stream
                         callback.processFailure(response.code());
-                        callback.processFailureWithParser(response.body().byteStream());
                     }
                 } else {
                     // Handle validation errors
                     logNetworkError(response);
+                    if (response.errorBody() != null){
+                        callback.processFailureWithParser(response.errorBody().byteStream());
+                    }
                     callback.processFailure(response.code());
                 }
             }
