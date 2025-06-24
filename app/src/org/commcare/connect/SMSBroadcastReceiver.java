4e68585e9bd8ed0ef102c7724e52d50b58c68fe1
parthmittal <pmittal@dimagi-associate.com>
2025-06-24 10:41:59 +0530

    - removed unused class and some nit changes

--- app/src/org/commcare/connect/SMSBroadcastReceiver.java
+++ app/src/org/commcare/connect/SMSBroadcastReceiver.java
@@ -17,6 +17,7 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
 
     public interface SMSListener {
         void onSuccess(Intent consentIntent);
+
         void onFailure(int statusCode);
     }
 
@@ -26,32 +27,40 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
     }
 
     public void setSmsListener(SMSListener listener) {
-       this.smsListener = listener;
+        this.smsListener = listener;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (!SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) return;
+        if (!SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) {
+            return;
+        }
 
         Bundle extras = intent.getExtras();
-        if (extras == null) return;
+        if (extras == null) {
+            return;
+        }
 
-        Status status = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
-        if (status == null) return;
+        Status status = (Status)extras.get(SmsRetriever.EXTRA_STATUS);
+        if (status == null) {
+            return;
+        }
+
+        if (smsListener == null) {
+            return;
+        }
 
         switch (status.getStatusCode()) {
             case CommonStatusCodes.SUCCESS:
                 Intent consentIntent = extras.getParcelable(SmsRetriever.EXTRA_CONSENT_INTENT);
-                if (consentIntent != null && smsListener != null) {
+                if (consentIntent != null) {
                     smsListener.onSuccess(consentIntent);
                 }
                 break;
 
             case CommonStatusCodes.TIMEOUT:
             case CommonStatusCodes.ERROR:
-                if (smsListener != null) {
-                    smsListener.onFailure(status.getStatusCode());
-                }
+                smsListener.onFailure(status.getStatusCode());
                 break;
         }
     }
