0cdebaeee6c5feb68cdc69e13ad24e6eab3d9aba
Jay Panchal <jpanchal@dimagi-associate.com>
2025-08-06 15:00:57 +0530

    Solved conflicts

--- app/src/org/commcare/connect/network/connectId/parser/RetrieveCredentialsResponseParser.kt
+++ app/src/org/commcare/connect/network/connectId/parser/RetrieveCredentialsResponseParser.kt
@@ -13,7 +13,7 @@ import java.io.InputStream
  */
 class RetrieveCredentialsResponseParser<T>(private val context: Context) : BaseApiResponseParser<T> {
 
-    override fun parse(responseCode: Int, responseData: InputStream): T {
+    override fun parse(responseCode: Int, responseData: InputStream, anyInputObject: Any?): T {
         val jsonText = responseData.bufferedReader().use { it.readText() }
         val jsonObject = JSONObject(jsonText)
         val jsonArray = jsonObject.getJSONArray("credentials")
