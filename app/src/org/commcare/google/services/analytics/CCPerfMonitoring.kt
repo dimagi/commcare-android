25cc12114828be824a6a228d42b40ce3f52db45e
Ahmad Vazirna <avazirna@gmail.com>
2025-09-10 16:05:09 +0200

    Add custom trace for app sync duration

--- app/src/org/commcare/google/services/analytics/CCPerfMonitoring.kt
+++ app/src/org/commcare/google/services/analytics/CCPerfMonitoring.kt
@@ -9,12 +9,15 @@ object CCPerfMonitoring {
     // Traces
     // Measures the duration of synchronous case list loading
     const val TRACE_SYNC_ENTITY_LIST_LOADING = "sync_case_list_loading"
+
     const val TRACE_CASE_SEARCH_TIME = "case_search_time"
 
     // Attributes
     const val ATTR_NUM_CASES_LOADED = "number_of_cases_loaded"
-    const val ATTR_RESULTS_COUNT: String = "case_search_results_count"
-    const val ATTR_SEARCH_QUERY_LENGTH: String = "case_search_query_length"
+    const val ATTR_RESULTS_COUNT = "case_search_results_count"
+    const val ATTR_SEARCH_QUERY_LENGTH = "case_search_query_length"
+    const val TRACE_APP_SYNC_DURATION = "app_sync_duration"
+    const val ATTR_SYNC_SUCESS = "sync_success"
 
     fun startTracing(traceName: String): Trace? {
         try {
