9fbc134fa0819f5e8f852ddf9e92fef1428cf1f3
Shubham Goyal <shubham1g5@gmail.com>
2025-10-08 10:51:17 +0200

    Switch to parsing opportunity id as both String and Int from intent as we are using both data types in different places in code

--- app/src/org/commcare/activities/connect/ConnectActivity.java
+++ app/src/org/commcare/activities/connect/ConnectActivity.java
@@ -24,6 +24,7 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.common.base.Strings;
 
+import org.apache.commons.lang3.StringUtils;
 import org.commcare.activities.NavigationHostCommCareActivity;
 import org.commcare.activities.PushNotificationActivity;
 import org.commcare.android.database.connect.models.ConnectJobRecord;
@@ -82,6 +83,12 @@ public class ConnectActivity extends NavigationHostCommCareActivity<ConnectActiv
     private void initStateFromExtras() {
         redirectionAction = getIntent().getStringExtra(REDIRECT_ACTION);
         int opportunityId = getIntent().getIntExtra(ConnectConstants.OPPORTUNITY_ID, -1);
+        if (opportunityId == -1) {
+            String opportunityIdStr = getIntent().getStringExtra(ConnectConstants.OPPORTUNITY_ID);
+            if (!StringUtils.isEmpty(opportunityIdStr)) {
+                opportunityId = Integer.parseInt(opportunityIdStr);
+            }
+        }
         if(opportunityId != -1) {
             job = ConnectJobUtils.getCompositeJob(this, opportunityId);
         }
