618b0f4b8b4af762eaece584df8d7c97a36991b2
Conroy Ricketts <cricketts@dimagi.com>
2025-11-24 15:00:11 -0500

    CCCT-1889 Personal ID Error Codes
    Added more comments.

--- app/src/org/commcare/connect/network/base/BaseApiHandler.kt
+++ app/src/org/commcare/connect/network/base/BaseApiHandler.kt
@@ -31,27 +31,52 @@ abstract class BaseApiHandler<T>(
     }
 
     enum class PersonalIdOrConnectApiErrorCodes {
+        // A catch-all for errors with an unusual response code.
         UNKNOWN_ERROR,
+        // Typical network errors like not having an internet connection.
         NETWORK_ERROR,
+        // The app is outdated and needs to be updated.
         OLD_API_ERROR,
+        // Examples of this include the user being in an unsupported country, an uninvited user
+        // receiving an OTP, or an integrity failure.
         FORBIDDEN_ERROR,
+        // The request to get an SSO auth token failed, usually due to network issues.
         TOKEN_UNAVAILABLE_ERROR,
+        // The request to get an SSO auth token is denied.
         TOKEN_DENIED_ERROR,
+        // The firebase ID token is invalid.
         TOKEN_INVALID_ERROR,
         INVALID_RESPONSE_ERROR,
+        // There was an issue parsing an API response.
         JSON_PARSING_ERROR,
+        // Examples of this include the user enterring an incorrect OTP, account lockout, or an
+        // expired SSO auth token.
         FAILED_AUTH_ERROR,
+        // Examples of this include a user profile photo failing to upload or the photo being too
+        // large.
         SERVER_ERROR,
+        // There were too many request attempts.
         RATE_LIMIT_EXCEEDED_ERROR,
+        // The user enetered an incorrect backup code too many times.
         ACCOUNT_LOCKED_ERROR,
+        // The user's device did not pass security checks.
         INTEGRITY_ERROR,
+        // There is something wrong with the API request such as a lost PersonalID configuration.
         BAD_REQUEST_ERROR,
+        // The user's backup code is not set correctly.
         NO_RECOVERY_PIN_SET_ERROR,
+        // There's missing data in the API request.
         MISSING_DATA_ERROR,
+        // There was a phone number mismatch when vaildating the firebase ID token.
         PHONE_MISMATCH_ERROR,
+        // The firebase ID token was missing when validating it.
         MISSING_TOKEN_ERROR,
+        // There was an issue verifying the firebase ID token.
         FAILED_VALIDATING_TOKEN_ERROR,
+        // The user's name is missing from the API request.
         NAME_REQUIRED_ERROR,
+        // The user attempted to create a new profile with a phone number that's already tied to an
+        // existing account.
         ACTIVE_USER_EXISTS_ERROR, ;
 
         fun shouldAllowRetry(): Boolean =
