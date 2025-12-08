5b5415960f2b0a4180626d6ad4e906e33bfa41ba
David Viggiano <dave.viggiano@gmail.com>
2025-12-08 13:29:51 -0500

    Deleted unused equals and hashCode functions

--- app/src/org/commcare/gis/EntityMapDisplayInfo.kt
+++ app/src/org/commcare/gis/EntityMapDisplayInfo.kt
@@ -8,37 +8,4 @@ data class EntityMapDisplayInfo(
     val boundaryColorHex: Int? = null,
     val points: Array<LatLng>? = null,
     val pointColorsHex: Array<Int>? = null
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as EntityMapDisplayInfo
-
-        if (location != other.location) return false
-        if (boundary != null) {
-            if (other.boundary == null) return false
-            if (!boundary.contentEquals(other.boundary)) return false
-        } else if (other.boundary != null) return false
-        if (boundaryColorHex != other.boundaryColorHex) return false
-        if (points != null) {
-            if (other.points == null) return false
-            if (!points.contentEquals(other.points)) return false
-        } else if (other.points != null) return false
-        if (pointColorsHex != null) {
-            if (other.pointColorsHex == null) return false
-            if (!pointColorsHex.contentEquals(other.pointColorsHex)) return false
-        } else if (other.pointColorsHex != null) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = location?.hashCode() ?: 0
-        result = 31 * result + (boundary?.contentHashCode() ?: 0)
-        result = 31 * result + (boundaryColorHex ?: 0)
-        result = 31 * result + (points?.contentHashCode() ?: 0)
-        result = 31 * result + (pointColorsHex?.contentHashCode() ?: 0)
-        return result
-    }
-}
+)
