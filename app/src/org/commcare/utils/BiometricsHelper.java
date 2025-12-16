10934042baab44a5b474e44504fbc5867757e347
jaypanchal-13 <jpanchal@dimagi-associate.com>
2025-12-16 17:51:19 +0530

    removed irrelevant biometric codes

--- app/src/org/commcare/utils/BiometricsHelper.java
+++ app/src/org/commcare/utils/BiometricsHelper.java
@@ -262,12 +262,6 @@ public class BiometricsHelper {
                     activity.getString(R.string.personalid_biometric_error_lockout);
             case BiometricPrompt.ERROR_LOCKOUT_PERMANENT ->
                     activity.getString(R.string.personalid_biometric_error_lockout_permanent);
-            case BiometricPrompt.ERROR_HW_NOT_PRESENT ->
-                    activity.getString(R.string.personalid_biometric_error_hw_not_present);
-            case BiometricPrompt.ERROR_HW_UNAVAILABLE ->
-                    activity.getString(R.string.personalid_biometric_error_hw_unavailable);
-            case BiometricPrompt.ERROR_NO_BIOMETRICS ->
-                    activity.getString(R.string.personalid_biometric_error_no_biometric);
             case BiometricPrompt.ERROR_TIMEOUT ->
                     activity.getString(R.string.personalid_biometric_error_timeout);
             default -> activity.getString(R.string.personalid_biometric_error_cannot_configure);
