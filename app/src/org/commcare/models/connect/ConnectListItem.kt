23f144e065bc4fabe3ae6f2fa73660dc930e859d
Conroy Ricketts <cricketts@dimagi.com>
2025-12-24 15:12:51 -0500

    CCCT-1948 Connect Landing Page Redesign
    Ran ktlint on ConnectListItem

--- app/src/org/commcare/models/connect/ConnectListItem.kt
+++ app/src/org/commcare/models/connect/ConnectListItem.kt
@@ -3,9 +3,12 @@ package org.commcare.models.connect
 import androidx.annotation.StringRes
 
 sealed class ConnectListItem {
-    data class SectionHeader(@StringRes val textResID: Int) : ConnectListItem()
+    data class SectionHeader(
+        @StringRes val textResID: Int,
+    ) : ConnectListItem()
+
     data class JobItem(
         val jobModel: ConnectLoginJobListModel,
-        val isCorrupt: Boolean
+        val isCorrupt: Boolean,
     ) : ConnectListItem()
 }
