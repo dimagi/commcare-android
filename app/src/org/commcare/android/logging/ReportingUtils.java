4e129b7f341977a1b4a4b8d3b16ad035d87e7e33
Ahmad Vazirna <avazirna@gmail.com>
2025-12-29 11:41:42 +0200

    Fix user info retrieval for Crashlytics session

--- app/src/org/commcare/android/logging/ReportingUtils.java
+++ app/src/org/commcare/android/logging/ReportingUtils.java
@@ -144,8 +144,8 @@ public class ReportingUtils {
             if (user.isEmpty()) {
                 return getPersonalID();
             }
-        } catch (Exception ignored) {
-        }
+            return user;
+        } catch (Exception ignored) {}
         return null;
     }
 
