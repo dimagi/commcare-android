cfeef05d061790d2846691db8a42bf8c22418f4c
Shubham Goyal <shubham1g5@gmail.com>
2026-01-13 12:39:16 -0600

    log stale locations as non-fatals, remove deprecated API

--- app/src/org/commcare/location/CommCareLocationController.kt
+++ app/src/org/commcare/location/CommCareLocationController.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import androidx.core.content.ContextCompat
 import org.commcare.CommCareApplication
+import org.javarosa.core.services.Logger
 
 /**
  * @author $|-|!˅@M
@@ -17,8 +18,21 @@ interface CommCareLocationController {
     fun destroy()
 }
 
-fun CommCareLocationController.isLocationPermissionGranted(mContext: Context?): Boolean {
+fun isLocationPermissionGranted(mContext: Context?): Boolean {
     val context = mContext ?: CommCareApplication.instance()
     return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
             ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+}
+
+fun isFresh(location: Location, maxAgeMs: Long = 2 * 60 * 1000): Boolean {
+    return System.currentTimeMillis() - location.time <= maxAgeMs
+}
+
+fun logStaleLocation(location: Location) {
+    if (!isFresh(location)) {
+        Logger.exception(
+            "Received a stale location",
+            Exception("Received a stale location for location: $location" + " with time ${location.time}" + " and current device time ${System.currentTimeMillis()}")
+        )
+    }
 }
