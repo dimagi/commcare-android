7a48c6921330e496aaff3175f749e5a00cf0cc94
Conroy Ricketts <cricketts@dimagi.com>
2026-01-19 15:47:03 -0500

    CCCT-2017 Release Toggles Parser
    Fixed failing unit tests.

--- app/unit-tests/src/org/commcare/connect/network/connectId/parser/ConnectReleaseTogglesParserTest.kt
+++ app/unit-tests/src/org/commcare/connect/network/connectId/parser/ConnectReleaseTogglesParserTest.kt
@@ -1,11 +1,16 @@
 package org.commcare.connect.network.connectId.parser
 
+import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
 import org.commcare.CommCareTestApplication
 import org.commcare.connect.database.ConnectAppDatabaseUtil
 import org.commcare.connect.network.connect.parser.ConnectReleaseTogglesParser
+import org.javarosa.core.model.utils.DateUtils
 import org.json.JSONException
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
@@ -18,11 +23,18 @@ import java.io.ByteArrayInputStream
 @Config(application = CommCareTestApplication::class)
 @RunWith(AndroidJUnit4::class)
 class ConnectReleaseTogglesParserTest {
+    private var context: Context = CommCareTestApplication.instance()
     private lateinit var parser: ConnectReleaseTogglesParser
 
     @Before
     fun setUp() {
         parser = ConnectReleaseTogglesParser()
+        mockkStatic(ConnectAppDatabaseUtil::class)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkStatic(ConnectAppDatabaseUtil::class)
     }
 
     @Test
@@ -47,7 +59,7 @@ class ConnectReleaseTogglesParserTest {
             }
             """.trimIndent()
 
-        val expectedDateString = "Mon Jan 26 00:00:00 GMT 2026"
+        val expectedDate = DateUtils.parseDateTime(mockDateString)
 
         val inputStream = ByteArrayInputStream(mockJson.toByteArray())
 
@@ -55,20 +67,20 @@ class ConnectReleaseTogglesParserTest {
         every { ConnectAppDatabaseUtil.storeReleaseToggles(any(), any()) } returns Unit
 
         // Act
-        val result = parser.parse(200, inputStream, null)
+        val result = parser.parse(200, inputStream, context)
 
         // Assert
         assertEquals(2, result.size)
 
         assertEquals("feature_a", result[0].slug)
         assertTrue(result[0].active)
-        assertEquals(expectedDateString, result[0].createdAt.toString())
-        assertEquals(expectedDateString, result[0].modifiedAt.toString())
+        assertEquals(expectedDate, result[0].createdAt)
+        assertEquals(expectedDate, result[0].modifiedAt)
 
         assertEquals("feature_b", result[1].slug)
         assertFalse(result[1].active)
-        assertEquals(expectedDateString, result[1].createdAt.toString())
-        assertEquals(expectedDateString, result[1].modifiedAt.toString())
+        assertEquals(expectedDate, result[1].createdAt)
+        assertEquals(expectedDate, result[1].modifiedAt)
     }
 
     @Test(expected = JSONException::class)
@@ -84,7 +96,7 @@ class ConnectReleaseTogglesParserTest {
         val inputStream = ByteArrayInputStream(mockJson.toByteArray())
 
         // Act
-        parser.parse(200, inputStream, null)
+        parser.parse(200, inputStream, context)
     }
 
     @Test(expected = JSONException::class)
@@ -100,7 +112,7 @@ class ConnectReleaseTogglesParserTest {
         val inputStream = ByteArrayInputStream(mockJson.toByteArray())
 
         // Act
-        parser.parse(200, inputStream, null)
+        parser.parse(200, inputStream, context)
     }
 
     @Test
@@ -121,7 +133,7 @@ class ConnectReleaseTogglesParserTest {
             }
             """.trimIndent()
 
-        val expectedDateString = "Mon Jan 26 00:00:00 GMT 2026"
+        val expectedDate = DateUtils.parseDateTime(mockDateString)
 
         val inputStream = ByteArrayInputStream(mockJson.toByteArray())
 
@@ -129,15 +141,15 @@ class ConnectReleaseTogglesParserTest {
         every { ConnectAppDatabaseUtil.storeReleaseToggles(any(), any()) } returns Unit
 
         // Act
-        val result = parser.parse(200, inputStream, null)
+        val result = parser.parse(200, inputStream, context)
 
         // Assert
         assertEquals(1, result.size)
 
         assertEquals("feature_a", result[0].slug)
         assertTrue(result[0].active)
-        assertEquals(expectedDateString, result[0].createdAt.toString())
-        assertEquals(expectedDateString, result[0].modifiedAt.toString())
+        assertEquals(expectedDate, result[0].createdAt)
+        assertEquals(expectedDate, result[0].modifiedAt)
     }
 
     @Test
@@ -159,7 +171,7 @@ class ConnectReleaseTogglesParserTest {
         every { ConnectAppDatabaseUtil.storeReleaseToggles(any(), any()) } returns Unit
 
         // Act
-        val result = parser.parse(200, inputStream, null)
+        val result = parser.parse(200, inputStream, context)
 
         // Assert
         assertTrue(result.isEmpty())
