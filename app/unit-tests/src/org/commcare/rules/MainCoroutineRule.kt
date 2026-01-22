218e32e1687a5644821becad8ac52beb764ad815
David Viggiano <dave.viggiano@gmail.com>
2026-01-22 15:43:29 -0500

    Upgraded deprecated TestCoroutineDispatcher to UnconfinedTestDispatcher.

--- app/unit-tests/src/org/commcare/rules/MainCoroutineRule.kt
+++ app/unit-tests/src/org/commcare/rules/MainCoroutineRule.kt
@@ -2,15 +2,16 @@ package org.commcare.rules
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class MainCoroutineRule(val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+class MainCoroutineRule(val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
 ) : TestWatcher() {
 
     override fun starting(description: Description) {
@@ -21,11 +22,10 @@ class MainCoroutineRule(val testDispatcher: TestCoroutineDispatcher = TestCorout
     override fun finished(description: Description) {
         super.finished(description)
         Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
     }
 
     fun runBlockingTest(block: suspend () -> Unit) =
-            this.testDispatcher.runBlockingTest {
+            runTest(testDispatcher) {
                 block()
             }
 }
