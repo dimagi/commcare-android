53a37de8085e6d8de5633f444a151548982b39d7
Ahmad Vazirna <avazirna@gmail.com>
2026-02-24 14:28:08 +0200

    Add SessionManager class to handle Android Keystore key

---
+++ app/src/org/commcare/services/SessionManager.kt
@@ -0,0 +1,28 @@
+package org.commcare.services
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import org.commcare.android.security.AesKeyStoreHandler
+import org.commcare.utils.EncryptionKeyAndTransform
+import java.security.Key
+
+/**
+ * Currently used to manage the session Android keystore backed encryption key,
+ */
+@RequiresApi(Build.VERSION_CODES.M)
+class SessionManager {
+
+    companion object {
+        private const val SESSION_KEY_ALIAS = "session_encryption_key"
+
+        private val sessionKeyAndTransformation by lazy {
+            AesKeyStoreHandler(SESSION_KEY_ALIAS, needsUserAuth = false).getKeyOrGenerate()
+        }
+
+        @JvmStatic
+        fun getEncryptionKey(): Key = sessionKeyAndTransformation.key
+
+        @JvmStatic
+        fun getKeyTransformation(): String = sessionKeyAndTransformation.transformation
+    }
+}
