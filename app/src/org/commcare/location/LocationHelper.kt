49218d91f6978d6d5bfcc4c416885d1f00060eec
David Viggiano <dave.viggiano@gmail.com>
2026-03-03 16:20:09 -0500

    Allowing null listener in CommCareLocationControllerFactory (for one-time requests).

--- app/src/org/commcare/location/LocationHelper.kt
+++ app/src/org/commcare/location/LocationHelper.kt
@@ -12,14 +12,7 @@ object LocationHelper {
 
     @JvmStatic
     fun getCurrentLocation(context: Context): Task<Location?> {
-        val noOpListener = object : CommCareLocationListener {
-            override fun onLocationRequestStart() {}
-            override fun onLocationResult(result: Location) {}
-            override fun missingPermissions() {}
-            override fun onLocationRequestFailure(failure: CommCareLocationListener.Failure) {}
-            override fun onLocationServiceChange(locationServiceEnabled: Boolean) {}
-        }
-        return CommCareLocationControllerFactory.getLocationController(context, noOpListener)
+        return CommCareLocationControllerFactory.getLocationController(context, null)
             .getCurrentLocation()
     }
 
