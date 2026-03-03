fa1def3f81e7adf5da0df16124f0d7a4dab45cde
jaypanchal-13 <jpanchal@dimagi-associate.com>
2026-03-03 11:43:05 +0530

    coderabbit

--- app/src/org/commcare/utils/KeyboardHelper.java
+++ app/src/org/commcare/utils/KeyboardHelper.java
@@ -2,6 +2,7 @@ package org.commcare.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.IBinder;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -38,16 +39,17 @@ public class KeyboardHelper {
     }
 
     public static void hideKeyboard(Activity activity) {
+
+        InputMethodManager imm =
+                (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+
         View view = activity.getCurrentFocus();
-        if (view == null) {
-            view = new View(activity);
-        }
 
-        InputMethodManager imm = (InputMethodManager)
-                activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        IBinder token = view != null
+                ? view.getWindowToken()
+                : activity.getWindow().getDecorView().getWindowToken();
 
-        if (imm != null) {
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
+        imm.hideSoftInputFromWindow(token, 0);
+
     }
 }
