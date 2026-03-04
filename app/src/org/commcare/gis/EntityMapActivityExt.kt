ffc964c2f1ae9fb50640f9b161fea09992664d49
David Viggiano <dave.viggiano@gmail.com>
2026-03-04 11:33:25 -0500

    Making sure map still loads when location or entity loading fails.

--- app/src/org/commcare/gis/EntityMapActivityExt.kt
+++ app/src/org/commcare/gis/EntityMapActivityExt.kt
@@ -4,7 +4,6 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import androidx.lifecycle.lifecycleScope
@@ -17,27 +16,29 @@ import kotlin.coroutines.resumeWithException
 fun EntityMapActivity.loadMap(mapReadyTask: Task<GoogleMap>) {
     lifecycleScope.launch {
         showProgressDialog(EntityMapActivity.MAP_LOAD_TASK_ID)
-        try {
-            coroutineScope {
-                val entitiesDeferred = async(Dispatchers.IO) { addEntityData() }
-                val locationDeferred = async { LocationHelper.getCurrentLocationWithTimeout(this@loadMap) }
-                val mapDeferred = async {
-                    suspendCancellableCoroutine { cont ->
-                        mapReadyTask.addOnSuccessListener { cont.resume(it) }
-                        mapReadyTask.addOnFailureListener { cont.resumeWithException(it) }
-                    }
-                }
 
-                val map = mapDeferred.await()
-                entitiesDeferred.await()
-                val location = locationDeferred.await()
-
-                dismissProgressDialogForTask(EntityMapActivity.MAP_LOAD_TASK_ID)
-                setupMap(map, location)
+        val entitiesDeferred = async(Dispatchers.IO) {
+            try {
+                addEntityData()
+                null
+            } catch (e: XPathException) {
+                e
             }
-        } catch (e: XPathException) {
-            dismissProgressDialogForTask(EntityMapActivity.MAP_LOAD_TASK_ID)
-            UserfacingErrorHandling<EntityMapActivity>().logErrorAndShowDialog(this@loadMap, e, true)
+        }
+        val locationDeferred = async { LocationHelper.getCurrentLocationWithTimeout(this@loadMap) }
+        val map = suspendCancellableCoroutine<GoogleMap> { cont ->
+            mapReadyTask.addOnSuccessListener { cont.resume(it) }
+            mapReadyTask.addOnFailureListener { cont.resumeWithException(it) }
+        }
+
+        val entityError = entitiesDeferred.await()
+        val location = locationDeferred.await()
+
+        dismissProgressDialogForTask(EntityMapActivity.MAP_LOAD_TASK_ID)
+        setupMap(map, location)
+
+        if (entityError != null) {
+            UserfacingErrorHandling<EntityMapActivity>().logErrorAndShowDialog(this@loadMap, entityError, true)
         }
     }
 }
