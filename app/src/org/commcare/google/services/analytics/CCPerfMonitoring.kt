ea020e8ddd96c205d4c1b9db2a0d9557c6b56a59
Ahmad Vazirna <avazirna@gmail.com>
2026-03-18 14:16:03 +0200

    Refactor convert new java class to kotlin object

--- app/src/org/commcare/google/services/analytics/CCPerfMonitoring.kt
+++ app/src/org/commcare/google/services/analytics/CCPerfMonitoring.kt
@@ -35,7 +35,7 @@ object CCPerfMonitoring {
 
 
     fun startTracing(traceName: String): Trace? {
-        if (!FirebaseUtils.isFirebaseEnabled()) {
+        if (!FirebaseUtils.isFirebaseEnabled) {
             return null
         }
         try {
