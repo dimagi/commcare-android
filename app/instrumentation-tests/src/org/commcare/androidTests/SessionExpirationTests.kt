081f0f4551128582af50dda9d9fd27d5247516c1
Ahmad Vazirna <avazirna@gmail.com>
2026-05-29 15:05:24 +0200

    Lint

--- app/instrumentation-tests/src/org/commcare/androidTests/SessionExpirationTests.kt
+++ app/instrumentation-tests/src/org/commcare/androidTests/SessionExpirationTests.kt
@@ -1,36 +1,22 @@
 package org.commcare.androidTests
 
 
-import android.widget.FrameLayout
 import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiObject
-import org.commcare.dalvik.R
 import androidx.test.uiautomator.Until
-import androidx.test.uiautomator.Until.findObject
-import junit.framework.Assert.assertNotNull
 import org.commcare.AppUtils
 import org.commcare.annotations.BrowserstackTests
 import org.commcare.dalvik.test.BuildConfig
 import org.commcare.utils.InstrumentationUtility
-import org.commcare.utils.isPresent
 import org.hamcrest.Matchers.*
-
-import org.junit.Assert
-
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.text.DateFormat
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 
@@ -56,7 +42,7 @@ class SessionExpirationTests: BaseTest() {
         if (BuildConfig.DEBUG || AppUtils.getInstalledAppRecords().size <= 1) {
             uiDevice.wait(Until.hasObject(By.textStartsWith("Logged Into Commcare")),5000)
         } else {
-            uiDevice.wait(Until.hasObject(By.textStartsWith("Logged Into "+ APP_NAME)),5000)
+            uiDevice.wait(Until.hasObject(By.textStartsWith("Logged Into $APP_NAME")),5000)
         }
         uiDevice.findObject(By.textStartsWith("Session Expires:")).click()
         uiDevice.wait(Until.hasObject(By.textStartsWith("Welcome")),45000)
