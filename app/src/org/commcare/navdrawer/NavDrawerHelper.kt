863f4532786de181c207a8d8cb25928b1f007824
Shubham Goyal <shubham1g5@gmail.com>
2026-06-03 21:30:41 +0530

    lint

--- app/src/org/commcare/navdrawer/NavDrawerHelper.kt
+++ app/src/org/commcare/navdrawer/NavDrawerHelper.kt
@@ -1,12 +1,10 @@
 package org.commcare.navdrawer
 
-import androidx.annotation.VisibleForTesting
+import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import org.commcare.CommCareApplication
-import androidx.core.content.edit
 
 object NavDrawerHelper {
-
     private const val SIDE_DRAWER_SHOWN: String = "side-drawer-shown"
 
     fun drawerShownBefore(): Boolean {
