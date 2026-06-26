f4e5118fa191a96e2d733bb886e0eab5ff39f8d0
Ahmad Vazirna <avazirna@gmail.com>
2026-06-26 16:04:24 +0200

    Trigger face capture

--- app/src/org/commcare/views/widgets/FaceCaptureWidget.kt
+++ app/src/org/commcare/views/widgets/FaceCaptureWidget.kt
@@ -1,6 +1,10 @@
 package org.commcare.views.widgets
 
 import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import org.commcare.activities.components.FormEntryConstants
+import org.commcare.fragments.MicroImageActivity
 import org.commcare.logic.PendingCalloutInterface
 import org.javarosa.form.api.FormEntryPrompt
 
@@ -10,5 +14,10 @@ class FaceCaptureWidget(context: Context?, prompt: FormEntryPrompt?, pic: Pendin
         mChooseButton.visibility = GONE
     }
 
-    override fun takePicture() {}
+    override fun takePicture() {
+        val i = Intent(getContext(), MicroImageActivity::class.java)
+
+        (context as AppCompatActivity).startActivityForResult(i, FormEntryConstants.FACE_CAPTURE)
+        pendingCalloutInterface.setPendingCalloutFormIndex(mPrompt.getIndex())
+    }
 }
