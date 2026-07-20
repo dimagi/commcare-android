dc307e560a738897b277b5901344c7bf9cc6b4f8
Ahmad Vazirna <avazirna@gmail.com>
2026-07-20 12:00:20 +0200

    Include display cutout insets when padding for system bars

--- app/src/org/commcare/utils/AndroidUtil.java
+++ app/src/org/commcare/utils/AndroidUtil.java
@@ -93,7 +93,7 @@ public class AndroidUtil {
 
         if (activityRootView != null) {
             ViewCompat.setOnApplyWindowInsetsListener(activityRootView, (view, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
 
                 // Apply padding so content doesn't overlap with system bars
                 view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
